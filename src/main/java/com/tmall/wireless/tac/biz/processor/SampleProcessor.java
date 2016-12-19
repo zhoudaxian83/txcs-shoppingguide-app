fa3a9fb92181440b04ed579d75ed09d371f267de
tiongsong <changkun.hck@alibaba-inc.com>
2016-12-19 18:21:30 +0800

    update

--- src/main/java/com/tmall/wireless/tac/biz/processor/SampleProcessor.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/SampleProcessor.java
@@ -1,5 +1,16 @@
 package com.tmall.wireless.tac.biz.processor;
 
-public class SampleProcessor {
+import com.tmall.wireless.tac.client.ProcessContext;
+import com.tmall.wireless.tac.client.ProcessHandler;
+import com.tmall.wireless.tac.client.ProcessResult;
+
+public class SampleProcessor implements ProcessHandler {
+
+	public ProcessResult execute(ProcessContext context) throws Exception {
+		ProcessResult processResult = new ProcessResult();
+		System.out.print("Hello Word TAC!!!");
+		// TODO Auto-generated method stub
+		return processResult;
+	}
 
 }
