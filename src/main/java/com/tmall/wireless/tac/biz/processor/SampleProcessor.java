52f381f86e4fbb4b2a08c0b9e2a795a52b7dd2cf
tiongsong <changkun.hck@alibaba-inc.com>
2016-12-22 18:17:20 +0800

    update

--- src/main/java/com/tmall/wireless/tac/biz/processor/SampleProcessor.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/SampleProcessor.java
@@ -1,16 +1,35 @@
 package com.tmall.wireless.tac.biz.processor;
 
-import com.tmall.wireless.tac.client.ProcessContext;
-import com.tmall.wireless.tac.client.ProcessHandler;
-import com.tmall.wireless.tac.client.ProcessResult;
 
-public class SampleProcessor implements ProcessHandler {
+import com.tmall.wireless.tac.client.common.TacResult;
+import com.tmall.wireless.tac.client.domain.Context;
+import com.tmall.wireless.tac.client.handler.TacHandler;
 
-	public ProcessResult execute(ProcessContext context) throws Exception {
-		ProcessResult processResult = new ProcessResult();
-		System.out.print("Hello Word TAC!!!");
-		// TODO Auto-generated method stub
-		return processResult;
-	}
+import java.util.HashMap;
+import java.util.Map;
+
+public class SampleProcessor implements TacHandler {
+
+    public TacResult<Map<String, Object>> execute(Context context) throws Exception {
+
+        Map<String, Object> resutlMap = new HashMap<String, Object>();
+        TacResult<Map<String, Object>> tacResult = TacResult.newResult(resutlMap);
+
+        Map<String, Object> dataMap = new HashMap<String, Object>();
+        dataMap.put("userId", context.getUserInfo().getUserId());
+        dataMap.put("version", context.getDeviceInfo().getVersion());
+        dataMap.put("platform", context.getDeviceInfo().getPlatform());
+        dataMap.put("utdid", context.getDeviceInfo().getUtdid());
+
+        dataMap.put("imgUrl", "http://www.tmall.com/1.jpg");
+
+        dataMap.put("imgUrl2", "http://www.tmall.com/2.jpg");
+
+        resutlMap.put("data", dataMap);
+
+        tacResult.setData(resutlMap);
+
+        return tacResult;
+    }
 
 }
