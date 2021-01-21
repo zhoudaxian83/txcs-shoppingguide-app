560bf7ffb49ed6b2612d44996e861ca67e3de154
yangqing.byq <yangqing.byq@alibaba-inc.com>
2021-01-21 17:06:10 +0800

    init

--- src/main/java/com/tmall/wireless/tac/biz/processor/SampleProcessor.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/SampleProcessor.java
@@ -23,33 +23,33 @@ public class SampleProcessor implements TacHandler<List<Map<String, Object>>> {
 
 	public TacResult<List<Map<String, Object>>> execute(Context context) throws Exception {
         tacLogger.info("SampleProcessor execute start...");
-        // Step1: 获取上下文参数
-		// 1、获取用户信息
-		long userId = context.getUserInfo().getUserId();       //用户id
-        String userNick = context.getUserInfo().getNick();     // 用户nick
-		// 2、获取设备信息
-		String ip  = context.getDeviceInfo().getIp();           // 设备ip
-        String channel = context.getDeviceInfo().getChannel() ; // 渠道
-        String platform = context.getDeviceInfo().getPlatform();// 平台
-        String version = context.getDeviceInfo().getVersion();  // 版本
-        String utdid = context.getDeviceInfo().getUtdid();      // utdid
-        // 3、获取自定义参数
-        String smAreaId = String.valueOf(context.get("330100")); // 获取商超areaId
-
-        // Step2: 业务逻辑
-        SiteInfoQueryDO siteInfoQueryDO = new SiteInfoQueryDO();
-        siteInfoQueryDO.setSmAreaId(smAreaId);
-        // 调用商超HSF获取城市站信息
-        //Result<Map<String, Object>> result = nativeIndexService.getSiteInfo(siteInfoQueryDO);
-
-        try {
-            int i=1/0;
-        } catch (Exception ex) {
-            // 异常日志记录
-            tacLogger.error("execute error:",ex);
-        }
-
-		List<Map<String,Object>> resultList = new ArrayList<Map<String,Object>>();
+//        // Step1: 获取上下文参数
+//		// 1、获取用户信息
+//		long userId = context.getUserInfo().getUserId();       //用户id
+//        String userNick = context.getUserInfo().getNick();     // 用户nick
+//		// 2、获取设备信息
+//		String ip  = context.getDeviceInfo().getIp();           // 设备ip
+//        String channel = context.getDeviceInfo().getChannel() ; // 渠道
+//        String platform = context.getDeviceInfo().getPlatform();// 平台
+//        String version = context.getDeviceInfo().getVersion();  // 版本
+//        String utdid = context.getDeviceInfo().getUtdid();      // utdid
+//        // 3、获取自定义参数
+//        String smAreaId = String.valueOf(context.get("330100")); // 获取商超areaId
+//
+//        // Step2: 业务逻辑
+//        SiteInfoQueryDO siteInfoQueryDO = new SiteInfoQueryDO();
+//        siteInfoQueryDO.setSmAreaId(smAreaId);
+//        // 调用商超HSF获取城市站信息
+//        //Result<Map<String, Object>> result = nativeIndexService.getSiteInfo(siteInfoQueryDO);
+//
+//        try {
+//            int i=1/0;
+//        } catch (Exception ex) {
+//            // 异常日志记录
+//            tacLogger.error("execute error:",ex);
+//        }
+//
+//		List<Map<String,Object>> resultList = new ArrayList<Map<String,Object>>();
         Map<String,Object> dataMap = new HashMap<String, Object>();
         dataMap.put("imgUrl", "http://www.tmall.com/1.jpg");
         dataMap.put("imgUrl2", "http://www.tmall.com/2.jpg");
