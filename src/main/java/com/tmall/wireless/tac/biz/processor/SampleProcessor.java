568d36207b0d7660deb36dc97aa9e4f5d5efdfe7
yangqing.byq <yangqing.byq@alibaba-inc.com>
2021-01-21 21:01:44 +0800

    add

--- src/main/java/com/tmall/wireless/tac/biz/processor/SampleProcessor.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/SampleProcessor.java
@@ -4,10 +4,12 @@ import com.tmall.recommend.biz.RpmReactiveHandler;
 import com.tmall.recommend.biz.model.context.RpmRequestContext;
 import com.tmall.wireless.tac.client.common.TacResult;
 import io.reactivex.Flowable;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Map;
 
+@Component
 public class SampleProcessor extends RpmReactiveHandler<List<Map<String, Object>>> {
 
     @Override
