889811b4fc12f31b473c1f76b1665b5a43c4d03e
yangqing.byq <yangqing.byq@alibaba-inc.com>
2021-01-22 13:46:15 +0800

    fix

--- src/main/java/com/tmall/wireless/tac/biz/processor/SampleProcessor.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/SampleProcessor.java
@@ -2,19 +2,26 @@ package com.tmall.wireless.tac.biz.processor;
 
 import com.tmall.recommend.biz.RpmReactiveHandler;
 import com.tmall.recommend.biz.model.context.RpmRequestContext;
+import com.tmall.recommend.framework.TestService;
 import com.tmall.wireless.tac.client.common.TacResult;
 import io.reactivex.Flowable;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
 import java.util.List;
 import java.util.Map;
 
 @Component
-public class SampleProcessor extends RpmReactiveHandler<List<Map<String, Object>>> {
+public class SampleProcessor extends RpmReactiveHandler<String> {
 
+    @Autowired
+    TestService testService;
 
     @Override
-    public Flowable<TacResult<List<Map<String, Object>>>> rpmExecuteFlowable(RpmRequestContext rpmRequestContext) throws Exception {
-        return Flowable.empty();
+    public Flowable<TacResult<String>> rpmExecuteFlowable(RpmRequestContext rpmRequestContext) throws Exception {
+
+        return Flowable.just(TacResult.newResult(testService.colaTest()));
+
     }
 }
