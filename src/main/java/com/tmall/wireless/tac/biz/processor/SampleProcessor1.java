f57422e1b4c573c3cf48c7024236a7e50a3f3170
yangqing.byq <yangqing.byq@alibaba-inc.com>
2021-01-24 13:08:23 +0800

    fix spring xml

---
+++ src/main/java/com/tmall/wireless/tac/biz/processor/SampleProcessor1.java
@@ -0,0 +1,24 @@
+package com.tmall.wireless.tac.biz.processor;
+
+import com.tmall.recommend.biz.RpmReactiveHandler;
+import com.tmall.recommend.biz.model.context.RpmRequestContext;
+import com.tmall.recommend.framework.TestService;
+import com.tmall.wireless.tac.client.common.TacResult;
+import io.reactivex.Flowable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SampleProcessor1 extends RpmReactiveHandler<String> {
+
+    @Autowired
+    TestService testService;
+
+    @Override
+    public Flowable<TacResult<String>> rpmExecuteFlowable(RpmRequestContext rpmRequestContext) throws Exception {
+
+        Object scenario = rpmRequestContext.getParamMap().get("scenario");
+        return Flowable.just(TacResult.newResult(testService.colaTest(scenario == null ? "" : scenario.toString())));
+
+    }
+}
