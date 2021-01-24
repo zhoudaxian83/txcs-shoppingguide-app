5d0ce110e034f46a8ae0f7c2f17e856730a5ea97
yangqing.byq <yangqing.byq@alibaba-inc.com>
2021-01-24 16:40:41 +0800

    init

---
+++ src/main/java/com/tmall/wireless/tac/biz/processor/DefaultRanderExtPt.java
@@ -0,0 +1,16 @@
+package com.tmall.wireless.tac.biz.processor;
+
+import com.alibaba.cola.extension.Extension;
+import com.tmall.recommend.framework.ItemRanderExtPt;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by yangqing.byq on 2021/1/24.
+ */
+@Component
+public class DefaultRanderExtPt implements ItemRanderExtPt<Object> {
+    @Override
+    public Object renderItems(Object o) {
+        return "default";
+    }
+}
