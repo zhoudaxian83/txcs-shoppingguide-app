d1057f1e64ef30f801b9860c3faec71601757f28
yangqing.byq <yangqing.byq@alibaba-inc.com>
2021-01-24 14:32:01 +0800

    add AppColaBootstrap

---
+++ src/main/java/com/tmall/wireless/tac/biz/processor/AppColaBootstrap.java
@@ -0,0 +1,74 @@
+package com.tmall.wireless.tac.biz.processor;
+
+import com.alibaba.cola.boot.*;
+import com.alibaba.cola.exception.framework.ColaException;
+import com.alibaba.cola.extension.ExtensionPointI;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.commons.collections.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+/**
+ * Created by yangqing.byq on 2021/1/24.
+ */
+
+public class AppColaBootstrap extends Bootstrap {
+
+
+    @Autowired
+    @Getter
+    Set<ExtensionPointI> appExtPts;
+    @Getter
+    @Setter
+    private List<String> packages;
+    private ClassPathScanHandler handler;
+
+    @Autowired
+    private RegisterFactory registerFactory;
+
+
+    public void init() {
+        Set<Class<?>> classSet = scanConfiguredPackages();
+        registerBeans(classSet);
+        if (CollectionUtils.isNotEmpty(appExtPts)) {
+            appExtPts.forEach(pt -> registerBeans(appExtPts.stream().map(Object::getClass).collect(Collectors.toSet())));
+        }
+    }
+
+    /**
+     * @param classSet
+     */
+    private void registerBeans(Set<Class<?>> classSet) {
+        for (Class<?> targetClz : classSet) {
+            RegisterI register = registerFactory.getRegister(targetClz);
+            if (null != register) {
+                register.doRegistration(targetClz);
+            }
+        }
+
+    }
+
+    /**
+     * Scan the packages configured in Spring xml
+     *
+     * @return
+     */
+    private Set<Class<?>> scanConfiguredPackages() {
+        if (packages == null) throw new ColaException("Command packages is not specified");
+
+        String[] pkgs = new String[packages.size()];
+        handler = new ClassPathScanHandler(packages.toArray(pkgs));
+
+        Set<Class<?>> classSet = new TreeSet<>(new ClassNameComparator());
+        for (String pakName : packages) {
+            classSet.addAll(handler.getPackageAllClasses(pakName, true));
+        }
+        return classSet;
+    }
+}
