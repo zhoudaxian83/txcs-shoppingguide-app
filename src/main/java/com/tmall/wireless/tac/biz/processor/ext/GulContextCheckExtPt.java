0c99c28a9a2b57e37239781e21ccb1cb51f1ce7d
yangqing.byq <yangqing.byq@alibaba-inc.com>
2021-02-13 19:25:37 +0800

    add context check

--- src/main/java/com/tmall/wireless/tac/biz/processor/ext/GulContextCheckExtPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/ext/GulContextCheckExtPt.java
@@ -1,14 +1,17 @@
 package com.tmall.wireless.tac.biz.processor.ext;
 
+import com.alibaba.cola.extension.Extension;
 import com.tmall.txcs.gs.framework.extensions.paramcheck.ContextCheckExtPt;
 import com.tmall.txcs.gs.framework.extensions.paramcheck.ContextCheckResult;
 import com.tmall.txcs.gs.framework.model.SgFrameworkContext;
 import com.tmall.txcs.gs.framework.model.SgFrameworkContextMix;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by yangqing.byq on 2021/2/13.
  */
-
+@Extension
+@Service
 public class GulContextCheckExtPt implements ContextCheckExtPt<SgFrameworkContextMix> {
 
     @Override
