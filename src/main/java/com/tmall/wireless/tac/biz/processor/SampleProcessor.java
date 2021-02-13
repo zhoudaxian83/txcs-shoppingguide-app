9903cefe293ac7dd3f7b67deb7ad045cae47dfad
yangqing.byq <yangqing.byq@alibaba-inc.com>
2021-02-13 20:35:32 +0800

    add

--- src/main/java/com/tmall/wireless/tac/biz/processor/SampleProcessor.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/SampleProcessor.java
@@ -1,27 +1,20 @@
 package com.tmall.wireless.tac.biz.processor;
 
 import com.alibaba.cola.extension.ExtensionPointI;
-import com.alibaba.fastjson.JSON;
-
-
 import com.tmall.txcs.gs.base.RpmReactiveHandler;
 import com.tmall.txcs.gs.framework.model.EntityVO;
 import com.tmall.txcs.gs.framework.model.SgFrameworkContextMix;
 import com.tmall.txcs.gs.framework.model.SgFrameworkResponse;
 import com.tmall.txcs.gs.framework.service.impl.SgFrameworkServiceMix;
 import com.tmall.txcs.gs.model.biz.context.SceneInfo;
-import com.tmall.wireless.tac.biz.processor.ext.GulContextCheckExtPt;
 import com.tmall.wireless.tac.client.common.TacResult;
 import com.tmall.wireless.tac.client.domain.Context;
 import io.reactivex.Flowable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Component
 public class SampleProcessor extends RpmReactiveHandler<String> {
