faa5bb68873673e7029d21a0cc851050331c2a8f
yangqing.byq <yangqing.byq@alibaba-inc.com>
2021-02-13 12:29:53 +0800

    add bootstrap

--- src/main/java/com/tmall/wireless/tac/biz/processor/SampleProcessor.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/SampleProcessor.java
@@ -4,11 +4,16 @@ import com.alibaba.fastjson.JSON;
 
 
 import com.tmall.txcs.gs.base.RpmReactiveHandler;
+import com.tmall.txcs.gs.framework.model.EntityVO;
+import com.tmall.txcs.gs.framework.model.SgFrameworkContextMix;
+import com.tmall.txcs.gs.framework.model.SgFrameworkResponse;
+import com.tmall.txcs.gs.framework.service.impl.SgFrameworkServiceMix;
 import com.tmall.wireless.tac.client.common.TacResult;
 import com.tmall.wireless.tac.client.domain.Context;
 import io.reactivex.Flowable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -18,10 +23,15 @@ public class SampleProcessor extends RpmReactiveHandler<String> {
 
 
 
-
+    @Autowired
+    SgFrameworkServiceMix sgFrameworkServiceMix;
 
     @Override
     public Flowable<TacResult<String>> executeFlowable(Context context) throws Exception {
+        SgFrameworkContextMix sgFrameworkContextMix = new SgFrameworkContextMix();
+
+        SgFrameworkResponse<EntityVO> sgFrameworkResponse = sgFrameworkServiceMix.recommend(sgFrameworkContextMix);
+
         return Flowable.just(TacResult.newResult(
                 "success"
         ));
