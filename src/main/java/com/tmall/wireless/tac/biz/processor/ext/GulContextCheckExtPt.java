baa8e05d90332abb7316af80b1f4e6ab54c72f0d
yangqing.byq <yangqing.byq@alibaba-inc.com>
2021-02-17 19:18:07 +0800

    add extension

--- src/main/java/com/tmall/wireless/tac/biz/processor/ext/GulContextCheckExtPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/ext/GulContextCheckExtPt.java
@@ -5,12 +5,15 @@ import com.tmall.txcs.gs.framework.extensions.paramcheck.ContextCheckExtPt;
 import com.tmall.txcs.gs.framework.extensions.paramcheck.ContextCheckResult;
 import com.tmall.txcs.gs.framework.model.SgFrameworkContext;
 import com.tmall.txcs.gs.framework.model.SgFrameworkContextMix;
+import com.tmall.txcs.gs.framework.model.constant.ScenarioConstant;
 import org.springframework.stereotype.Service;
 
 /**
  * Created by yangqing.byq on 2021/2/13.
  */
-@Extension(bizId = "txcs", useCase = "B2C", scenario = "gul")
+@Extension(bizId = ScenarioConstant.ENTITY_TYPE_ITEM,
+        useCase = ScenarioConstant.BIZ_TYPE_B2C,
+        scenario = "gul")
 @Service
 public class GulContextCheckExtPt implements ContextCheckExtPt<SgFrameworkContextMix> {
 
