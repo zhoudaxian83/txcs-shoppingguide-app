498c16d7874fea41ae7dbb63155241ec177c4c12
yangqing.byq <yangqing.byq@alibaba-inc.com>
2021-02-27 17:43:30 +0800

    add

--- src/main/java/com/tmall/wireless/tac/biz/processor/iteminfo/dto/ItemInfoBySourceDTOZhaoshang.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/iteminfo/dto/ItemInfoBySourceDTOZhaoshang.java
@@ -2,7 +2,6 @@ package com.tmall.wireless.tac.biz.processor.iteminfo.dto;
 
 import com.tmall.txcs.gs.framework.support.itemInfo.bysource.ItemInfoBySourceDTO;
 import lombok.Data;
-import org.springframework.stereotype.Service;
 
 /**
  * Created by yangqing.byq on 2021/2/26.
