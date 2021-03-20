df3bc9162ed182c128b60d62aac693d383798c5d
yangqing.byq <yangqing.byq@alibaba-inc.com>
2021-03-20 18:14:44 +0800

    add

--- src/main/java/com/tmall/wireless/tac/biz/processor/browsrec/BrowseRecommendScene.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/browsrec/BrowseRecommendScene.java
@@ -1,6 +1,7 @@
 package com.tmall.wireless.tac.biz.processor.browsrec;
 
 import com.alibaba.fastjson.JSON;
+import com.google.common.collect.Lists;
 import com.tmall.txcs.biz.supermarket.scene.UserParamsKeyConstant;
 import com.tmall.txcs.biz.supermarket.scene.util.CsaUtil;
 import com.tmall.txcs.biz.supermarket.scene.util.MapUtil;
@@ -9,6 +10,9 @@ import com.tmall.txcs.gs.framework.model.EntityVO;
 import com.tmall.txcs.gs.framework.model.SgFrameworkContextItem;
 import com.tmall.txcs.gs.framework.model.SgFrameworkResponse;
 import com.tmall.txcs.gs.framework.model.constant.ScenarioConstant;
+import com.tmall.txcs.gs.framework.model.meta.ItemGroupMetaInfo;
+import com.tmall.txcs.gs.framework.model.meta.ItemInfoSourceMetaInfo;
+import com.tmall.txcs.gs.framework.model.meta.ItemMetaInfo;
 import com.tmall.txcs.gs.framework.service.impl.SgFrameworkServiceItem;
 import com.tmall.txcs.gs.model.biz.context.PageInfoDO;
 import com.tmall.txcs.gs.model.biz.context.SceneInfo;
@@ -22,6 +26,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -57,7 +62,7 @@ public class BrowseRecommendScene {
         sgFrameworkContextItem.setUserDO(userDO);
 
         sgFrameworkContextItem.setLocParams(CsaUtil.parseCsaObj(context.get(UserParamsKeyConstant.USER_PARAMS_KEY_CSA), smAreaId));
-        sgFrameworkContextItem.setItemMetaInfo(MetaInfoUtil.getBrowseRecommendItemMetaInfo());
+        sgFrameworkContextItem.setItemMetaInfo(this.getBrowseRecommendItemMetaInfo());
 
 
         PageInfoDO pageInfoDO = new PageInfoDO();
@@ -69,5 +74,43 @@ public class BrowseRecommendScene {
                 .map(TacResult::newResult)
                 .onErrorReturn(r -> TacResult.errorResult(""));
 
+    }
+
+
+    public static ItemMetaInfo getBrowseRecommendItemMetaInfo() {
+        ItemMetaInfo itemMetaInfo = new ItemMetaInfo();
+        List<ItemGroupMetaInfo> itemGroupMetaInfoList = Lists.newArrayList();
+        List<ItemInfoSourceMetaInfo> itemInfoSourceMetaInfoList = Lists.newArrayList();
+        ItemGroupMetaInfo itemGroupMetaInfo = new ItemGroupMetaInfo();
+        itemGroupMetaInfoList.add(itemGroupMetaInfo);
+        itemGroupMetaInfo.setGroupName("sm_B2C");
+        itemGroupMetaInfo.setItemInfoSourceMetaInfos(itemInfoSourceMetaInfoList);
+        ItemGroupMetaInfo itemGroupMetaInfo1 = new ItemGroupMetaInfo();
+        itemGroupMetaInfoList.add(itemGroupMetaInfo1);
+        itemGroupMetaInfo1.setGroupName("sm_O2OOneHour");
+        itemGroupMetaInfo1.setItemInfoSourceMetaInfos(itemInfoSourceMetaInfoList);
+        ItemGroupMetaInfo itemGroupMetaInfo2 = new ItemGroupMetaInfo();
+        itemGroupMetaInfoList.add(itemGroupMetaInfo2);
+        itemGroupMetaInfo2.setGroupName("sm_O2OHalfDay");
+        itemGroupMetaInfo2.setItemInfoSourceMetaInfos(itemInfoSourceMetaInfoList);
+        ItemGroupMetaInfo itemGroupMetaInfo3 = new ItemGroupMetaInfo();
+        itemGroupMetaInfoList.add(itemGroupMetaInfo3);
+        itemGroupMetaInfo3.setGroupName("sm_O2ONextDay");
+        itemGroupMetaInfo3.setItemInfoSourceMetaInfos(itemInfoSourceMetaInfoList);
+        ItemInfoSourceMetaInfo itemInfoSourceMetaInfoCaptain = new ItemInfoSourceMetaInfo();
+        itemInfoSourceMetaInfoCaptain.setSourceName("captain");
+        itemInfoSourceMetaInfoList.add(itemInfoSourceMetaInfoCaptain);
+        itemMetaInfo.setItemGroupRenderInfoList(itemGroupMetaInfoList);
+        ItemInfoSourceMetaInfo itemInfoSourceMetaInfoTpp = new ItemInfoSourceMetaInfo();
+        itemInfoSourceMetaInfoTpp.setSourceName("tpp");
+        itemInfoSourceMetaInfoList.add(itemInfoSourceMetaInfoTpp);
+
+
+        ItemInfoSourceMetaInfo itemInfoSourceMetaInfoTest = new ItemInfoSourceMetaInfo();
+        itemInfoSourceMetaInfoTest.setSourceName("test");
+        itemInfoSourceMetaInfoList.add(itemInfoSourceMetaInfoTest);
+
+        itemMetaInfo.setItemGroupRenderInfoList(itemGroupMetaInfoList);
+        return itemMetaInfo;
     }
 }
