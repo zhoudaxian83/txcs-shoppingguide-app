4bae35b83e62a8aac8b3b61ab807d01470d11c67
yangqing.byq <yangqing.byq@alibaba-inc.com>
2021-03-20 17:49:09 +0800

    add

---
+++ src/main/java/com/tmall/wireless/tac/biz/processor/browsrec/BrowseRecommendScene.java
@@ -0,0 +1,73 @@
+package com.tmall.wireless.tac.biz.processor.browsrec;
+
+import com.alibaba.fastjson.JSON;
+import com.tmall.txcs.biz.supermarket.scene.UserParamsKeyConstant;
+import com.tmall.txcs.biz.supermarket.scene.util.CsaUtil;
+import com.tmall.txcs.biz.supermarket.scene.util.MapUtil;
+import com.tmall.txcs.biz.supermarket.scene.util.MetaInfoUtil;
+import com.tmall.txcs.gs.framework.model.EntityVO;
+import com.tmall.txcs.gs.framework.model.SgFrameworkContextItem;
+import com.tmall.txcs.gs.framework.model.SgFrameworkResponse;
+import com.tmall.txcs.gs.framework.model.constant.ScenarioConstant;
+import com.tmall.txcs.gs.framework.service.impl.SgFrameworkServiceItem;
+import com.tmall.txcs.gs.model.biz.context.PageInfoDO;
+import com.tmall.txcs.gs.model.biz.context.SceneInfo;
+import com.tmall.txcs.gs.model.biz.context.UserDO;
+import com.tmall.wireless.tac.client.common.TacResult;
+import com.tmall.wireless.tac.client.domain.Context;
+import com.tmall.wireless.tac.client.domain.UserInfo;
+import io.reactivex.Flowable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+/**
+ * Created by yangqing.byq on 2021/3/17.
+ */
+@Service
+public class BrowseRecommendScene {
+    Logger LOGGER = LoggerFactory.getLogger(BrowseRecommendScene.class);
+
+    @Autowired
+    SgFrameworkServiceItem sgFrameworkServiceItem;
+
+    public Flowable<TacResult<SgFrameworkResponse<EntityVO>>> recommend(Context context) {
+
+        LOGGER.error("ITEM_REQUEST:{}", JSON.toJSONString(context));
+
+
+        Long smAreaId = MapUtil.getLongWithDefault(context.getParams(), "smAreaId", 330100L);
+
+        SgFrameworkContextItem sgFrameworkContextItem = new SgFrameworkContextItem();
+
+        sgFrameworkContextItem.setRequestParams(context.getParams());
+
+        SceneInfo sceneInfo = new SceneInfo();
+        sceneInfo.setBiz(ScenarioConstant.BIZ_TYPE_SUPERMARKET);
+        sceneInfo.setSubBiz(ScenarioConstant.LOC_TYPE_B2C);
+        sceneInfo.setScene(ScenarioConstant.SCENARIO_GUL_BROWSE_RECOMMEND);
+        sgFrameworkContextItem.setSceneInfo(sceneInfo);
+
+        UserDO userDO = new UserDO();
+        userDO.setUserId(Optional.of(context).map(Context::getUserInfo).map(UserInfo::getUserId).orElse(0L));
+        userDO.setNick(Optional.of(context).map(Context::getUserInfo).map(UserInfo::getNick).orElse(""));
+        sgFrameworkContextItem.setUserDO(userDO);
+
+        sgFrameworkContextItem.setLocParams(CsaUtil.parseCsaObj(context.get(UserParamsKeyConstant.USER_PARAMS_KEY_CSA), smAreaId));
+        sgFrameworkContextItem.setItemMetaInfo(MetaInfoUtil.getBrowseRecommendItemMetaInfo());
+
+
+        PageInfoDO pageInfoDO = new PageInfoDO();
+        pageInfoDO.setIndex(0);
+        pageInfoDO.setPageSize(20);
+        sgFrameworkContextItem.setUserPageInfo(pageInfoDO);
+
+        return sgFrameworkServiceItem.recommend(sgFrameworkContextItem)
+                .map(TacResult::newResult)
+                .onErrorReturn(r -> TacResult.errorResult(""));
+
+    }
+}
