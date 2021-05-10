b09e12dd60f0afc897202534b15968370d824ca2
guijian <maxiang.ma@alibaba-inc.com>
2021-05-10 21:13:30 +0800

    首页改版-代码完善

--- src/main/java/com/tmall/wireless/tac/biz/processor/firstScreenMind/FirstScreenMindContentInfoQueryExtPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/firstScreenMind/FirstScreenMindContentInfoQueryExtPt.java
@@ -44,7 +44,7 @@ public class FirstScreenMindContentInfoQueryExtPt implements ContentInfoQueryExt
     @Override
     public Flowable<Response<Map<Long, ContentDTO>>> process(ContentInfoQueryRequest contentInfoQueryRequest) {
 
-        tacLogger.info("***FirstScreenMindContentInfoQueryExtPt contentInfoQueryRequest***:"+contentInfoQueryRequest);
+        tacLogger.info("***********FirstScreenMindContentInfoQueryExtPt contentInfoQueryRequest*******:"+contentInfoQueryRequest);
         /*场景详情缓存前缀*/
         String sceneLabelDetail = "txcs_scene_detail_v1";
         String pKey = sceneLabelDetail;
@@ -55,8 +55,12 @@ public class FirstScreenMindContentInfoQueryExtPt implements ContentInfoQueryExt
             for (ContentEntity contentEntity : contentEntities) {
                 sKeyList.add(String.valueOf(contentEntity.getContentId()));
             }
+            tacLogger.info("***********FirstScreenMindContentInfoQueryExtPt labelSceneNamespace*******:"+labelSceneNamespace);
+            tacLogger.info("***********FirstScreenMindContentInfoQueryExtPt pKey*******:"+pKey);
+            tacLogger.info("***********FirstScreenMindContentInfoQueryExtPt sKeyList*******:"+sKeyList);
             Result<Map<Object, Result<DataEntry>>> labelSceneResult =
                     multiClusterTairManager.prefixGets(labelSceneNamespace, pKey, sKeyList);
+            tacLogger.info("***********FirstScreenMindContentInfoQueryExtPt labelSceneResult*******:"+labelSceneResult);
             if (!labelSceneResult.isSuccess()) {
                 return Flowable.just(Response.fail(""));
             }
@@ -79,6 +83,7 @@ public class FirstScreenMindContentInfoQueryExtPt implements ContentInfoQueryExt
                 contentDTO.setContentInfo((Map<String, Object>) dataEntry.getValue());
                 contentDTOMap.put(((Long) sKey),contentDTO);
             }
+            tacLogger.info("***********FirstScreenMindContentInfoQueryExtPt contentDTOMap*******:"+contentDTOMap);
             //结果判空
             if (MapUtils.isEmpty(contentDTOMap)) {
                 LOGGER.info(RenderErrorEnum.contentBatchTairValueNull.getCode(), RenderErrorEnum.contentBatchTairValueNull.getMessage());
