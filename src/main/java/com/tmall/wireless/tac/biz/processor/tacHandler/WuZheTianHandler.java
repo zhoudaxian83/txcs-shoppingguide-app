5ff57fd3e43fdf038d3b510dad36aa589e2bfe60
wb-ljc587473 <wb-ljc587473@alibaba-inc.com>
2021-05-13 21:16:44 +0800

    test

--- src/main/java/com/tmall/wireless/tac/biz/processor/tacHandler/WuZheTianHandler.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/tacHandler/WuZheTianHandler.java
@@ -1,9 +1,9 @@
 package com.tmall.wireless.tac.biz.processor.tacHandler;
 
+import com.tmall.txcs.biz.supermarket.scene.gul.GulSubTabScene;
 import com.tmall.txcs.gs.base.RpmReactiveHandler;
 import com.tmall.txcs.gs.framework.model.EntityVO;
 import com.tmall.txcs.gs.framework.model.SgFrameworkResponse;
-import com.tmall.wireless.tac.biz.processor.wzt.WuZheTianRecommendScene;
 import com.tmall.wireless.tac.client.common.TacResult;
 import com.tmall.wireless.tac.client.domain.Context;
 import io.reactivex.Flowable;
@@ -18,10 +18,10 @@ import org.springframework.stereotype.Component;
 public class WuZheTianHandler extends RpmReactiveHandler<SgFrameworkResponse<EntityVO>> {
 
     @Autowired
-    WuZheTianRecommendScene wuZheTianRecommendScene;
+    GulSubTabScene gulSubTabScene;
 
     @Override
     public Flowable<TacResult<SgFrameworkResponse<EntityVO>>> executeFlowable(Context context) throws Exception {
-        return wuZheTianRecommendScene.recommend(context);
+        return gulSubTabScene.recommend(context);
     }
 }
