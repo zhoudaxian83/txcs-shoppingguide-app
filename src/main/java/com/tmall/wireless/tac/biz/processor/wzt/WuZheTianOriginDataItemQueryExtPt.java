361ee246435ed4a753cf66e8518d1dd82ae57453
wb-ljc587473 <wb-ljc587473@alibaba-inc.com>
2021-05-14 14:24:16 +0800

    test

--- src/main/java/com/tmall/wireless/tac/biz/processor/wzt/WuZheTianOriginDataItemQueryExtPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/wzt/WuZheTianOriginDataItemQueryExtPt.java
@@ -1,14 +1,24 @@
 package com.tmall.wireless.tac.biz.processor.wzt;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.alibaba.cola.extension.Extension;
 
+import com.google.common.collect.Lists;
 import com.tmall.txcs.gs.framework.extensions.origindata.OriginDataDTO;
 import com.tmall.txcs.gs.framework.extensions.origindata.OriginDataItemQueryExtPt;
 import com.tmall.txcs.gs.framework.model.SgFrameworkContextItem;
 import com.tmall.txcs.gs.model.model.dto.ItemEntity;
 import com.tmall.wireless.tac.biz.processor.common.ScenarioConstantApp;
+import com.tmall.wireless.tac.biz.processor.firstpage.banner.iteminfo.model.BannerItemDTO;
+import com.tmall.wireless.tac.biz.processor.firstpage.banner.iteminfo.uitl.BannerUtil;
 import com.tmall.wireless.tac.client.dataservice.TacLogger;
 import io.reactivex.Flowable;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.MapUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,12 +34,25 @@ public class WuZheTianOriginDataItemQueryExtPt implements OriginDataItemQueryExt
     @Autowired
     TacLogger tacLogger;
 
-
-
     @Override
     public Flowable<OriginDataDTO<ItemEntity>> process(SgFrameworkContextItem context) {
         tacLogger.info("WuZheTianOriginDataItemQueryExtPt");
         OriginDataDTO<ItemEntity> originDataDTO = new OriginDataDTO<>();
+        originDataDTO.setResult(buildItemList());
         return Flowable.just(originDataDTO);
+    }
+
+    private List<ItemEntity> buildItemList() {
+        List<ItemEntity> result = Lists.newArrayList();
+        ItemEntity itemEntity = new ItemEntity();
+        itemEntity.setItemId(123L);
+        itemEntity.setO2oType("TEST");
+        result.add(itemEntity);
+
+        ItemEntity itemEntity2 = new ItemEntity();
+        itemEntity2.setItemId(123L);
+        itemEntity2.setO2oType("TEST2");
+        result.add(itemEntity2);
+        return result;
     }
 }
