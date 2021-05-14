ed70fe813511171cf42c8154fd1f8678685a2b64
wb-ljc587473 <wb-ljc587473@alibaba-inc.com>
2021-05-14 12:55:07 +0800

    test

--- src/main/java/com/tmall/wireless/tac/biz/processor/wzt/WuZheTianOriginDataItemQueryExtPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/wzt/WuZheTianOriginDataItemQueryExtPt.java
@@ -1,20 +1,33 @@
 package com.tmall.wireless.tac.biz.processor.wzt;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import com.alibaba.cola.extension.Extension;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.tmall.txcs.biz.supermarket.extpt.origindata.ConvertUtil;
+import com.tmall.txcs.biz.supermarket.scene.util.MapUtil;
 import com.tmall.txcs.gs.framework.extensions.origindata.OriginDataDTO;
 import com.tmall.txcs.gs.framework.extensions.origindata.OriginDataItemQueryExtPt;
+import com.tmall.txcs.gs.framework.model.SgFrameworkContext;
 import com.tmall.txcs.gs.framework.model.SgFrameworkContextItem;
 import com.tmall.txcs.gs.model.model.dto.ItemEntity;
 import com.tmall.txcs.gs.model.model.dto.RecommendResponseEntity;
 import com.tmall.txcs.gs.model.model.dto.tpp.RecommendItemEntityDTO;
+import com.tmall.wireless.tac.biz.processor.common.RequestKeyConstantApp;
 import com.tmall.wireless.tac.biz.processor.common.ScenarioConstantApp;
+import com.tmall.wireless.tac.biz.processor.firstpage.banner.iteminfo.model.BannerItemDTO;
+import com.tmall.wireless.tac.biz.processor.firstpage.banner.iteminfo.uitl.BannerUtil;
 import com.tmall.wireless.tac.client.dataservice.TacLogger;
 import io.reactivex.Flowable;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.MapUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,27 +45,50 @@ public class WuZheTianOriginDataItemQueryExtPt implements OriginDataItemQueryExt
     @Autowired
     TacLogger tacLogger;
 
+    public static final String defaultBizType = "sm";
+
     @Override
-    public Flowable<OriginDataDTO<ItemEntity>> process(SgFrameworkContextItem context) {
-        tacLogger.info("WuZheTianOriginDataItemQueryExtPt");
+    public Flowable<OriginDataDTO<ItemEntity>> process(SgFrameworkContextItem sgFrameworkContextItem) {
+
         OriginDataDTO<ItemEntity> originDataDTO = new OriginDataDTO<>();
-        originDataDTO.setScm("test");
+
+        String bannerInfo = MapUtil.getStringWithDefault(
+            Optional.ofNullable(sgFrameworkContextItem).map(SgFrameworkContext::getRequestParams)
+                .orElse(Maps.newHashMap()),
+            RequestKeyConstantApp.BANNER_INFO,
+            "");
+
+        if (StringUtils.isEmpty(bannerInfo)) {
+            return Flowable.just(originDataDTO);
+        }
+
+        originDataDTO.setResult(buildItemList(bannerInfo));
         return Flowable.just(originDataDTO);
     }
 
-    private OriginDataDTO<ItemEntity> convert(RecommendResponseEntity<RecommendItemEntityDTO> recommendResponseEntity) {
-        OriginDataDTO<ItemEntity> originDataDTO = new OriginDataDTO<>();
+    private List<ItemEntity> buildItemList(String bannerInfo) {
+        List<ItemEntity> result = Lists.newArrayList();
+        Map<String, List<BannerItemDTO>> bannerIndex2ItemList = BannerUtil.parseBannerItem(bannerInfo);
 
-        originDataDTO.setHasMore(recommendResponseEntity.isHasMore());
-        originDataDTO.setIndex(recommendResponseEntity.getIndex());
-        originDataDTO.setPvid(recommendResponseEntity.getPvid());
-        originDataDTO.setScm(recommendResponseEntity.getScm());
-        originDataDTO.setTppBuckets(recommendResponseEntity.getTppBuckets());
+        if (MapUtils.isEmpty(bannerIndex2ItemList)) {
+            return result;
+        }
 
-        originDataDTO.setResult(recommendResponseEntity
-            .getResult()
-            .stream()
-            .filter(Objects::nonNull).map(ConvertUtil::convert).collect(Collectors.toList()));
-        return originDataDTO;
+        bannerIndex2ItemList.keySet().forEach(key -> {
+            List<BannerItemDTO> bannerItemDTOList = bannerIndex2ItemList.get(key);
+            if (CollectionUtils.isEmpty(bannerItemDTOList)) {
+                return;
+            }
+            List<ItemEntity> itemEntityList = bannerItemDTOList.stream().map(bannerItemDTO -> {
+                ItemEntity itemEntity = new ItemEntity();
+                itemEntity.setItemId(bannerItemDTO.getItemId());
+                itemEntity.setO2oType(bannerItemDTO.getLocType());
+                itemEntity.setBizType(defaultBizType);
+                return itemEntity;
+            }).collect(Collectors.toList());
+            result.addAll(itemEntityList);
+        });
+
+        return result;
     }
 }
