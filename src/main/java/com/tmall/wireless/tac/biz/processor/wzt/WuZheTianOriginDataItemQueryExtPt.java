16f65a8c87ae29fb362c0ad5b149b31797c594a7
wb-ljc587473 <wb-ljc587473@alibaba-inc.com>
2021-05-14 10:11:35 +0800

    test

--- src/main/java/com/tmall/wireless/tac/biz/processor/wzt/WuZheTianOriginDataItemQueryExtPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/wzt/WuZheTianOriginDataItemQueryExtPt.java
@@ -1,32 +1,48 @@
 package com.tmall.wireless.tac.biz.processor.wzt;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import com.alibaba.cola.extension.Extension;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.tmall.txcs.biz.supermarket.extpt.origindata.ConvertUtil;
+import com.tmall.txcs.biz.supermarket.extpt.origindata.DefaultOriginDataItemQueryExtPt;
 import com.tmall.txcs.biz.supermarket.iteminfo.source.captain.ItemInfoBySourceDTOMain;
 import com.tmall.txcs.biz.supermarket.scene.util.MapUtil;
 import com.tmall.txcs.gs.framework.extensions.buildvo.BuildItemVOExtPt;
 import com.tmall.txcs.gs.framework.extensions.buildvo.BuildItemVoRequest;
+import com.tmall.txcs.gs.framework.extensions.excutor.SgExtensionExecutor;
 import com.tmall.txcs.gs.framework.extensions.origindata.OriginDataDTO;
 import com.tmall.txcs.gs.framework.extensions.origindata.OriginDataItemQueryExtPt;
+import com.tmall.txcs.gs.framework.extensions.origindata.request.ItemOriginDataRequestExtPt;
 import com.tmall.txcs.gs.framework.model.ErrorCode;
 import com.tmall.txcs.gs.framework.model.ItemEntityVO;
 import com.tmall.txcs.gs.framework.model.SgFrameworkContext;
 import com.tmall.txcs.gs.framework.model.SgFrameworkContextItem;
 import com.tmall.txcs.gs.framework.model.constant.ScenarioConstant;
 import com.tmall.txcs.gs.model.Response;
+import com.tmall.txcs.gs.model.item.O2oType;
 import com.tmall.txcs.gs.model.model.dto.ItemEntity;
+import com.tmall.txcs.gs.model.model.dto.RecommendResponseEntity;
+import com.tmall.txcs.gs.model.model.dto.tpp.RecommendItemEntityDTO;
 import com.tmall.txcs.gs.model.spi.model.ItemInfoBySourceDTO;
 import com.tmall.txcs.gs.model.spi.model.ItemInfoDTO;
+import com.tmall.txcs.gs.model.spi.model.RecommendRequest;
+import com.tmall.txcs.gs.spi.recommend.RecommendSpi;
 import com.tmall.wireless.tac.biz.processor.common.RequestKeyConstantApp;
 import com.tmall.wireless.tac.biz.processor.common.ScenarioConstantApp;
 import com.tmall.wireless.tac.biz.processor.firstpage.banner.iteminfo.model.ItemInfoBySourceDTOInv;
 import io.reactivex.Flowable;
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -38,24 +54,58 @@ import org.springframework.stereotype.Service;
 @Service
 public class WuZheTianOriginDataItemQueryExtPt implements OriginDataItemQueryExtPt {
 
+
+    Logger LOGGER = LoggerFactory.getLogger(WuZheTianOriginDataItemQueryExtPt.class);
+
+    @Autowired
+    RecommendSpi recommendSpi;
+    @Autowired
+    private SgExtensionExecutor sgExtensionExecutor;
+
+    private static Map<String, String> tppO2oTypeConvertMap;
+
+    static {
+        tppO2oTypeConvertMap = Maps.newHashMap();
+        tppO2oTypeConvertMap.putIfAbsent("one_hour", O2oType.O2OOneHour.name());
+        tppO2oTypeConvertMap.putIfAbsent("half_day", O2oType.O2OHalfDay.name());
+        tppO2oTypeConvertMap.putIfAbsent("next_day", O2oType.O2ONextDay.name());
+        tppO2oTypeConvertMap.putIfAbsent("B2C", O2oType.B2C.name());
+    }
+
     @Override
-    public Flowable<OriginDataDTO<ItemEntity>> process(SgFrameworkContextItem sgFrameworkContextItem) {
+    public Flowable<OriginDataDTO<ItemEntity>> process(SgFrameworkContextItem context) {
+
+        RecommendRequest recommendRequest = sgExtensionExecutor.execute(
+            ItemOriginDataRequestExtPt.class,
+            context.getBizScenario(),
+            pt -> pt.process0(context));
+
+        return recommendSpi.recommendItem(recommendRequest)
+            .map(recommendResponseEntityResponse -> {
+                // tpp 返回失败
+                if (!recommendResponseEntityResponse.isSuccess()
+                    || recommendResponseEntityResponse.getValue() == null
+                    || CollectionUtils.isEmpty(recommendResponseEntityResponse.getValue().getResult())) {
+                    return new OriginDataDTO<>();
+                }
+                return convert(recommendResponseEntityResponse.getValue());
+            });
+    }
+
+
+    private OriginDataDTO<ItemEntity> convert(RecommendResponseEntity<RecommendItemEntityDTO> recommendResponseEntity) {
         OriginDataDTO<ItemEntity> originDataDTO = new OriginDataDTO<>();
 
-        String bannerInfo = MapUtil.getStringWithDefault(
-            Optional.ofNullable(sgFrameworkContextItem).map(SgFrameworkContext::getRequestParams)
-                .orElse(Maps.newHashMap()),
-            RequestKeyConstantApp.BANNER_INFO,
-            "");
+        originDataDTO.setHasMore(recommendResponseEntity.isHasMore());
+        originDataDTO.setIndex(recommendResponseEntity.getIndex());
+        originDataDTO.setPvid(recommendResponseEntity.getPvid());
+        originDataDTO.setScm(recommendResponseEntity.getScm());
+        originDataDTO.setTppBuckets(recommendResponseEntity.getTppBuckets());
 
-        if (StringUtils.isEmpty(bannerInfo)) {
-            return Flowable.just(originDataDTO);
-        }
-        List<ItemEntity> itemEntityList = Lists.newArrayList();
-        ItemEntity itemEntity = new ItemEntity();
-        itemEntity.setBizType("测试");
-        itemEntityList.add(itemEntity);
-        originDataDTO.setResult(itemEntityList);
-        return Flowable.just(originDataDTO);
+        originDataDTO.setResult(recommendResponseEntity
+            .getResult()
+            .stream()
+            .filter(Objects::nonNull).map(ConvertUtil::convert).collect(Collectors.toList()));
+        return originDataDTO;
     }
 }
