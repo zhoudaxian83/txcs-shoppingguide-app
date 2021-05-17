cd588a980e09a1ad1edbe81e9e421b030d234ae9
wb-ljc587473 <wb-ljc587473@alibaba-inc.com>
2021-05-17 11:30:16 +0800

    test

--- src/main/java/com/tmall/wireless/tac/biz/processor/wzt/WuZheTianOriginDataItemQueryExtPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/wzt/WuZheTianOriginDataItemQueryExtPt.java
@@ -3,9 +3,6 @@ package com.tmall.wireless.tac.biz.processor.wzt;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 import com.alibaba.cola.extension.Extension;
 import com.alibaba.fastjson.JSON;
@@ -14,12 +11,9 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.taobao.tair.DataEntry;
 import com.taobao.tair.Result;
-import com.tmall.txcs.biz.supermarket.extpt.origindata.ConvertUtil;
-import com.tmall.txcs.biz.supermarket.scene.util.MapUtil;
 import com.tmall.txcs.gs.framework.extensions.excutor.SgExtensionExecutor;
 import com.tmall.txcs.gs.framework.extensions.origindata.OriginDataDTO;
 import com.tmall.txcs.gs.framework.extensions.origindata.OriginDataItemQueryExtPt;
-import com.tmall.txcs.gs.framework.model.SgFrameworkContext;
 import com.tmall.txcs.gs.framework.model.SgFrameworkContextItem;
 import com.tmall.txcs.gs.model.Response;
 import com.tmall.txcs.gs.model.item.O2oType;
@@ -29,15 +23,9 @@ import com.tmall.txcs.gs.model.model.dto.tpp.RecommendItemEntityDTO;
 import com.tmall.txcs.gs.model.spi.model.RecommendRequest;
 import com.tmall.txcs.gs.spi.recommend.RecommendSpi;
 import com.tmall.txcs.gs.spi.recommend.TairFactorySpi;
-import com.tmall.wireless.tac.biz.processor.common.RequestKeyConstantApp;
 import com.tmall.wireless.tac.biz.processor.common.ScenarioConstantApp;
-import com.tmall.wireless.tac.biz.processor.firstpage.banner.iteminfo.model.BannerItemDTO;
-import com.tmall.wireless.tac.biz.processor.firstpage.banner.iteminfo.uitl.BannerUtil;
 import com.tmall.wireless.tac.client.dataservice.TacLogger;
 import io.reactivex.Flowable;
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.collections.MapUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -66,107 +54,62 @@ public class WuZheTianOriginDataItemQueryExtPt implements OriginDataItemQueryExt
     @Autowired
     private SgExtensionExecutor sgExtensionExecutor;
 
-    public static final String defaultBizType = "sm";
-
     @Override
-    public Flowable<OriginDataDTO<ItemEntity>> process(SgFrameworkContextItem sgFrameworkContextItem) {
-
+    public Flowable<OriginDataDTO<ItemEntity>> process(SgFrameworkContextItem context) {
+        /**
+         * 1、tair获取商品列表
+         * 2、tpp渲染个性化排序
+         * 3、排序商品存入tair供下次使用
+         * 4、获取前20作为当前页数据
+         * 5、查询限购信息
+         * 6、captain获取商品数据
+         * 7、处理过滤逻辑
+         * 8、转换为vo给前端展示
+         *
+         */
+        tacLogger.info("[WuZheTianOriginDataItemQueryExtPt] context={}" + JSON.toJSONString(context));
         OriginDataDTO<ItemEntity> originDataDTO = new OriginDataDTO<>();
+        originDataDTO.setResult(buildItemList());
 
-        String bannerInfo = MapUtil.getStringWithDefault(
-            Optional.ofNullable(sgFrameworkContextItem).map(SgFrameworkContext::getRequestParams)
-                .orElse(Maps.newHashMap()),
-            RequestKeyConstantApp.BANNER_INFO,
-            "");
-        tacLogger.info("WuZheTianOriginDataItemQueryExtPt-test1");
-        originDataDTO.setResult(buildItemList(bannerInfo));
+        //获取商品排期列表
+        List<String> sKeyList = new ArrayList<>();
+        sKeyList.add("wuZheTian_HD_pre");
+        sKeyList.add("wuZheTian_HB_pre");
+        sKeyList.add("wuZheTian_HN_pre");
+        sKeyList.add("wuZheTian_HZ_pre");
+        sKeyList.add("wuZheTian_XN_pre");
+        Result<List<DataEntry>> mgetResult = tairFactorySpi.getOriginDataFailProcessTair().getMultiClusterTairManager()
+            .mget(labelSceneNamespace, sKeyList);
+        tacLogger.info("[WuZheTianOriginDataItemQueryExtPt] mgetResult=" + JSON.toJSONString(mgetResult));
+
+        //tpp获取个性化排序规则
+        RecommendRequest recommendRequest = new RecommendRequest();
+        Flowable<Response<RecommendResponseEntity<RecommendItemEntityDTO>>> responseFlowable = recommendSpi
+            .recommendItem(recommendRequest);
+
+        //获取限购信息
+        //ItemLimitInfoQuery itemLimitInfoQuery = new ItemLimitInfoQuery();
+        //itemLimitInfoQuery.setUserId(0L);
+        //itemLimitInfoQuery.setItemIdList(Arrays.asList(600819862645L, 623789407071L));
+        //ItemLimitResult itemLimitResult = todayCrazyLimitFacade.query(itemLimitInfoQuery);
+        //
+        //tacLogger.info("[WuZheTianOriginDataItemQueryExtPt] itemLimitResult=" + JSON.toJSONString(itemLimitResult));
         return Flowable.just(originDataDTO);
     }
 
-    private List<ItemEntity> buildItemList(String bannerInfo) {
+    private List<ItemEntity> buildItemList() {
         List<ItemEntity> result = Lists.newArrayList();
-        List<ItemEntity> itemEntityList = Lists.newArrayList();
         ItemEntity itemEntity = new ItemEntity();
         itemEntity.setItemId(591228976713L);
         itemEntity.setO2oType("B2C");
-        itemEntity.setBizType(defaultBizType);
-        result.addAll(itemEntityList);
+        itemEntity.setBizType("sm");
+        result.add(itemEntity);
+
+        ItemEntity itemEntity2 = new ItemEntity();
+        itemEntity2.setItemId(591228976713L);
+        itemEntity2.setO2oType("B2C");
+        itemEntity2.setBizType("sm");
+        result.add(itemEntity2);
         return result;
     }
-
-    //@Override
-    //public Flowable<OriginDataDTO<ItemEntity>> process(SgFrameworkContextItem context) {
-    //    /**
-    //     * 1、tair获取商品列表
-    //     * 2、tpp渲染个性化排序
-    //     * 3、排序商品存入tair供下次使用
-    //     * 4、获取前20作为当前页数据
-    //     * 5、查询限购信息
-    //     * 6、captain获取商品数据
-    //     * 7、处理过滤逻辑
-    //     * 8、转换为vo给前端展示
-    //     *
-    //     */
-    //    tacLogger.info("[WuZheTianOriginDataItemQueryExtPt] context={}" + JSON.toJSONString(context));
-    //    OriginDataDTO<ItemEntity> originDataDTO = new OriginDataDTO<>();
-    //    originDataDTO.setResult(buildItemList());
-    //
-    //    //获取商品排期列表
-    //    List<String> sKeyList = new ArrayList<>();
-    //    sKeyList.add("wuZheTian_HD_pre");
-    //    sKeyList.add("wuZheTian_HB_pre");
-    //    sKeyList.add("wuZheTian_HN_pre");
-    //    sKeyList.add("wuZheTian_HZ_pre");
-    //    sKeyList.add("wuZheTian_XN_pre");
-    //    Result<List<DataEntry>> mgetResult = tairFactorySpi.getOriginDataFailProcessTair()
-    //    .getMultiClusterTairManager()
-    //        .mget(labelSceneNamespace, sKeyList);
-    //    tacLogger.info("[WuZheTianOriginDataItemQueryExtPt] mgetResult=" + JSON.toJSONString(mgetResult));
-    //
-    //    //tpp获取个性化排序规则
-    //    RecommendRequest recommendRequest = new RecommendRequest();
-    //    Flowable<Response<RecommendResponseEntity<RecommendItemEntityDTO>>> responseFlowable = recommendSpi
-    //        .recommendItem(recommendRequest);
-    //
-    //    //获取限购信息
-    //    //ItemLimitInfoQuery itemLimitInfoQuery = new ItemLimitInfoQuery();
-    //    //itemLimitInfoQuery.setUserId(0L);
-    //    //itemLimitInfoQuery.setItemIdList(Arrays.asList(600819862645L, 623789407071L));
-    //    //ItemLimitResult itemLimitResult = todayCrazyLimitFacade.query(itemLimitInfoQuery);
-    //    //
-    //    //tacLogger.info("[WuZheTianOriginDataItemQueryExtPt] itemLimitResult=" + JSON.toJSONString(itemLimitResult));
-    //    return Flowable.just(originDataDTO);
-    //}
-    //
-    //
-    //private OriginDataDTO<ItemEntity> convert(RecommendResponseEntity<RecommendItemEntityDTO>
-    // recommendResponseEntity) {
-    //    OriginDataDTO<ItemEntity> originDataDTO = new OriginDataDTO<>();
-    //
-    //    originDataDTO.setHasMore(recommendResponseEntity.isHasMore());
-    //    originDataDTO.setIndex(recommendResponseEntity.getIndex());
-    //    originDataDTO.setPvid(recommendResponseEntity.getPvid());
-    //    originDataDTO.setScm(recommendResponseEntity.getScm());
-    //    originDataDTO.setTppBuckets(recommendResponseEntity.getTppBuckets());
-    //
-    //    originDataDTO.setResult(recommendResponseEntity
-    //        .getResult()
-    //        .stream()
-    //        .filter(Objects::nonNull).map(ConvertUtil::convert).collect(Collectors.toList()));
-    //    return originDataDTO;
-    //}
-    //
-    //private List<ItemEntity> buildItemList() {
-    //    List<ItemEntity> result = Lists.newArrayList();
-    //    ItemEntity itemEntity = new ItemEntity();
-    //    itemEntity.setItemId(123L);
-    //    itemEntity.setO2oType("TEST");
-    //    result.add(itemEntity);
-    //
-    //    ItemEntity itemEntity2 = new ItemEntity();
-    //    itemEntity2.setItemId(12L);
-    //    itemEntity2.setO2oType("TEST2");
-    //    result.add(itemEntity2);
-    //    return result;
-    //}
 }
