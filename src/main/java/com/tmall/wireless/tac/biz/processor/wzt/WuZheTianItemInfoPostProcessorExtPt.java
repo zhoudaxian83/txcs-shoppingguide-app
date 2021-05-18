1a6e91f31d843eb438b1c68b6f8c176abcab451a
wb-ljc587473 <wb-ljc587473@alibaba-inc.com>
2021-05-18 20:39:42 +0800

    test

--- src/main/java/com/tmall/wireless/tac/biz/processor/wzt/WuZheTianItemInfoPostProcessorExtPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/wzt/WuZheTianItemInfoPostProcessorExtPt.java
@@ -1,9 +1,13 @@
 package com.tmall.wireless.tac.biz.processor.wzt;
 
+import com.alibaba.fastjson.JSON;
+
 import com.tmall.txcs.gs.framework.extensions.itemdatapost.ItemInfoPostProcessorExtPt;
 import com.tmall.txcs.gs.framework.extensions.itemdatapost.ItemInfoPostProcessorResp;
 import com.tmall.txcs.gs.framework.model.SgFrameworkContextItem;
 import com.tmall.txcs.gs.model.Response;
+import com.tmall.wireless.tac.client.dataservice.TacLogger;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * @Author: luoJunChong
@@ -12,8 +16,13 @@ import com.tmall.txcs.gs.model.Response;
  */
 public class WuZheTianItemInfoPostProcessorExtPt implements ItemInfoPostProcessorExtPt {
 
+    @Autowired
+    TacLogger tacLogger;
+
     @Override
     public Response<ItemInfoPostProcessorResp> process(SgFrameworkContextItem sgFrameworkContextItem) {
-        return null;
+        tacLogger.info("ItemInfoPostProcessorExtPt扩展点测试=" + JSON.toJSONString(sgFrameworkContextItem));
+        ItemInfoPostProcessorResp itemInfoPostProcessorResp = new ItemInfoPostProcessorResp();
+        return Response.success(itemInfoPostProcessorResp);
     }
 }
