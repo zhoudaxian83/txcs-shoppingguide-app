0818c9180d261bfacef5a318a334ed976860670e
wb-ljc587473 <wb-ljc587473@alibaba-inc.com>
2021-05-18 20:41:30 +0800

    test

--- src/main/java/com/tmall/wireless/tac/biz/processor/wzt/WuZheTianItemInfoPostProcessorExtPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/wzt/WuZheTianItemInfoPostProcessorExtPt.java
@@ -1,19 +1,26 @@
 package com.tmall.wireless.tac.biz.processor.wzt;
 
+import com.alibaba.cola.extension.Extension;
 import com.alibaba.fastjson.JSON;
 
 import com.tmall.txcs.gs.framework.extensions.itemdatapost.ItemInfoPostProcessorExtPt;
 import com.tmall.txcs.gs.framework.extensions.itemdatapost.ItemInfoPostProcessorResp;
 import com.tmall.txcs.gs.framework.model.SgFrameworkContextItem;
 import com.tmall.txcs.gs.model.Response;
+import com.tmall.wireless.tac.biz.processor.common.ScenarioConstantApp;
 import com.tmall.wireless.tac.client.dataservice.TacLogger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * @Author: luoJunChong
  * @Date: 2021/5/18 18:57
  * description:
  */
+@Extension(bizId = ScenarioConstantApp.BIZ_TYPE_SUPERMARKET,
+    useCase = ScenarioConstantApp.LOC_TYPE_B2C,
+    scenario = ScenarioConstantApp.WU_ZHE_TIAN)
+@Service
 public class WuZheTianItemInfoPostProcessorExtPt implements ItemInfoPostProcessorExtPt {
 
     @Autowired
