2489ddea41c18346616b339151cb6df303581096
wb-ljc587473 <wb-ljc587473@alibaba-inc.com>
2021-05-18 21:08:21 +0800

    test

--- src/main/java/com/tmall/wireless/tac/biz/processor/wzt/WuZheTianItemInfoPostProcessorExtPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/wzt/WuZheTianItemInfoPostProcessorExtPt.java
@@ -3,9 +3,13 @@ package com.tmall.wireless.tac.biz.processor.wzt;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.alibaba.boot.hsf.annotation.HSFConsumer;
 import com.alibaba.cola.extension.Extension;
 import com.alibaba.fastjson.JSON;
 
+import com.tmall.aself.shoppingguide.client.todaycrazyv2.TodayCrazyLimitFacade;
+import com.tmall.aself.shoppingguide.client.todaycrazyv2.query.ItemLimitInfoQuery;
+import com.tmall.aself.shoppingguide.client.todaycrazyv2.result.ItemLimitResult;
 import com.tmall.txcs.gs.framework.extensions.itemdatapost.ItemInfoPostProcessorExtPt;
 import com.tmall.txcs.gs.framework.extensions.itemdatapost.ItemInfoPostProcessorResp;
 import com.tmall.txcs.gs.framework.model.SgFrameworkContextItem;
@@ -34,9 +38,19 @@ public class WuZheTianItemInfoPostProcessorExtPt implements ItemInfoPostProcesso
     @Autowired
     TacLogger tacLogger;
 
+    @HSFConsumer(serviceVersion = "1.0.0")
+    private TodayCrazyLimitFacade todayCrazyLimitFacade;
+
     @Override
     public Response<ItemInfoPostProcessorResp> process(SgFrameworkContextItem sgFrameworkContextItem) {
         tacLogger.info("ItemInfoPostProcessorExtPt扩展点测试=" + JSON.toJSONString(sgFrameworkContextItem));
+
+        ItemLimitInfoQuery itemLimitInfoQuery = new ItemLimitInfoQuery();
+        itemLimitInfoQuery.setUserId(1681359525L);
+        itemLimitInfoQuery.addSku(600819862645L, 623789407071L);
+        ItemLimitResult itemLimitResult = todayCrazyLimitFacade.query(itemLimitInfoQuery);
+        tacLogger.info("itemLimitInfoQuery返回结果=" + JSON.toJSONString(itemLimitResult));
+
         Map<String, Object> paramsValue = new HashMap<>(16);
         paramsValue.put("name", "itemLimitInfoQuery");
         paramsValue.put("type", "com.tmall.aself.shoppingguide.client.todaycrazyv2.query.ItemLimitInfoQuery");
