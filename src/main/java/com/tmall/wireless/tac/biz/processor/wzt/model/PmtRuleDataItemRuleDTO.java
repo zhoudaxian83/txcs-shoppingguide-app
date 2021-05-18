ef935db35c4d653ba4a04b5ce3cfff9e3df313c3
wb-ljc587473 <wb-ljc587473@alibaba-inc.com>
2021-05-18 23:00:57 +0800

    test

--- src/main/java/com/tmall/wireless/tac/biz/processor/wzt/model/PmtRuleDataItemRuleDTO.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/wzt/model/PmtRuleDataItemRuleDTO.java
@@ -1,5 +1,6 @@
 package com.tmall.wireless.tac.biz.processor.wzt.model;
 
+import java.io.Serializable;
 import java.util.List;
 
 import lombok.Data;
@@ -9,7 +10,7 @@ import lombok.Data;
  * @Date: 2021/5/17 19:05
  */
 @Data
-public class PmtRuleDataItemRuleDTO {
+public class PmtRuleDataItemRuleDTO implements Serializable {
     private static final long serialVersionUID = -1L;
     private ColumnCenterPmtRuleDataSetDTO pmtRuleDataSetDTO;
     private List<ColumnCenterDataSetItemRuleDTO> dataSetItemRuleDTOList;
