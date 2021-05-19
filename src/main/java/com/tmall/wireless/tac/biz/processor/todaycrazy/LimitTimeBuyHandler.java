e805aaad66a7fe5b9449c8b3b56c6568d87bc09b
guijian <maxiang.ma@alibaba-inc.com>
2021-05-19 15:05:46 +0800

    限时抢购

--- src/main/java/com/tmall/wireless/tac/biz/processor/todaycrazy/LimitTimeBuyHandler.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/todaycrazy/LimitTimeBuyHandler.java
@@ -4,6 +4,7 @@ import com.tmall.txcs.gs.base.RpmReactiveHandler;
 import com.tmall.txcs.gs.framework.model.EntityVO;
 import com.tmall.txcs.gs.framework.model.SgFrameworkResponse;
 import com.tmall.wireless.tac.client.common.TacResult;
+import com.tmall.wireless.tac.client.dataservice.TacLogger;
 import com.tmall.wireless.tac.client.domain.Context;
 import io.reactivex.Flowable;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +15,13 @@ public class LimitTimeBuyHandler extends RpmReactiveHandler {
 
     @Autowired
     LimitTimeBuyScene limitTimeBuyScene;
+    @Autowired
+    TacLogger tacLogger;
 
     @Override
     public Flowable<TacResult<SgFrameworkResponse<EntityVO>>> executeFlowable(Context context) throws Exception {
+        tacLogger.info("***LimitTimeBuyHandler context.getParams()****:"+context.getParams());
+        tacLogger.info("***LimitTimeBuyHandler context.getParams().toString()****:"+context.getParams().toString());
         return limitTimeBuyScene.recommend(context);
     }
 }
