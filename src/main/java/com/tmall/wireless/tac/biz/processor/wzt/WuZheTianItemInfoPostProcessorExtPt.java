762a24d0669ebfc23567815117fa5a75800ad9a3
wb-ljc587473 <wb-ljc587473@alibaba-inc.com>
2021-05-19 17:37:27 +0800

    test

--- src/main/java/com/tmall/wireless/tac/biz/processor/wzt/WuZheTianItemInfoPostProcessorExtPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/wzt/WuZheTianItemInfoPostProcessorExtPt.java
@@ -10,6 +10,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 
 import com.ali.com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.tmall.txcs.biz.supermarket.scene.util.MapUtil;
 import com.tmall.txcs.gs.framework.extensions.itemdatapost.ItemInfoPostProcessorExtPt;
@@ -55,20 +56,21 @@ public class WuZheTianItemInfoPostProcessorExtPt implements ItemInfoPostProcesso
     }
 
     private Map<String, Object> buildGetItemLimitResult(SgFrameworkContextItem sgFrameworkContextItem) {
+        //获取itemId和sku
+
         List<Long> items = sgFrameworkContextItem.getItemEntityOriginDataDTO().getResult().stream().map(item -> {
             return item.getItemId();
         }).collect(Collectors.toList());
         Long userId = MapUtil.getLongWithDefault(sgFrameworkContextItem.getRequestParams(), "userId", 0L);
         Map<String, Object> paramsValue = new HashMap<>(16);
         Map paramMap = Maps.newHashMap();
-
         paramMap.put("userId", userId);
-        //List<Map> skuList = Lists.newArrayList();
-        //Map skuMap = Maps.newHashMap();
-        //skuMap.put("skuId", 4637368768647L);
-        //skuMap.put("itemId", 643897236869L);
-        //skuList.add(skuMap);
-        paramMap.put("itemIdList", Joiner.on(",").join(items));
+        List<Map> skuList = Lists.newArrayList();
+        Map skuMap = Maps.newHashMap();
+        skuMap.put("skuId", 4637368768647L);
+        skuMap.put("itemId", 643897236869L);
+        skuList.add(skuMap);
+        paramMap.put("itemIdList", skuList);
         paramsValue.put("itemLimitInfoQuery", paramMap);
         return paramsValue;
     }
