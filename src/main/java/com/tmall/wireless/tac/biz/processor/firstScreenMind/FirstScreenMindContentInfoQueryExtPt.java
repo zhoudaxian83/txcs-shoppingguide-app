a474d761e05ad5f068c46e137eefbaa066a15a4f
yangqing.byq <yangqing.byq@alibaba-inc.com>
2021-05-19 20:21:24 +0800

    updata ContentInfoQueryExtPt

--- src/main/java/com/tmall/wireless/tac/biz/processor/firstScreenMind/FirstScreenMindContentInfoQueryExtPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/firstScreenMind/FirstScreenMindContentInfoQueryExtPt.java
@@ -12,13 +12,17 @@ import com.tmall.aselfcommon.model.scene.domain.TairSceneDTO;
 import com.tmall.aselfcommon.model.scene.valueobject.SceneDetailValue;
 import com.tmall.txcs.gs.framework.extensions.content.ContentInfoQueryExtPt;
 import com.tmall.txcs.gs.framework.extensions.content.ContentInfoQueryRequest;
+import com.tmall.txcs.gs.framework.extensions.origindata.OriginDataDTO;
+import com.tmall.txcs.gs.framework.model.SgFrameworkContextContent;
 import com.tmall.txcs.gs.model.Response;
 import com.tmall.txcs.gs.model.content.ContentDTO;
+import com.tmall.txcs.gs.model.content.ContentInfoDTO;
 import com.tmall.txcs.gs.model.model.dto.ContentEntity;
 import com.tmall.txcs.gs.model.model.dto.ItemEntity;
 import com.tmall.txcs.gs.model.spi.model.ItemInfoDTO;
 import com.tmall.txcs.gs.spi.recommend.TairFactorySpi;
 import com.tmall.wireless.tac.biz.processor.common.ScenarioConstantApp;
+import com.tmall.wireless.tac.biz.processor.firstScreenMind.common.ContentInfoSupport;
 import com.tmall.wireless.tac.biz.processor.firstScreenMind.enums.FrontBackMapEnum;
 import com.tmall.wireless.tac.biz.processor.firstScreenMind.enums.RenderContentTypeEnum;
 import com.tmall.wireless.tac.biz.processor.firstScreenMind.enums.RenderErrorEnum;
@@ -56,6 +60,101 @@ public class FirstScreenMindContentInfoQueryExtPt implements ContentInfoQueryExt
     private static final int labelSceneNamespace = 184;
 
     @Override
+    public Flowable<Response<Map<Long, ContentInfoDTO>>> process(SgFrameworkContextContent sgFrameworkContextContent) {
+        /*场景详情缓存前缀*/
+        String pKey = "txcs_scene_detail_v2";
+        Map<Long, ContentInfoDTO> contentDTOMap = Maps.newHashMap();
+        try {
+            List<ContentEntity> contentEntities  = Optional.of(sgFrameworkContextContent).map(SgFrameworkContextContent::getContentEntityOriginDataDTO).map(OriginDataDTO::getResult).orElse(Lists.newArrayList());
+
+            List<String> sKeyList = new ArrayList<>();
+            for (ContentEntity contentEntity : contentEntities) {
+                sKeyList.add(pKey + "_" + contentEntity.getContentId());
+            }
+            Result<List<DataEntry>> mgetResult =tairFactorySpi.getOriginDataFailProcessTair().getMultiClusterTairManager().mget(labelSceneNamespace, sKeyList);
+            tacLogger.info("***********FirstScreenMindContentInfoQueryExtPt mgetResult*******:"+mgetResult.toString());
+            if (!mgetResult.isSuccess() || CollectionUtils.isEmpty(mgetResult.getValue())) {
+                return Flowable.just(Response.fail(""));
+            }
+            List<DataEntry> dataEntryList = mgetResult.getValue();
+            Map<Long, TairSceneDTO> tairResult = Maps.newHashMap();
+            //循环遍历获取结果
+            dataEntryList.forEach(dataEntry -> {
+                // txcs_scene_detail_v2_2020053172349
+                Object tairKey = dataEntry.getKey();
+                String tairKeyStr = String.valueOf(tairKey);
+                String[] s = tairKeyStr.split("_");
+                String contentId = s[s.length - 1];
+                TairSceneDTO value = (TairSceneDTO) dataEntry.getValue();
+                tairResult.put(Long.valueOf(contentId), value);
+            });;
+            for(ContentEntity contentEntity : contentEntities){
+                Long contentId = contentEntity.getContentId();
+                TairSceneDTO tairSceneDTO = tairResult.get(contentId);
+                /**如果内容后台返回的补全内容为空，那么把这个内容过滤掉，并且日志记录*/
+                if(!tairResult.containsKey(contentId) || tairSceneDTO == null){
+                    tacLogger.info("批量补全内容中心信息返回为空contentId:" + contentId +",tairResult:"+tairResult);
+                    continue;
+                }
+                ContentInfoDTO contentDTO = new ContentInfoDTO();
+//                contentDTO.setContentId(contentId);
+//                contentDTO.setContentEntity(contentEntity);
+                Map<String, Object> contentInfo = Maps.newHashMap();
+                contentInfo.put("contentId",tairSceneDTO.getId());
+                contentInfo.put("contentTitle",tairSceneDTO.getTitle());
+                contentInfo.put("contentSubtitle",tairSceneDTO.getSubtitle());
+                contentInfo.put("itemSetIds", getItemSetIds(tairSceneDTO));
+                Map<String, Object> tairPropertyMap = tairSceneDTO.getProperty();
+                //前后端映射
+                for(FrontBackMapEnum frontBackMapEnum : FrontBackMapEnum.values()){
+                    contentInfo.put(frontBackMapEnum.getFront(),tairPropertyMap.get(frontBackMapEnum.getBack()));
+                }
+                /**内容类型*/
+                String type = GcsSceneType.of(tairSceneDTO.getType()).name();
+                String marketChannel = GcsMarketChannel.of(tairSceneDTO.getMarketChannel()).name();
+                /**后台没有类型，那么就直接返回普通场景打底*/
+                if(RenderCheckUtil.StringEmpty(type) || RenderCheckUtil.StringEmpty(marketChannel)){
+                    contentInfo.put("contentType",RenderContentTypeEnum.getBottomContentType());
+                }
+                //b2c普通场景
+                if(marketChannel.equals(GcsMarketChannel.B2C.name()) && type.equals(GcsSceneType.NORMAL.name())){
+                    contentInfo.put("contentType",RenderContentTypeEnum.b2cNormalContent.getType());
+                    //b2c组合场景
+                }else if(marketChannel.equals(GcsMarketChannel.B2C.name()) && type.equals(GcsSceneType.COMBINE.name())){
+                    contentInfo.put("contentType",RenderContentTypeEnum.b2cCombineContent.getType());
+                    contentInfo.put("subContentModelList",buildSubContentBaseInfoV2(contentInfo,tairSceneDTO));
+                    //b2c品牌场景
+                }else if(marketChannel.equals(GcsMarketChannel.B2C.name()) && type.equals(GcsSceneType.BRAND.name())){
+                    contentInfo.put("contentType",RenderContentTypeEnum.b2cBrandContent.getType());
+                    //o2o组合场景
+                }else if(marketChannel.equals(GcsMarketChannel.O2O.name()) && type.equals(GcsSceneType.NORMAL.name())){
+                    contentInfo.put("contentType",RenderContentTypeEnum.o2oNormalContent.getType());
+                    //o2o品牌场景
+                }else if(marketChannel.equals(GcsMarketChannel.O2O.name()) && type.equals(GcsSceneType.COMBINE.name())){
+                    contentInfo.put("contentType",RenderContentTypeEnum.o2oCombineContent.getType());
+                    contentInfo.put("subContentModelList",buildSubContentBaseInfoV2(contentInfo,tairSceneDTO));
+                    //o2o品牌场景
+                }else if(marketChannel.equals(GcsMarketChannel.O2O.name()) && type.equals(GcsSceneType.BRAND.name())){
+                    contentInfo.put("contentType",RenderContentTypeEnum.o2oBrandContent.getType());
+                } else if (type.equals(GcsSceneType.RECIPE.name())) {
+                    contentInfo.put("contentType",RenderContentTypeEnum.recipeContent.getType());
+                } else if (type.equals(GcsSceneType.MEDIA.name())) {
+                    contentInfo.put("contentType",RenderContentTypeEnum.mediaContent.getType());
+                } else {
+                    //默认打底-普通场景
+                    contentInfo.put("contentType",RenderContentTypeEnum.getBottomContentType());
+                }
+
+                contentDTO.setContentInfo(contentInfo);
+                contentDTOMap.put(contentId,contentDTO);
+            }
+        }catch (Exception e){
+            LOGGER.info(RenderErrorEnum.contentBatchTairExc.getCode(), RenderErrorEnum.contentBatchTairExc.getMessage());
+            return Flowable.just(Response.fail(RenderErrorEnum.contentBatchTairExc.getCode()));
+        }
+        tacLogger.info("****FirstScreenMindContentInfoQueryExtPt contentDTOMap*****:"+contentDTOMap.toString());
+        return Flowable.just(Response.success(contentDTOMap));
+    }
     public Flowable<Response<Map<Long, ContentDTO>>> process(ContentInfoQueryRequest contentInfoQueryRequest) {
 
         tacLogger.info("***********FirstScreenMindContentInfoQueryExtPt contentInfoQueryRequest*******:"+contentInfoQueryRequest);
@@ -186,6 +285,7 @@ public class FirstScreenMindContentInfoQueryExtPt implements ContentInfoQueryExt
         }
         return subContentModelList;
     }
+
 
 
 }
