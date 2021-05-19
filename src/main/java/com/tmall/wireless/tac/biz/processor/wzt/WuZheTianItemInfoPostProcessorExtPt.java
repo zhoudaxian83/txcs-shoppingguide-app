7decfec1e6741854f7faaeadab25bfad0959176c
1 <haixiao.zhx@alibaba-inc.com>
2021-05-19 12:02:32 +0800

    done

--- src/main/java/com/tmall/wireless/tac/biz/processor/wzt/WuZheTianItemInfoPostProcessorExtPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/wzt/WuZheTianItemInfoPostProcessorExtPt.java
@@ -4,6 +4,7 @@ import com.alibaba.cola.extension.Extension;
 import com.alibaba.fastjson.JSON;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.tmall.aself.shoppingguide.client.todaycrazyv2.query.ItemLimitInfoQuery;
 import com.tmall.txcs.gs.framework.extensions.itemdatapost.ItemInfoPostProcessorExtPt;
 import com.tmall.txcs.gs.framework.extensions.itemdatapost.ItemInfoPostProcessorResp;
 import com.tmall.txcs.gs.framework.model.SgFrameworkContextItem;
@@ -39,16 +40,16 @@ public class WuZheTianItemInfoPostProcessorExtPt implements ItemInfoPostProcesso
     public Response<ItemInfoPostProcessorResp> process(SgFrameworkContextItem sgFrameworkContextItem) {
         tacLogger.info("ItemInfoPostProcessorExtPt扩展点测试=" + JSON.toJSONString(sgFrameworkContextItem));
 
+        ItemLimitInfoQuery itemLimitInfoQuery = new ItemLimitInfoQuery();
 
         Map<String, Object> paramsValue = new HashMap<>(16);
         Map paramMap = Maps.newHashMap();
         paramsValue.put("itemLimitInfoQuery",paramMap);
-
         paramMap.put("userId",1681359525L);
         List<Map> skuList = Lists.newArrayList();
         Map skuMap = Maps.newHashMap();
-        skuMap.put("skuId",623789407071L);
-        skuMap.put("itemId",600819862645L);
+        skuMap.put("skuId",4637368768647L);
+        skuMap.put("itemId",643897236869L);
         skuList.add(skuMap);
         paramMap.put("itemIdList",skuList);
 
