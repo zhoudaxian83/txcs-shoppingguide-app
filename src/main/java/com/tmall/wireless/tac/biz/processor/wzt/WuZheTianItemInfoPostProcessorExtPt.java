9badbe6b86452ea2f49b73be637bdb394983b3cd
wb-ljc587473 <wb-ljc587473@alibaba-inc.com>
2021-05-19 15:21:37 +0800

    test

--- src/main/java/com/tmall/wireless/tac/biz/processor/wzt/WuZheTianItemInfoPostProcessorExtPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/wzt/WuZheTianItemInfoPostProcessorExtPt.java
@@ -6,8 +6,8 @@ import java.util.Map;
 
 import com.alibaba.cola.extension.Extension;
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.tmall.txcs.gs.framework.extensions.itemdatapost.ItemInfoPostProcessorExtPt;
@@ -41,7 +41,6 @@ public class WuZheTianItemInfoPostProcessorExtPt implements ItemInfoPostProcesso
     @Override
     public Response<ItemInfoPostProcessorResp> process(SgFrameworkContextItem sgFrameworkContextItem) {
         tacLogger.info("ItemInfoPostProcessorExtPt扩展点测试=" + JSON.toJSONString(sgFrameworkContextItem));
-
         Map<String, Object> paramsValue = new HashMap<>(16);
         Map paramMap = Maps.newHashMap();
         paramsValue.put("itemLimitInfoQuery", paramMap);
@@ -56,9 +55,10 @@ public class WuZheTianItemInfoPostProcessorExtPt implements ItemInfoPostProcesso
         try {
             tacLogger.info("测试返回结果begin");
             Object o = rpcSpi.invokeHsf("todayCrazyLimit", paramsValue);
-            ItemLimitResult itemLimitResult = (ItemLimitResult)o;
-            tacLogger.info("测试返回结果=" + JSON.toJSONString(itemLimitResult));
-            tacLogger.info("测试返回结果=" + JSON.toJSONString(o));
+            // java中Object转JsonObject
+            JSONObject jsonObject = (JSONObject)JSON.toJSON(o);
+            tacLogger.info("1-测试返回结果=" + JSON.toJSONString(jsonObject));
+            tacLogger.info("2-测试返回结果=" + JSON.toJSONString(o));
         } catch (Exception e) {
             tacLogger.error("测试返回结果-异常", e);
             e.printStackTrace();
