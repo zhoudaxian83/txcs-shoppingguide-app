05928cb1391a47085a132ced380d947c6df2165a
wb-ljc587473 <wb-ljc587473@alibaba-inc.com>
2021-05-19 13:49:01 +0800

    test

--- src/main/java/com/tmall/wireless/tac/biz/processor/wzt/model/ItemLimitResult.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/wzt/model/ItemLimitResult.java
@@ -3,7 +3,6 @@ package com.tmall.wireless.tac.biz.processor.wzt.model;
 import java.util.List;
 import java.util.Map;
 
-import com.tmall.aself.shoppingguide.client.ResultDTO;
 import lombok.Data;
 
 @Data
