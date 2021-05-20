47819d19f0b91e02b93dfa3757ad5c6376d1cd5e
guijian <maxiang.ma@alibaba-inc.com>
2021-05-20 23:00:22 +0800

    限时抢购

--- src/main/java/com/tmall/wireless/tac/biz/processor/firstScreenMind/FirstScreenMindContentOriginDataRequestExPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/firstScreenMind/FirstScreenMindContentOriginDataRequestExPt.java
@@ -1,9 +1,14 @@
 package com.tmall.wireless.tac.biz.processor.firstScreenMind;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import com.alibaba.cola.extension.Extension;
-import com.google.common.base.Joiner;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
+import com.ali.com.google.common.base.Joiner;
+import com.ali.com.google.common.collect.Lists;
+import com.ali.com.google.common.collect.Maps;
+import com.taobao.vipserver.commons.collections.MapUtils;
 import com.tmall.txcs.biz.supermarket.scene.util.MapUtil;
 import com.tmall.txcs.gs.framework.extensions.origindata.request.ContentOriginDataRequestExtPt;
 import com.tmall.txcs.gs.framework.model.SgFrameworkContext;
@@ -13,6 +18,7 @@ import com.tmall.txcs.gs.framework.model.meta.ContentRecommendMetaInfo;
 import com.tmall.txcs.gs.model.biz.context.LocParams;
 import com.tmall.txcs.gs.model.biz.context.PageInfoDO;
 import com.tmall.txcs.gs.model.biz.context.UserDO;
+import com.tmall.txcs.gs.model.constant.MtopContextKey;
 import com.tmall.txcs.gs.model.constant.RpmContants;
 import com.tmall.txcs.gs.model.spi.model.RecommendRequest;
 import com.tmall.wireless.tac.biz.processor.common.RequestKeyConstantApp;
@@ -21,11 +27,6 @@ import com.tmall.wireless.tac.client.dataservice.TacLogger;
 import com.tmall.wireless.tac.client.domain.Enviroment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Extension(bizId = ScenarioConstantApp.BIZ_TYPE_SUPERMARKET,
         useCase = ScenarioConstantApp.LOC_TYPE_B2C,
@@ -42,60 +43,77 @@ public class FirstScreenMindContentOriginDataRequestExPt implements ContentOrigi
         boolean isMind = isMind(sgFrameworkContextContent.getRequestParams());
 
         ContentRecommendMetaInfo contentRecommendMetaInfo = Optional.of(sgFrameworkContextContent)
-                .map(SgFrameworkContextContent::getContentMetaInfo)
-                .map(ContentMetaInfo::getContentRecommendMetaInfo)
-                .orElse(null);
-        tacLogger.info("****FirstScreenMindContentOriginDataRequestExPt sgFrameworkContextContent***:"+sgFrameworkContextContent.toString());
+            .map(SgFrameworkContextContent::getContentMetaInfo)
+            .map(ContentMetaInfo::getContentRecommendMetaInfo)
+            .orElse(null);
+        tacLogger.info(
+            "****FirstScreenMindContentOriginDataRequestExPt sgFrameworkContextContent***:" + sgFrameworkContextContent
+                .toString());
 
         Map<String, Object> requestParams = sgFrameworkContextContent.getRequestParams();
-        if(requestParams == null || requestParams.isEmpty()){
+        if (requestParams == null || requestParams.isEmpty()) {
             return null;
         }
 
         List<Long> contentSetIdList = getContentSetIdList(requestParams);
-
 
         if (isMind) {
             RecommendRequest tppRequest = new RecommendRequest();
             Map<String, String> params = Maps.newHashMap();
             tppRequest.setParams(params);
             tppRequest.setLogResult(true);
-            tppRequest.setUserId(Optional.of(sgFrameworkContextContent).map(SgFrameworkContext::getUserDO).map(UserDO::getUserId).orElse(0L));
+            tppRequest.setUserId(Optional.of(sgFrameworkContextContent).map(SgFrameworkContext::getUserDO)
+                .map(UserDO::getUserId).orElse(0L));
             tppRequest.setAppId(25379L);
             contentSetIdList = getMindContentSetIdList(requestParams);// 新版本的内容集id
-            List<String> newContentSetIdList = contentSetIdList.stream().map(id -> "intelligentCombinationItems_" + id).collect(Collectors.toList());
+            List<String> newContentSetIdList = contentSetIdList.stream().map(id -> "intelligentCombinationItems_" + id)
+                .collect(
+                    Collectors.toList());
             params.put("sceneSet", Joiner.on(",").join(newContentSetIdList));
             params.put("commerce", "B2C");
-            params.put("regionCode", Joiner.on(",").join(Optional.ofNullable(sgFrameworkContextContent).map(SgFrameworkContext::getLocParams).map(LocParams::getLogicIdByPriority).orElse(Lists.newArrayList())));
-            params.put("smAreaId", Optional.ofNullable(sgFrameworkContextContent).map(SgFrameworkContext::getLocParams).map(LocParams::getSmAreaId).orElse(0L).toString());
-            params.put("exposureDataUserId",Optional.ofNullable(sgFrameworkContextContent).map(SgFrameworkContext::getUserDO).map(UserDO::getCna).orElse(""));
-            params.put("pageSize",Optional.ofNullable(sgFrameworkContextContent).map(SgFrameworkContext::getUserPageInfo).map(PageInfoDO::getPageSize).orElse(20).toString());
+            params.put("regionCode", Joiner.on(",").join(Optional.ofNullable(sgFrameworkContextContent).map(
+                SgFrameworkContext::getLocParams).map(LocParams::getLogicIdByPriority).orElse(Lists.newArrayList())));
+            params.put("smAreaId", Optional.ofNullable(sgFrameworkContextContent).map(SgFrameworkContext::getLocParams)
+                .map(
+                    LocParams::getSmAreaId).orElse(0L).toString());
+            params.put("exposureDataUserId", Optional.ofNullable(sgFrameworkContextContent).map(
+                SgFrameworkContext::getUserDO).map(UserDO::getCna).orElse(""));
+            params.put("pageSize", Optional.ofNullable(sgFrameworkContextContent).map(
+                SgFrameworkContext::getUserPageInfo).map(
+                PageInfoDO::getPageSize).orElse(20).toString());
             if (Enviroment.PRE.equals(RpmContants.enviroment)) {
                 params.put("_devEnv_", "1");
             }
-            tacLogger.info("****FirstScreenMindContentOriginDataRequestExPt tppRequest***:"+tppRequest.toString());
-            LOGGER.info("****FirstScreenMindContentOriginDataRequestExPt tppRequest***:"+tppRequest.toString());
+            tacLogger.info("****FirstScreenMindContentOriginDataRequestExPt tppRequest***:" + tppRequest.toString());
+            LOGGER.info("****FirstScreenMindContentOriginDataRequestExPt tppRequest***:" + tppRequest.toString());
             return tppRequest;
         }
 
         RecommendRequest tppRequest = new RecommendRequest();
         Map<String, String> params = Maps.newHashMap();
-        params.put("contentSetIdList",Joiner.on(",").join(contentSetIdList));
+        params.put("contentSetIdList", Joiner.on(",").join(contentSetIdList));
 
         // 新版本的内容集id
-        List<String> newContentSetIdList = contentSetIdList.stream().map(id -> "intelligentCombinationItems_" + id).collect(Collectors.toList());
+        List<String> newContentSetIdList = contentSetIdList.stream().map(id -> "intelligentCombinationItems_" + id)
+            .collect(Collectors.toList());
         params.put("sceneSet", Joiner.on(",").join(newContentSetIdList));
 
-        params.put("pageSize", Optional.ofNullable(sgFrameworkContextContent).map(SgFrameworkContext::getUserPageInfo).map(PageInfoDO::getPageSize).orElse(4).toString());
+        params.put("pageSize", Optional.ofNullable(sgFrameworkContextContent).map(SgFrameworkContext::getUserPageInfo)
+            .map(PageInfoDO::getPageSize).orElse(4).toString());
         //逛超市TPP内容召回每个内容挂载的商品数量
         params.put("itemCountPerContent", "10");
         params.put("contentType", "7");
         params.put("contentSetSource", "intelligentCombinationItems");
         //未登陆用户唯一身份ID，确认是否必须
-        params.put("exposureDataUserId", Optional.ofNullable(sgFrameworkContextContent).map(SgFrameworkContext::getUserDO).map(UserDO::getCna).orElse(""));
-        params.put("smAreaId", Optional.ofNullable(sgFrameworkContextContent).map(SgFrameworkContext::getLocParams).map(LocParams::getSmAreaId).orElse(0L).toString());
-        params.put("logicAreaId", Joiner.on(",").join(Optional.ofNullable(sgFrameworkContextContent).map(SgFrameworkContext::getLocParams).map(LocParams::getLogicIdByPriority).orElse(Lists.newArrayList())));
-        Integer index = Optional.ofNullable(sgFrameworkContextContent).map(SgFrameworkContext::getUserPageInfo).map(PageInfoDO::getIndex).orElse(0);
+        params.put("exposureDataUserId", Optional.ofNullable(sgFrameworkContextContent).map(
+            SgFrameworkContext::getUserDO).map(UserDO::getCna).orElse(""));
+        params.put("smAreaId", Optional.ofNullable(sgFrameworkContextContent).map(SgFrameworkContext::getLocParams).map(
+            LocParams::getSmAreaId).orElse(0L).toString());
+        params.put("logicAreaId", Joiner.on(",").join(Optional.ofNullable(sgFrameworkContextContent).map(
+            SgFrameworkContext::getLocParams).map(LocParams::getLogicIdByPriority).orElse(
+            Lists.newArrayList())));
+        Integer index = Optional.ofNullable(sgFrameworkContextContent).map(SgFrameworkContext::getUserPageInfo).map(
+            PageInfoDO::getIndex).orElse(0);
         params.put("isFirstPage", index > 0 ? "false" : "true");
 
         if (contentRecommendMetaInfo != null) {
@@ -104,15 +122,17 @@ public class FirstScreenMindContentOriginDataRequestExPt implements ContentOrigi
         tppRequest.setAppId(25409L);
         tppRequest.setParams(params);
         tppRequest.setLogResult(true);
-        tppRequest.setUserId(Optional.ofNullable(sgFrameworkContextContent).map(SgFrameworkContext::getUserDO).map(UserDO::getUserId).orElse(0L));
-        tacLogger.info("****FirstScreenMindContentOriginDataRequestExPt tppRequest***:"+tppRequest.toString());
-        LOGGER.info("****FirstScreenMindContentOriginDataRequestExPt tppRequest***:"+tppRequest.toString());
+        tppRequest.setUserId(Optional.ofNullable(sgFrameworkContextContent).map(SgFrameworkContext::getUserDO)
+            .map(UserDO::getUserId).orElse(0L));
+        tacLogger.info("****FirstScreenMindContentOriginDataRequestExPt tppRequest***:" + tppRequest.toString());
+        LOGGER.info("****FirstScreenMindContentOriginDataRequestExPt tppRequest***:" + tppRequest.toString());
         return tppRequest;
     }
 
     private boolean isMind(Map<String, Object> requestParams) {
 
-        Long mindContentCode = MapUtil.getLongWithDefault(requestParams, RequestKeyConstantApp.FIRST_SCREEN_SCENE_CONTENT_SET_MIND, 0L);
+        Long mindContentCode = MapUtil.getLongWithDefault(requestParams,
+            RequestKeyConstantApp.FIRST_SCREEN_SCENE_CONTENT_SET_MIND, 0L);
 
         if (mindContentCode <= 0) {
             return false;
@@ -121,49 +141,41 @@ public class FirstScreenMindContentOriginDataRequestExPt implements ContentOrigi
         Object isFixPositionBanner = requestParams.get("isFixPositionBanner");
 
         Boolean isMind = false;
-        if(isFixPositionBanner == null || "".equals(isFixPositionBanner)){
+        if (isFixPositionBanner == null || "".equals(isFixPositionBanner)) {
             isMind = true;
-        } else if(isFixPositionBanner instanceof Boolean){
-            isMind = (Boolean) isFixPositionBanner;
-        }else if(isFixPositionBanner instanceof String && "true".equals(isFixPositionBanner)){
+        } else if (isFixPositionBanner instanceof Boolean) {
+            isMind = (Boolean)isFixPositionBanner;
+        } else if (isFixPositionBanner instanceof String && "true".equals(isFixPositionBanner)) {
             isMind = true;
         }
         return isMind;
     }
 
-    private List<Long> getMindContentSetIdList(Map<String, Object>  requestParams) {
+    private List<Long> getMindContentSetIdList(Map<String, Object> requestParams) {
 
         List<Long> result = Lists.newArrayList();
-        result.add(MapUtil.getLongWithDefault(requestParams, RequestKeyConstantApp.FIRST_SCREEN_SCENE_CONTENT_SET_MIND, 0L));
+        result.add(
+            MapUtil.getLongWithDefault(requestParams, RequestKeyConstantApp.FIRST_SCREEN_SCENE_CONTENT_SET_MIND, 0L));
 
         return result.stream().filter(contentSetId -> contentSetId > 0).collect(Collectors.toList());
     }
 
-    private List<Long> getContentSetIdList(Map<String, Object>  requestParams) {
-
+    private List<Long> getContentSetIdList(Map<String, Object> requestParams) {
 
         List<Long> result = Lists.newArrayList();
-        result.add(MapUtil.getLongWithDefault(requestParams, RequestKeyConstantApp.FIRST_SCREEN_SCENE_CONTENT_SET_RANKING, 0L));
-        result.add(MapUtil.getLongWithDefault(requestParams, RequestKeyConstantApp.FIRST_SCREEN_SCENE_CONTENT_SET_RECIPE, 0L));
-        result.add(MapUtil.getLongWithDefault(requestParams, RequestKeyConstantApp.FIRST_SCREEN_SCENE_CONTENT_SET_BRAND, 0L));
-        result.add(MapUtil.getLongWithDefault(requestParams, RequestKeyConstantApp.FIRST_SCREEN_SCENE_CONTENT_SET_MIND, 0L));
-        result.add(MapUtil.getLongWithDefault(requestParams, RequestKeyConstantApp.FIRST_SCREEN_SCENE_CONTENT_SET_O2O, 0L));
-        result.add(MapUtil.getLongWithDefault(requestParams, RequestKeyConstantApp.FIRST_SCREEN_SCENE_CONTENT_SET_B2C, 0L));
+        result.add(MapUtil
+            .getLongWithDefault(requestParams, RequestKeyConstantApp.FIRST_SCREEN_SCENE_CONTENT_SET_RANKING, 0L));
+        result.add(
+            MapUtil.getLongWithDefault(requestParams, RequestKeyConstantApp.FIRST_SCREEN_SCENE_CONTENT_SET_RECIPE, 0L));
+        result.add(
+            MapUtil.getLongWithDefault(requestParams, RequestKeyConstantApp.FIRST_SCREEN_SCENE_CONTENT_SET_BRAND, 0L));
+        result.add(
+            MapUtil.getLongWithDefault(requestParams, RequestKeyConstantApp.FIRST_SCREEN_SCENE_CONTENT_SET_MIND, 0L));
+        result.add(
+            MapUtil.getLongWithDefault(requestParams, RequestKeyConstantApp.FIRST_SCREEN_SCENE_CONTENT_SET_O2O, 0L));
+        result.add(
+            MapUtil.getLongWithDefault(requestParams, RequestKeyConstantApp.FIRST_SCREEN_SCENE_CONTENT_SET_B2C, 0L));
 
         return result.stream().filter(contentSetId -> contentSetId > 0).collect(Collectors.toList());
     }
-    private String getCna(Map<String, Object>  requestParams){
-        String cna = "";
-        if(MapUtils.isNotEmpty(requestParams) && requestParams.get(MtopContextKey.COOKIES) != null && requestParams.get(MtopContextKey.COOKIES) instanceof Map){
-            Map<String,String> cookies = (Map<String, String>)requestParams.get(MtopContextKey.COOKIES);
-            if(MapUtils.isNotEmpty(cookies)){
-                cna = cookies.get()
-            }
-
-        }
-
-
-    }
-
-
 }
