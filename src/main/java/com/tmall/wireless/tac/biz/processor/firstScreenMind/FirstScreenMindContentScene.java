e4f76402e119ab288d02a8ee9fc54ca7a6735a2d
guijian <maxiang.ma@alibaba-inc.com>
2021-05-20 14:37:31 +0800

    限时抢购

--- src/main/java/com/tmall/wireless/tac/biz/processor/firstScreenMind/FirstScreenMindContentScene.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/firstScreenMind/FirstScreenMindContentScene.java
@@ -17,6 +17,7 @@ import com.tmall.wireless.tac.biz.processor.common.ScenarioConstantApp;
 import com.tmall.wireless.tac.client.common.TacResult;
 import com.tmall.wireless.tac.client.dataservice.TacLogger;
 import com.tmall.wireless.tac.client.domain.Context;
+import com.tmall.wireless.tac.client.domain.DeviceInfo;
 import com.tmall.wireless.tac.client.domain.UserInfo;
 import io.reactivex.Flowable;
 import org.apache.commons.lang3.StringUtils;
@@ -98,6 +99,7 @@ public class FirstScreenMindContentScene {
         UserDO userDO = new UserDO();
         userDO.setUserId(Optional.of(context).map(Context::getUserInfo).map(UserInfo::getUserId).orElse(0L));
         userDO.setNick(Optional.of(context).map(Context::getUserInfo).map(UserInfo::getNick).orElse(""));
+        userDO.setUtdid(Optional.of(context).map(Context::getDeviceInfo).map(DeviceInfo::getUtdid).orElse(""));
         return userDO;
     }
     public ContentMetaInfo getContentMetaInfo() {
