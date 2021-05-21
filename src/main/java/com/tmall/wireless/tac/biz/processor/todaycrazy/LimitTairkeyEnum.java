4a9ff5c0f44049e951b6064799ef09fe295c0e75
guijian <maxiang.ma@alibaba-inc.com>
2021-05-21 12:18:27 +0800

    限时抢购

--- src/main/java/com/tmall/wireless/tac/biz/processor/todaycrazy/LimitTairkeyEnum.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/todaycrazy/LimitTairkeyEnum.java
@@ -8,12 +8,16 @@ import lombok.Getter;
  * 为了防止热点key,源数据存在于5个key中
  */
 public enum LimitTairkeyEnum {
-    FLASH_SALE_HD("flashSale_HD"),
+    /*FLASH_SALE_HD("flashSale_HD"),
     FLASH_SALE_HN("flashSale_HN"),
     FLASH_SALE_HB("flashSale_HB"),
     FLASH_SALE_XN("flashSale_XN"),
-    FLASH_SALE_XB("flashSale_HZ");
-
+    FLASH_SALE_XB("flashSale_HZ");*/
+    FLASH_SALE_HD("flashSaleNew_HD"),
+    FLASH_SALE_HN("flashSaleNew_HN"),
+    FLASH_SALE_HB("flashSaleNew_HB"),
+    FLASH_SALE_XN("flashSaleNew_XN"),
+    FLASH_SALE_XB("flashSaleNew_HZ");
     LimitTairkeyEnum(String key) {
         this.key = key;
     }
