ab8fe5de11fc53210eb13f522de4661b17d04572
yangqing.byq <yangqing.byq@alibaba-inc.com>
2021-05-28 02:59:08 +0800

    add HadesLogU

--- src/main/java/com/tmall/wireless/tac/biz/processor/firstScreenMind/FirstScreenMindContentScene.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/firstScreenMind/FirstScreenMindContentScene.java
@@ -4,12 +4,14 @@ package com.tmall.wireless.tac.biz.processor.firstScreenMind;
 import com.alibaba.fastjson.JSON;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.tmall.hades.monitor.print.HadesLogUtil;
 import com.tmall.txcs.biz.supermarket.scene.UserParamsKeyConstant;
 import com.tmall.txcs.biz.supermarket.scene.util.CsaUtil;
 import com.tmall.txcs.biz.supermarket.scene.util.MapUtil;
 import com.tmall.txcs.gs.framework.model.*;
 import com.tmall.txcs.gs.framework.model.meta.*;
 import com.tmall.txcs.gs.framework.service.impl.SgFrameworkServiceContent;
+import com.tmall.txcs.gs.framework.support.LogUtil;
 import com.tmall.txcs.gs.model.biz.context.PageInfoDO;
 import com.tmall.txcs.gs.model.biz.context.SceneInfo;
 import com.tmall.txcs.gs.model.biz.context.UserDO;
@@ -27,8 +29,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.spec.OAEPParameterSpec;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -42,6 +46,7 @@ public class FirstScreenMindContentScene {
 
     public Flowable<TacResult<SgFrameworkResponse<ContentVO>>> recommend(Context context) {
 
+        long startTime = System.currentTimeMillis();
         tacLogger.info("***FirstScreenMindContentScene context***:"+ JSON.toJSONString(context));
 
         Long smAreaId = MapUtil.getLongWithDefault(context.getParams(), "smAreaId", 330100L);
@@ -61,6 +66,11 @@ public class FirstScreenMindContentScene {
         sgFrameworkContextContent.setUserPageInfo(pageInfoDO);
         tacLogger.info("*****FirstScreenMindContentScene sgFrameworkContextContent.toString()***:"+sgFrameworkContextContent.toString());
         LOGGER.info("*****FirstScreenMindContentScene sgFrameworkContextContent.toString()***:"+sgFrameworkContextContent.toString());
+
+        HadesLogUtil.stream(sgFrameworkContextContent.getBizScenario().getUniqueIdentity())
+                .kv("step", "requestLog")
+                .kv("userId", Optional.of(sgFrameworkContextContent).map(SgFrameworkContext::getUserDO).map(UserDO::getUserId).map(Objects::toString).orElse("0"))
+                .error();
         return sgFrameworkServiceContent.recommend(sgFrameworkContextContent)
                 .map(response -> {
                     Map<String, Object> requestParams = sgFrameworkContextContent.getRequestParams();
@@ -88,6 +98,11 @@ public class FirstScreenMindContentScene {
                     }
                     response.getExtInfos().put("propertyMap", propertyMap);
                     LOGGER.info("FirstScreenMindContentScene JSON.toJSONString(response)"+JSON.toJSONString(response));
+                    HadesLogUtil.stream(sgFrameworkContextContent.getBizScenario().getUniqueIdentity())
+                            .kv("step", "requestLog")
+                            .kv("userId", Optional.of(sgFrameworkContextContent).map(SgFrameworkContext::getUserDO).map(UserDO::getUserId).map(Objects::toString).orElse("0"))
+                            .kv("rt", String.valueOf(System.currentTimeMillis() - startTime))
+                            .error();
                     return response;
                 })
                 .map(TacResult::newResult)
