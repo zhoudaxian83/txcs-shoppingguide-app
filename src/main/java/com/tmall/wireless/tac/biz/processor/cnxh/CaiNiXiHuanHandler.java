4ffbe6ba352f0381c993be5430689377c081e890
wb-ljc587473 <wb-ljc587473@alibaba-inc.com>
2021-05-27 10:44:52 +0800

    test

---
+++ src/main/java/com/tmall/wireless/tac/biz/processor/cnxh/CaiNiXiHuanHandler.java
@@ -0,0 +1,25 @@
+package com.tmall.wireless.tac.biz.processor.cnxh;
+
+import com.tmall.txcs.biz.supermarket.scene.gul.GulSubTabScene;
+import com.tmall.txcs.gs.base.RpmReactiveHandler;
+import com.tmall.txcs.gs.framework.model.EntityVO;
+import com.tmall.txcs.gs.framework.model.SgFrameworkResponse;
+import com.tmall.wireless.tac.client.common.TacResult;
+import com.tmall.wireless.tac.client.domain.Context;
+import io.reactivex.Flowable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by yangqing.byq on 2021/3/9.
+ */
+@Component
+public class CaiNiXiHuanHandler extends RpmReactiveHandler<SgFrameworkResponse<EntityVO>> {
+    @Autowired
+    CaiNiXiHuanPageBannerItemInfoScene caiNiXiHuanPageBannerItemInfoScene;
+
+    @Override
+    public Flowable<TacResult<SgFrameworkResponse<EntityVO>>> executeFlowable(Context context) throws Exception {
+        return caiNiXiHuanPageBannerItemInfoScene.recommend(context);
+    }
+}
