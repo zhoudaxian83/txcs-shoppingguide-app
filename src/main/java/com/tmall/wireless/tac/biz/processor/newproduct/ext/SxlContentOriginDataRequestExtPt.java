dc4fbd675c713528899cac5dd7b9229dd23a787a
1 <haixiao.zhx@alibaba-inc.com>
2021-06-08 20:05:03 +0800

    done

--- src/main/java/com/tmall/wireless/tac/biz/processor/newproduct/ext/SxlContentOriginDataRequestExtPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/newproduct/ext/SxlContentOriginDataRequestExtPt.java
@@ -2,8 +2,10 @@ package com.tmall.wireless.tac.biz.processor.newproduct.ext;
 
 import com.alibaba.cola.extension.Extension;
 import com.google.common.collect.Maps;
+import com.tmall.txcs.gs.framework.extensions.origindata.request.ContentOriginDataRequestExtPt;
 import com.tmall.txcs.gs.framework.extensions.origindata.request.ItemOriginDataRequestExtPt;
 import com.tmall.txcs.gs.framework.model.SgFrameworkContext;
+import com.tmall.txcs.gs.framework.model.SgFrameworkContextContent;
 import com.tmall.txcs.gs.framework.model.SgFrameworkContextItem;
 import com.tmall.txcs.gs.framework.model.constant.ScenarioConstant;
 import com.tmall.txcs.gs.model.biz.context.LocParams;
@@ -24,13 +26,12 @@ import java.util.Optional;
     useCase = ScenarioConstant.LOC_TYPE_B2C,
     scenario = ScenarioConstantApp.SCENARIO_SHANG_XIN_CONTENT)
 @Service
-public class SxlContentOriginDataRequestExtPt implements ItemOriginDataRequestExtPt {
+public class SxlContentOriginDataRequestExtPt implements ContentOriginDataRequestExtPt {
 
     private static final Long APPID = 25831L;
 
-
     @Override
-    public RecommendRequest process(SgFrameworkContextItem sgFrameworkContextItem) {
+    public RecommendRequest process(SgFrameworkContextContent sgFrameworkContextContent) {
 
         /**
          * https://tui.taobao.com/recommend?appid=25831&itemSets=crm_5233&commerce=B2C&regionCode=108&smAreaId=330110&itemSetFilterTriggers=crm_5233&OPEN_MAINTENANCE=1
@@ -38,12 +39,12 @@ public class SxlContentOriginDataRequestExtPt implements ItemOriginDataRequestEx
         RecommendRequest tppRequest = new RecommendRequest();
         tppRequest.setAppId(APPID);
 
-        sgFrameworkContextItem.getEntitySetParams().getContentSetIdList();
+        sgFrameworkContextContent.getEntitySetParams().getContentSetIdList();
         Map<String, String> params = Maps.newHashMap();
         params.put("itemSets", "crm_322385,crm_5233");
         params.put("commerce", "B2C");
-        params.put("smAreaId", Optional.ofNullable(sgFrameworkContextItem).map(SgFrameworkContext::getLocParams).map(LocParams::getSmAreaId).orElse(0L).toString());
-        tppRequest.setUserId(Optional.ofNullable(sgFrameworkContextItem).map(SgFrameworkContext::getUserDO)
+        params.put("smAreaId", Optional.ofNullable(sgFrameworkContextContent).map(SgFrameworkContext::getLocParams).map(LocParams::getSmAreaId).orElse(0L).toString());
+        tppRequest.setUserId(Optional.ofNullable(sgFrameworkContextContent).map(SgFrameworkContext::getUserDO)
             .map(UserDO::getUserId).orElse(0L));
         tppRequest.setParams(params);
         return tppRequest;
