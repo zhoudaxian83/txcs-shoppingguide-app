887f9aed834671f3c5f5383202d85ddcb9e8d354
1 <haixiao.zhx@alibaba-inc.com>
2021-06-09 20:01:01 +0800

    done

--- src/main/java/com/tmall/wireless/tac/biz/processor/newproduct/ext/SxlContentOriginDataRequestExtPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/newproduct/ext/SxlContentOriginDataRequestExtPt.java
@@ -1,7 +1,16 @@
 package com.tmall.wireless.tac.biz.processor.newproduct.ext;
 
+import com.alibaba.aladdin.lamp.domain.request.Request;
+import com.alibaba.aladdin.lamp.domain.request.RequestItem;
+import com.alibaba.aladdin.lamp.domain.request.modules.DeviceInfo;
+import com.alibaba.aladdin.lamp.domain.request.modules.LocationInfo;
+import com.alibaba.aladdin.lamp.domain.response.ResResponse;
+import com.alibaba.aladdin.lamp.domain.user.UserProfile;
 import com.alibaba.cola.extension.Extension;
+import com.alibaba.fastjson.JSON;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.tmall.txcs.biz.supermarket.scene.util.MapUtil;
 import com.tmall.txcs.gs.framework.extensions.origindata.request.ContentOriginDataRequestExtPt;
 import com.tmall.txcs.gs.framework.extensions.origindata.request.ItemOriginDataRequestExtPt;
 import com.tmall.txcs.gs.framework.model.SgFrameworkContext;
@@ -12,9 +21,16 @@ import com.tmall.txcs.gs.model.biz.context.LocParams;
 import com.tmall.txcs.gs.model.biz.context.PageInfoDO;
 import com.tmall.txcs.gs.model.biz.context.UserDO;
 import com.tmall.txcs.gs.model.spi.model.RecommendRequest;
+import com.tmall.txcs.gs.spi.recommend.AldSpi;
 import com.tmall.wireless.tac.biz.processor.common.ScenarioConstantApp;
+import com.tmall.wireless.tac.biz.processor.newproduct.handler.SxlItemFeedsHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -29,11 +45,18 @@ import java.util.Optional;
 @Service
 public class SxlContentOriginDataRequestExtPt implements ContentOriginDataRequestExtPt {
 
+    Logger LOGGER = LoggerFactory.getLogger(SxlContentOriginDataRequestExtPt.class);
+
     private static final Long APPID = 25831L;
+
+    @Autowired
+    private AldSpi aldSpi;
 
     @Override
     public RecommendRequest process(SgFrameworkContextContent sgFrameworkContextContent) {
 
+        Map<String, ResResponse> aldResponse = getAldInfo(sgFrameworkContextContent);
+        LOGGER.error("SxlContentOriginDataRequestExtPt aldResponse:{}",JSON.toJSONString(aldResponse));
         /**
          * https://tui.taobao.com/recommend?appid=25831&itemSets=crm_5233&commerce=B2C&regionCode=108&smAreaId=330110&itemSetFilterTriggers=crm_5233&OPEN_MAINTENANCE=1
          */
@@ -47,5 +70,34 @@ public class SxlContentOriginDataRequestExtPt implements ContentOriginDataReques
             .map(UserDO::getUserId).orElse(0L));
         tppRequest.setParams(params);
         return tppRequest;
+    }
+
+
+    private Map<String, ResResponse> getAldInfo(SgFrameworkContextContent sgFrameworkContextContent){
+
+       return aldSpi.queryAldInfoSync(buildAldRequest(sgFrameworkContextContent));
+
+    }
+
+    private Request buildAldRequest(SgFrameworkContextContent sgFrameworkContextContent){
+        Request request = new Request();
+        request.setBizId(101);
+        request.setCallSource("txcs-shoppingguide");
+        request.setDebug(false);
+        RequestItem item = new RequestItem();
+        item.setResId("17390113");
+        UserProfile userProfile = request.getUserProfile();
+        userProfile.setUserId(sgFrameworkContextContent.getUserDO().getUserId());
+        DeviceInfo deviceInfo = request.getDeviceInfo();;
+        deviceInfo.setTtid(sgFrameworkContextContent.getUserDO().getUtdid());
+        request.setRequestItems(Lists.newArrayList(item));
+        //地址信息
+        LocationInfo locationInfo = request.getLocationInfo();
+        //四级地址
+        locationInfo.setCityLevel4(String.valueOf(sgFrameworkContextContent.getLocParams().getSmAreaId()));
+        List<String> wdkCodes = Lists.newArrayList();
+        locationInfo.setWdkCodes(wdkCodes);
+        return request;
+
     }
 }
---
+++ src/main/java/com/tmall/wireless/tac/biz/processor/newproduct/service/SxlItemRecService.java
@@ -0,0 +1,8 @@
+package com.tmall.wireless.tac.biz.processor.newproduct.service;
+
+/**
+ * @author haixiao.zhang
+ * @date 2021/6/9
+ */
+public class SxlItemRecService {
+}
