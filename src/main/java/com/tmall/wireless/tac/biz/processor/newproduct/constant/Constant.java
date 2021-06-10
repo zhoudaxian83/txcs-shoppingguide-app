d59eecb625d7b0c36a44e68412a1a84f279ce7f8
1 <haixiao.zhx@alibaba-inc.com>
2021-06-10 11:05:21 +0800

    done

---
+++ src/main/java/com/tmall/wireless/tac/biz/processor/newproduct/constant/Constant.java
@@ -0,0 +1,13 @@
+package com.tmall.wireless.tac.biz.processor.newproduct.constant;
+
+/**
+ * @author haixiao.zhang
+ * @date 2021/6/10
+ */
+public class Constant {
+
+    public static final String CONTENT_ALD_RES_ID = "";
+
+    public static final String ITEM_ALD_RES_ID = "";
+
+}
