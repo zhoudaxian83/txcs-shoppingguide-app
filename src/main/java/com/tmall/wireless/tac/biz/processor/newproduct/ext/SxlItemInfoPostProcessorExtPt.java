c5077067f68186d7e4468d94e1c059b882375e7f
1 <haixiao.zhx@alibaba-inc.com>
2021-06-10 17:54:46 +0800

    done

--- src/main/java/com/tmall/wireless/tac/biz/processor/newproduct/ext/SxlItemInfoPostProcessorExtPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/newproduct/ext/SxlItemInfoPostProcessorExtPt.java
@@ -1,31 +1,17 @@
 package com.tmall.wireless.tac.biz.processor.newproduct.ext;
 
-import com.alibaba.cola.extension.Extension;
-import com.alibaba.fastjson.JSON;
 import com.tmall.txcs.gs.framework.extensions.itemdatapost.ItemInfoPostProcessorExtPt;
 import com.tmall.txcs.gs.framework.extensions.itemdatapost.ItemInfoPostProcessorResp;
-import com.tmall.txcs.gs.framework.model.EntityVO;
-import com.tmall.txcs.gs.framework.model.ItemGroup;
 import com.tmall.txcs.gs.framework.model.SgFrameworkContextItem;
-import com.tmall.txcs.gs.framework.model.SgFrameworkResponse;
-import com.tmall.txcs.gs.framework.model.constant.ScenarioConstant;
-import com.tmall.txcs.gs.framework.support.itemInfo.ItemInfoGroupResponse;
 import com.tmall.txcs.gs.model.Response;
-import com.tmall.wireless.tac.biz.processor.common.ScenarioConstantApp;
 import com.tmall.wireless.tac.client.dataservice.TacLogger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-import java.util.Map;
 
 /**
  * @author haixiao.zhang
  * @date 2021/6/10
  */
-@Extension(bizId = ScenarioConstant.BIZ_TYPE_SUPERMARKET,
-    useCase = ScenarioConstant.LOC_TYPE_B2C,
-    scenario = ScenarioConstantApp.SCENARIO_SHANG_XIN_ITEM)
-@Service
 public class SxlItemInfoPostProcessorExtPt implements ItemInfoPostProcessorExtPt {
 
     @Autowired
@@ -34,12 +20,6 @@ public class SxlItemInfoPostProcessorExtPt implements ItemInfoPostProcessorExtPt
     @Override
     public Response<ItemInfoPostProcessorResp> process(SgFrameworkContextItem sgFrameworkContextItem) {
 
-        Map<ItemGroup, ItemInfoGroupResponse> itemGroupItemInfoGroupResponseMap = sgFrameworkContextItem
-            .getItemInfoGroupResponseMap();
-
-        SgFrameworkResponse<EntityVO> entityVOgFrameworkResponse = sgFrameworkContextItem.getEntityVOSgFrameworkResponse();
-
-        tacLogger.info("SxlItemInfoPostProcessorExtPt entityVOgFrameworkResponse:{}"+JSON.toJSONString(entityVOgFrameworkResponse.getItemAndContentList()));
         return Response.success(new ItemInfoPostProcessorResp());
     }
 }
--- src/main/java/com/tmall/wireless/tac/biz/processor/newproduct/handler/SxlItemFeedsHandler.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/newproduct/handler/SxlItemFeedsHandler.java
@@ -1,5 +1,11 @@
 package com.tmall.wireless.tac.biz.processor.newproduct.handler;
 
+import com.alibaba.aladdin.lamp.domain.request.Request;
+import com.alibaba.aladdin.lamp.domain.request.RequestItem;
+import com.alibaba.aladdin.lamp.domain.request.modules.DeviceInfo;
+import com.alibaba.aladdin.lamp.domain.request.modules.LocationInfo;
+import com.alibaba.aladdin.lamp.domain.response.ResResponse;
+import com.alibaba.aladdin.lamp.domain.user.UserProfile;
 import com.alibaba.fastjson.JSON;
 import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
@@ -9,27 +15,30 @@ import com.tmall.txcs.biz.supermarket.scene.UserParamsKeyConstant;
 import com.tmall.txcs.biz.supermarket.scene.util.CsaUtil;
 import com.tmall.txcs.biz.supermarket.scene.util.MapUtil;
 import com.tmall.txcs.gs.base.RpmReactiveHandler;
-import com.tmall.txcs.gs.framework.model.EntityVO;
-import com.tmall.txcs.gs.framework.model.ItemEntityVO;
-import com.tmall.txcs.gs.framework.model.SgFrameworkContextItem;
-import com.tmall.txcs.gs.framework.model.SgFrameworkResponse;
+import com.tmall.txcs.gs.framework.model.*;
 import com.tmall.txcs.gs.framework.model.meta.*;
 import com.tmall.txcs.gs.framework.service.impl.SgFrameworkServiceItem;
 import com.tmall.txcs.gs.model.biz.context.EntitySetParams;
 import com.tmall.txcs.gs.model.biz.context.PageInfoDO;
 import com.tmall.txcs.gs.model.biz.context.SceneInfo;
 import com.tmall.txcs.gs.model.biz.context.UserDO;
+import com.tmall.txcs.gs.spi.recommend.AldSpi;
 import com.tmall.wireless.tac.biz.processor.common.RequestKeyConstantApp;
 import com.tmall.wireless.tac.biz.processor.common.ScenarioConstantApp;
 import com.tmall.wireless.tac.biz.processor.firstpage.banner.iteminfo.model.BannerItemDTO;
 import com.tmall.wireless.tac.biz.processor.firstpage.banner.iteminfo.model.BannerItemVO;
 import com.tmall.wireless.tac.biz.processor.firstpage.banner.iteminfo.model.BannerVO;
 import com.tmall.wireless.tac.biz.processor.firstpage.banner.iteminfo.uitl.BannerUtil;
+import com.tmall.wireless.tac.biz.processor.newproduct.constant.Constant;
+import com.tmall.wireless.tac.biz.processor.newproduct.service.SxlContentRecService;
+import com.tmall.wireless.tac.biz.processor.newproduct.service.SxlItemRecService;
 import com.tmall.wireless.tac.client.common.TacResult;
+import com.tmall.wireless.tac.client.dataservice.TacLogger;
 import com.tmall.wireless.tac.client.domain.Context;
 import com.tmall.wireless.tac.client.domain.UserInfo;
 import io.reactivex.Flowable;
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.MapUtils;
 import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -49,165 +58,75 @@ import java.util.stream.Collectors;
 @Component
 public class SxlItemFeedsHandler extends RpmReactiveHandler<SgFrameworkResponse<EntityVO>> {
 
-    Logger LOGGER = LoggerFactory.getLogger(SxlItemFeedsHandler.class);
+    private Logger LOGGER = LoggerFactory.getLogger(SxlItemFeedsHandler.class);
 
     @Autowired
-    SgFrameworkServiceItem sgFrameworkServiceItem;
+    private SxlItemRecService sxlItemRecService;
 
-    static List<Pair<String, String>> dataTubeKeyList = Lists.newArrayList(
-        Pair.of("recommendWords","recommendWords"),
-        Pair.of("videoUrl","videoUrl"),
-        Pair.of("type","sxlType"),
-        Pair.of("atmosphereImageUrl","atmosphereImageUrl"),
-        Pair.of("sellingPointDesc","sellingPointDesc")
-    );
+    @Autowired
+    TacLogger tacLogger;
+
+    @Autowired
+    private AldSpi aldSpi;
 
     @Override
     public Flowable<TacResult<SgFrameworkResponse<EntityVO>>> executeFlowable(Context context) throws Exception {
 
-        LOGGER.error("ITEM_REQUEST:{}", JSON.toJSONString(context));
+        LOGGER.error("SxlItemFeedsHandler ITEM_REQUEST:{}", JSON.toJSONString(context));
 
-        HadesLogUtil.debug("ITEM_REQUEST:{}"+JSON.toJSONString(context));
+        HadesLogUtil.debug("SxlItemFeedsHandler ITEM_REQUEST:{}" + JSON.toJSONString(context));
 
-        Long smAreaId = MapUtil.getLongWithDefault(context.getParams(), "smAreaId", 330100L);
-        Long itemSetId = MapUtil.getLongWithDefault(context.getParams(), RequestKeyConstantApp.ITEMSET_ID, 322385L);
+        Flowable<TacResult<SgFrameworkResponse<EntityVO>>> tacResultFlowable = sxlItemRecService.recommend(context);
 
-        SgFrameworkContextItem sgFrameworkContextItem = new SgFrameworkContextItem();
-        EntitySetParams entitySetParams = new EntitySetParams();
-        entitySetParams.setItemSetSource("crm");
-        entitySetParams.setItemSetIdList(Lists.newArrayList(itemSetId));
-        sgFrameworkContextItem.setRequestParams(context.getParams());
-        sgFrameworkContextItem.setEntitySetParams(entitySetParams);
-        SceneInfo sceneInfo = new SceneInfo();
-        sceneInfo.setBiz(ScenarioConstantApp.BIZ_TYPE_SUPERMARKET);
-        sceneInfo.setSubBiz(ScenarioConstantApp.LOC_TYPE_B2C);
-        sceneInfo.setScene(ScenarioConstantApp.SCENARIO_SHANG_XIN_ITEM);
-        sgFrameworkContextItem.setSceneInfo(sceneInfo);
-        UserDO userDO = new UserDO();
-        userDO.setUserId(Optional.of(context).map(Context::getUserInfo).map(UserInfo::getUserId).orElse(0L));
-        userDO.setNick(Optional.of(context).map(Context::getUserInfo).map(UserInfo::getNick).orElse(""));
-        sgFrameworkContextItem.setUserDO(userDO);
+        getAldInfo(context);
+        tacResultFlowable.map(e->{
 
-        sgFrameworkContextItem.setLocParams(CsaUtil
-            .parseCsaObj(context.get(UserParamsKeyConstant.USER_PARAMS_KEY_CSA), smAreaId));
+            List<EntityVO> lis = e.getData().getItemAndContentList();
+            EntityVO entityVO = new EntityVO();
+            return e;
+        }).onErrorReturn((r -> TacResult.errorResult("")));
 
-        sgFrameworkContextItem.setItemMetaInfo(getO2OItemMetaInfo());
-
-        PageInfoDO pageInfoDO = new PageInfoDO();
-        pageInfoDO.setIndex(0);
-        pageInfoDO.setPageSize(20);
-        sgFrameworkContextItem.setUserPageInfo(pageInfoDO);
-
-        return sgFrameworkServiceItem.recommend(sgFrameworkContextItem)
-            .map(TacResult::newResult)
-            .onErrorReturn(r -> TacResult.errorResult(""));
-
+        return tacResultFlowable;
 
     }
 
-    private Map<String, BannerVO> convertResult(SgFrameworkResponse<EntityVO> response, String bannerInfo) {
-        Map<String, BannerVO> result = Maps.newHashMap();
+    private void getAldInfo(Context context){
 
-        Map<String, EntityVO> itemEntityVOMap = buildItemEntityVoMap(response);
+        Map<String, ResResponse> mapResponse = aldSpi.queryAldInfoSync(buildAldRequest(context));
 
-        Map<String, List<BannerItemDTO>> bannerIndex2ItemList = BannerUtil.parseBannerItem(bannerInfo);
+        if(MapUtils.isNotEmpty(mapResponse)){
+            List<Map<String, Object>> dataList = (List<Map<String, Object>>)mapResponse.get(Constant.ITEM_ALD_RES_ID).get("data");
+            tacLogger.info("getAldInfo:"+JSON.toJSONString(dataList));
 
-        bannerIndex2ItemList.keySet().forEach(key -> {
-            BannerVO bannerVO = new BannerVO();
-            List<BannerItemDTO> bannerItemDTOList = bannerIndex2ItemList.get(key);
-            if (CollectionUtils.isEmpty(bannerItemDTOList)) {
-                return;
-            }
+            dataList.forEach(e->{
 
-            List<Long> failItemList = Lists.newArrayList();
-            List<Long> itemIdList = Lists.newArrayList();
-            List<BannerItemVO> items = Lists.newArrayList();
+            });
 
-            for (BannerItemDTO bannerItemDTO : bannerItemDTOList) {
-                Long itemId = bannerItemDTO.getItemId();
-                String locType = bannerItemDTO.getLocType();
-                String s = locType + "_" + itemId;
-                EntityVO entityVO = itemEntityVOMap.get(s);
-                if (entityVO == null) {
-                    failItemList.add(itemId);
-                } else {
-                    itemIdList.add(itemId);
-                    BannerItemVO bannerItemVO = new BannerItemVO();
-                    bannerItemVO.setItemId(itemId);
-                    bannerItemVO.setLocType(locType);
-                    bannerItemVO.setItemImg(entityVO.getString("itemImg"));
-                    items.add(bannerItemVO);
-                }
-            }
-            bannerVO.setFailItemList(failItemList);
-            bannerVO.setItems(items);
-            bannerVO.setEntryItems(Joiner.on(",").join(itemIdList));
-            bannerVO.setItemIdList(itemIdList);
-            result.put(key, bannerVO);
-        });
-
-        return result;
-    }
-
-    private Map<String, EntityVO> buildItemEntityVoMap(SgFrameworkResponse<EntityVO> response) {
-        Map<String, EntityVO> itemEntityVOMap = Maps.newHashMap();
-
-        if (response == null || CollectionUtils.isEmpty(response.getItemAndContentList())) {
-            return itemEntityVOMap;
         }
 
-        response.getItemAndContentList().stream().forEach(i -> {
-            if (i instanceof ItemEntityVO) {
-                Long itemId = i.getLong("itemId");
-                //String locType = i.getString("locType");
-                //String key = locType + "_" + itemId;
-                //itemEntityVOMap.put(key, i);
-            }
-        });
-        return itemEntityVOMap;
     }
 
-    public static ItemMetaInfo getO2OItemMetaInfo() {
-        ItemMetaInfo itemMetaInfo = new ItemMetaInfo();
-        List<ItemGroupMetaInfo> itemGroupMetaInfoList = Lists.newArrayList();
-        List<ItemInfoSourceMetaInfo> itemInfoSourceMetaInfoList = Lists.newArrayList();
-        ItemGroupMetaInfo itemGroupMetaInfo1 = new ItemGroupMetaInfo();
-        itemGroupMetaInfoList.add(itemGroupMetaInfo1);
-        itemGroupMetaInfo1.setGroupName("sm_B2C");
-        itemGroupMetaInfo1.setItemInfoSourceMetaInfos(itemInfoSourceMetaInfoList);
-        ItemInfoSourceMetaInfo itemInfoSourceMetaInfoCaptain = new ItemInfoSourceMetaInfo();
-        itemInfoSourceMetaInfoCaptain.setSourceName("captain");
-        itemInfoSourceMetaInfoCaptain.setSceneCode("shoppingguide.newLauch.common");
-        itemInfoSourceMetaInfoCaptain.setDataTubeMateInfo(buildDataTubeMateInfo(322385L));
+    private Request buildAldRequest(Context context){
+        Request request = new Request();
+        request.setBizId(Constant.ALD_BIZ_ID);
+        request.setCallSource(Constant.ALD_CALL_SOURCE);
+        request.setDebug(false);
+        RequestItem item = new RequestItem();
+        item.setResId(Constant.ITEM_ALD_RES_ID);
+        UserProfile userProfile = request.getUserProfile();
+        userProfile.setUserId(context.getUserInfo().getUserId());
+        DeviceInfo deviceInfo = request.getDeviceInfo();
+        //deviceInfo.setTtid(sgFrameworkContextContent.get().);
+        request.setRequestItems(Lists.newArrayList(item));
+        //地址信息
+        LocationInfo locationInfo = request.getLocationInfo();
+        //四级地址
+        Long smAreaId = MapUtil.getLongWithDefault(context.getParams(), "smAreaId", 330100L);
+        locationInfo.setCityLevel4(String.valueOf(smAreaId));
+        List<String> wdkCodes = Lists.newArrayList();
+        locationInfo.setWdkCodes(wdkCodes);
+        return request;
 
-        itemInfoSourceMetaInfoList.add(itemInfoSourceMetaInfoCaptain);
-        itemMetaInfo.setItemGroupRenderInfoList(itemGroupMetaInfoList);
-        ItemInfoSourceMetaInfo itemInfoSourceMetaInfoTpp = new ItemInfoSourceMetaInfo();
-        itemInfoSourceMetaInfoTpp.setSourceName("tpp");
-        itemInfoSourceMetaInfoList.add(itemInfoSourceMetaInfoTpp);
-
-
-        ItemRecommendMetaInfo itemRecommendMetaInfo = new ItemRecommendMetaInfo();
-        itemRecommendMetaInfo.setAppId(24910L);
-        itemMetaInfo.setItemRecommendMetaInfo(itemRecommendMetaInfo);
-        itemMetaInfo.setItemGroupRenderInfoList(itemGroupMetaInfoList);
-        itemRecommendMetaInfo.setUseRecommendSpiV2(true);
-        return itemMetaInfo;
     }
 
-
-    private static DataTubeMateInfo buildDataTubeMateInfo(Long itemSetId) {
-
-
-        DataTubeMateInfo dataTubeMateInfo = new DataTubeMateInfo();
-        dataTubeMateInfo.setActivityId(String.valueOf(itemSetId));
-        dataTubeMateInfo.setChannelName("itemExtLdb");
-        dataTubeMateInfo.setDataKeyList(dataTubeKeyList.stream().map(k -> {
-            DataTubeKey dataTubeKey = new DataTubeKey();
-            dataTubeKey.setDataKey(k.getRight());
-            dataTubeKey.setVoKey(k.getLeft());
-            return dataTubeKey;
-        }).collect(Collectors.toList()));
-        return dataTubeMateInfo;
-    }
 }
