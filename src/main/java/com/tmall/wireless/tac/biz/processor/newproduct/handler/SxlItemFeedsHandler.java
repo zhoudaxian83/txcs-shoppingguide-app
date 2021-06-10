a8a29bd83f82be492e299d222477644cd1f59b01
1 <haixiao.zhx@alibaba-inc.com>
2021-06-10 17:59:55 +0800

    done

--- src/main/java/com/tmall/wireless/tac/biz/processor/newproduct/handler/SxlItemFeedsHandler.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/newproduct/handler/SxlItemFeedsHandler.java
@@ -7,39 +7,19 @@ import com.alibaba.aladdin.lamp.domain.request.modules.LocationInfo;
 import com.alibaba.aladdin.lamp.domain.response.ResResponse;
 import com.alibaba.aladdin.lamp.domain.user.UserProfile;
 import com.alibaba.fastjson.JSON;
-import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 import com.tmall.hades.monitor.print.HadesLogUtil;
-import com.tmall.txcs.biz.supermarket.scene.UserParamsKeyConstant;
-import com.tmall.txcs.biz.supermarket.scene.util.CsaUtil;
 import com.tmall.txcs.biz.supermarket.scene.util.MapUtil;
 import com.tmall.txcs.gs.base.RpmReactiveHandler;
 import com.tmall.txcs.gs.framework.model.*;
-import com.tmall.txcs.gs.framework.model.meta.*;
-import com.tmall.txcs.gs.framework.service.impl.SgFrameworkServiceItem;
-import com.tmall.txcs.gs.model.biz.context.EntitySetParams;
-import com.tmall.txcs.gs.model.biz.context.PageInfoDO;
-import com.tmall.txcs.gs.model.biz.context.SceneInfo;
-import com.tmall.txcs.gs.model.biz.context.UserDO;
 import com.tmall.txcs.gs.spi.recommend.AldSpi;
-import com.tmall.wireless.tac.biz.processor.common.RequestKeyConstantApp;
-import com.tmall.wireless.tac.biz.processor.common.ScenarioConstantApp;
-import com.tmall.wireless.tac.biz.processor.firstpage.banner.iteminfo.model.BannerItemDTO;
-import com.tmall.wireless.tac.biz.processor.firstpage.banner.iteminfo.model.BannerItemVO;
-import com.tmall.wireless.tac.biz.processor.firstpage.banner.iteminfo.model.BannerVO;
-import com.tmall.wireless.tac.biz.processor.firstpage.banner.iteminfo.uitl.BannerUtil;
 import com.tmall.wireless.tac.biz.processor.newproduct.constant.Constant;
-import com.tmall.wireless.tac.biz.processor.newproduct.service.SxlContentRecService;
 import com.tmall.wireless.tac.biz.processor.newproduct.service.SxlItemRecService;
 import com.tmall.wireless.tac.client.common.TacResult;
 import com.tmall.wireless.tac.client.dataservice.TacLogger;
 import com.tmall.wireless.tac.client.domain.Context;
-import com.tmall.wireless.tac.client.domain.UserInfo;
 import io.reactivex.Flowable;
-import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.collections.MapUtils;
-import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -47,8 +27,6 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 /**
  * 上新了超市商品推荐
