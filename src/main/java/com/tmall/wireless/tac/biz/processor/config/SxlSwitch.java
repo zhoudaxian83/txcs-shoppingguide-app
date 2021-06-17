9c797d457bf30b9b7ecbbfe9dfc83527b9a454e3
1 <haixiao.zhx@alibaba-inc.com>
2021-06-17 13:35:08 +0800

    done

--- src/main/java/com/tmall/wireless/tac/biz/processor/config/SxlSwitch.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/config/SxlSwitch.java
@@ -3,14 +3,21 @@ package com.tmall.wireless.tac.biz.processor.config;
 import com.taobao.csp.switchcenter.annotation.AppSwitch;
 import com.taobao.csp.switchcenter.annotation.NameSpace;
 import com.taobao.csp.switchcenter.bean.Switch;
+import com.taobao.csp.switchcenter.core.SwitchManager;
 
 /**
  * @author haixiao.zhang
  * @date 2021/6/17
  */
-@NameSpace(nameSpace = "txcs-shoppingguide")
+@NameSpace(nameSpace = "txcs-shoppingguide-app")
 public class SxlSwitch {
 
     @AppSwitch(des = "召回商品数", level = Switch.Level.p4)
     public static Integer ITEM_PAGE_SIZE = 60;
+
+    public static Object getValue(String key){
+
+        return SwitchManager.getValue("",key);
+    }
+
 }
