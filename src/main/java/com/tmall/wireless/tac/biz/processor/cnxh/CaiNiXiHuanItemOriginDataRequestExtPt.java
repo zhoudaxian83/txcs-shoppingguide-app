8733f11cd9b27f7547ffc8a91c04aec1872608bc
wb-ljc587473 <wb-ljc587473@alibaba-inc.com>
2021-06-21 14:59:21 +0800

    test

--- src/main/java/com/tmall/wireless/tac/biz/processor/cnxh/CaiNiXiHuanItemOriginDataRequestExtPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/cnxh/CaiNiXiHuanItemOriginDataRequestExtPt.java
@@ -2,35 +2,20 @@ package com.tmall.wireless.tac.biz.processor.cnxh;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 import com.alibaba.cola.extension.Extension;
-import com.alibaba.fastjson.JSON;
 
 import com.tmall.aself.shoppingguide.client.loc.util.AddressUtil;
 import com.tmall.aselfcommon.model.todaycrazy.enums.LogicalArea;
-import com.tmall.txcs.biz.supermarket.extpt.origindata.ConvertUtil;
 import com.tmall.txcs.biz.supermarket.scene.util.MapUtil;
 import com.tmall.txcs.gs.framework.extensions.excutor.SgExtensionExecutor;
-import com.tmall.txcs.gs.framework.extensions.origindata.OriginDataDTO;
-import com.tmall.txcs.gs.framework.extensions.origindata.OriginDataItemQueryExtPt;
 import com.tmall.txcs.gs.framework.extensions.origindata.request.ItemOriginDataRequestExtPt;
 import com.tmall.txcs.gs.framework.model.SgFrameworkContextItem;
-import com.tmall.txcs.gs.framework.support.LogUtil;
-import com.tmall.txcs.gs.model.model.dto.ItemEntity;
-import com.tmall.txcs.gs.model.model.dto.RecommendResponseEntity;
-import com.tmall.txcs.gs.model.model.dto.tpp.RecommendItemEntityDTO;
 import com.tmall.txcs.gs.model.spi.model.RecommendRequest;
-import com.tmall.txcs.gs.spi.recommend.RecommendSpi;
 import com.tmall.wireless.tac.biz.processor.cnxh.enums.O2OChannelEnum;
 import com.tmall.wireless.tac.biz.processor.cnxh.utils.O2OChannelUtil;
 import com.tmall.wireless.tac.biz.processor.common.ScenarioConstantApp;
 import com.tmall.wireless.tac.client.dataservice.TacLogger;
-import io.reactivex.Flowable;
-import org.apache.commons.collections.CollectionUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -41,45 +26,16 @@ import org.springframework.stereotype.Service;
     useCase = ScenarioConstantApp.LOC_TYPE_B2C,
     scenario = ScenarioConstantApp.O2O_CNXH)
 @Service
-public class CaiNiXiHuanOriginDataItemQueryExtPt implements OriginDataItemQueryExtPt {
+public class CaiNiXiHuanItemOriginDataRequestExtPt implements ItemOriginDataRequestExtPt {
 
-    Logger LOGGER = LoggerFactory.getLogger(CaiNiXiHuanOriginDataItemQueryExtPt.class);
-
-    @Autowired
-    RecommendSpi recommendSpi;
     @Autowired
     private SgExtensionExecutor sgExtensionExecutor;
     @Autowired
     TacLogger tacLogger;
 
     @Override
-    public Flowable<OriginDataDTO<ItemEntity>> process(SgFrameworkContextItem context) {
-        RecommendRequest recommendRequest = this.buildTppParams(context);
-        long startTime = System.currentTimeMillis();
-        return (recommendSpi.recommendItem(recommendRequest))
-            .map(recommendResponseEntityResponse -> {
-                // tpp 返回失败
-                if (!recommendResponseEntityResponse.isSuccess()
-                    || recommendResponseEntityResponse.getValue() == null
-                    || CollectionUtils.isEmpty(recommendResponseEntityResponse.getValue().getResult())) {
-                    LogUtil.info(context.getBizScenario().getUniqueIdentity(),
-                        "recommendSpi",
-                        "recommendSpi",
-                        false,
-                        JSON.toJSONString(recommendResponseEntityResponse),
-                        System.currentTimeMillis() - startTime
-                    );
-                    return new OriginDataDTO<>();
-                }
-                LogUtil.info(context.getBizScenario().getUniqueIdentity(),
-                    "recommendSpi",
-                    "recommendSpi",
-                    true,
-                    getTppLogInfo(recommendResponseEntityResponse.getValue()),
-                    System.currentTimeMillis() - startTime
-                );
-                return convert(recommendResponseEntityResponse.getValue());
-            });
+    public RecommendRequest process(SgFrameworkContextItem sgFrameworkContextItem) {
+        return this.buildTppParams(sgFrameworkContextItem);
     }
 
     private RecommendRequest buildTppParams(SgFrameworkContextItem context) {
@@ -140,31 +96,6 @@ public class CaiNiXiHuanOriginDataItemQueryExtPt implements OriginDataItemQueryE
         recommendRequest.setUserId(userId);
         return recommendRequest;
 
-    }
-
-    private String getTppLogInfo(RecommendResponseEntity<RecommendItemEntityDTO> recommendResponseEntityResponse) {
-        StringBuilder log = new StringBuilder("hasMore:" + recommendResponseEntityResponse.isHasMore() + "|");
-        recommendResponseEntityResponse.getResult().forEach(
-            recommendItemEntityDTO -> {
-                log.append(recommendItemEntityDTO.getItemId()).append(",");
-                log.append(recommendItemEntityDTO.getO2oType()).append("|");
-            }
-        );
-        return log.toString();
-    }
-
-    private OriginDataDTO<ItemEntity> convert(RecommendResponseEntity<RecommendItemEntityDTO> recommendResponseEntity) {
-        OriginDataDTO<ItemEntity> originDataDTO = new OriginDataDTO<>();
-        originDataDTO.setHasMore(recommendResponseEntity.isHasMore());
-        originDataDTO.setIndex(recommendResponseEntity.getIndex());
-        originDataDTO.setPvid(recommendResponseEntity.getPvid());
-        originDataDTO.setScm(recommendResponseEntity.getScm());
-        originDataDTO.setTppBuckets(recommendResponseEntity.getTppBuckets());
-        originDataDTO.setResult(recommendResponseEntity
-            .getResult()
-            .stream()
-            .filter(Objects::nonNull).map(ConvertUtil::convert).collect(Collectors.toList()));
-        return originDataDTO;
     }
 
     /**
