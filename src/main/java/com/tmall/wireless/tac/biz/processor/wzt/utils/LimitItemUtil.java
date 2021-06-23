b3a90321dd327334c881f9a95216cd3794af8ca2
wb-ljc587473 <wb-ljc587473@alibaba-inc.com>
2021-06-23 21:34:25 +0800

    优化

--- src/main/java/com/tmall/wireless/tac/biz/processor/wzt/utils/LimitItemUtil.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/wzt/utils/LimitItemUtil.java
@@ -14,13 +14,16 @@ public class LimitItemUtil {
     public static boolean notLimit(ItemLimitDTO itemLimitDTO) {
         //当已售数量大于等于总限制数，个人限制数量大于等于个人限购数沉底处理
         return itemLimitDTO.getUsedCount() < itemLimitDTO.getTotalLimit()
-                && itemLimitDTO.getUserUsedCount() < itemLimitDTO.getUserLimit();
+            && itemLimitDTO.getUserUsedCount() < itemLimitDTO.getUserLimit();
     }
 
     public static List<EntityVO> doLimitItems(List<EntityVO> entityVOList) {
         List<EntityVO> noLimitEntityVOList = Lists.newArrayList();
         entityVOList.forEach(entityVO -> {
-            ItemLimitDTO itemLimitDTO = (ItemLimitDTO) entityVO.get("itemLimit");
+            ItemLimitDTO itemLimitDTO = (ItemLimitDTO)entityVO.get("itemLimit");
+            if (itemLimitDTO == null || itemLimitDTO.getSkuId() == null) {
+                noLimitEntityVOList.add(entityVO);
+            }
             //去掉超出限购的，如果都超出限购则正常放回全部数据
             if (LimitItemUtil.notLimit(itemLimitDTO)) {
                 noLimitEntityVOList.add(entityVO);
