bd4d0e5fbdc81e03e10f58b01a4fe69e02f87646
wb-ljc587473 <wb-ljc587473@alibaba-inc.com>
2021-06-24 11:59:34 +0800

    test

--- src/main/java/com/tmall/wireless/tac/biz/processor/wzt/utils/LimitItemUtil.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/wzt/utils/LimitItemUtil.java
@@ -24,17 +24,13 @@ public class LimitItemUtil {
             return true;
         }
         //兼容只有总限购或个人限购的情况
-        if (itemLimitDTO.getUsedCount() == null) {
-            itemLimitDTO.setUsedCount(0L);
+        boolean totalLimit = itemLimitDTO.getUsedCount() != null && itemLimitDTO.getTotalLimit() != null;
+        boolean userLimit = itemLimitDTO.getUserUsedCount() != null && itemLimitDTO.getUserLimit() != null;
+        if (totalLimit && !userLimit) {
+            return itemLimitDTO.getUsedCount() < itemLimitDTO.getTotalLimit();
         }
-        if (itemLimitDTO.getTotalLimit() == null) {
-            itemLimitDTO.setTotalLimit(0L);
-        }
-        if (itemLimitDTO.getUserUsedCount() == null) {
-            itemLimitDTO.setUserUsedCount(0L);
-        }
-        if (itemLimitDTO.getUserLimit() == null) {
-            itemLimitDTO.setUserLimit(0L);
+        if (!totalLimit && userLimit) {
+            return itemLimitDTO.getUserUsedCount() < itemLimitDTO.getUserLimit();
         }
         //当已售数量大于等于总限制数，个人限制数量大于等于个人限购数沉底处理
         return itemLimitDTO.getUsedCount() < itemLimitDTO.getTotalLimit()
