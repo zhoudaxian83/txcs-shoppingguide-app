3edc5baa332e8cf9cb6c2b7c104c538a88cefb84
yangqing.byq <yangqing.byq@alibaba-inc.com>
2021-06-25 11:48:49 +0800

    add

--- src/main/java/com/tmall/wireless/tac/biz/processor/firstScreenMind/FirstScreenMindContentInfoQueryExtPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/firstScreenMind/FirstScreenMindContentInfoQueryExtPt.java
@@ -1,6 +1,7 @@
 package com.tmall.wireless.tac.biz.processor.firstScreenMind;
 
 import com.alibaba.cola.extension.Extension;
+import com.alibaba.fastjson.JSON;
 import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
@@ -67,8 +68,8 @@ public class FirstScreenMindContentInfoQueryExtPt implements ContentInfoQueryExt
             }
             Result<List<DataEntry>> mgetResult = tairFactorySpi.getOriginDataFailProcessTair().getMultiClusterTairManager().mget(labelSceneNamespace, sKeyList);
             tacLogger.info("***********mgetResult.getValue()*******:"+mgetResult.getValue());
-            LOGGER.info("***********mgetResult.getValue()*******:"+mgetResult.getValue());
-            if (!mgetResult.isSuccess() || CollectionUtils.isEmpty(mgetResult.getValue())) {
+            LOGGER.info("***********mgetResultRequest:{};mgetResult.getValue:{}*******:", JSON.toJSONString(sKeyList), mgetResult.getValue());
+            if (CollectionUtils.isEmpty(mgetResult.getValue())) {
                 return Flowable.just(Response.fail(""));
             }
             List<DataEntry> dataEntryList = mgetResult.getValue();
