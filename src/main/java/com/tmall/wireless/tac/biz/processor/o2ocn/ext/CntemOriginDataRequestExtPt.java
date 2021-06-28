0ac6c79ea3fa0e84f539d265ac29e662dcc285da
1 <haixiao.zhx@alibaba-inc.com>
2021-06-28 14:04:04 +0800

    done

--- src/main/java/com/tmall/wireless/tac/biz/processor/o2ocn/ext/CntemOriginDataRequestExtPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/o2ocn/ext/CntemOriginDataRequestExtPt.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import com.alibaba.cola.extension.Extension;
 import com.alibaba.fastjson.JSON;
 
+import com.tmall.aself.shoppingguide.client.loc.domain.AddressDTO;
 import com.tmall.aself.shoppingguide.client.loc.util.AddressUtil;
 import com.tmall.aselfcommon.model.todaycrazy.enums.LogicalArea;
 import com.tmall.txcs.biz.supermarket.scene.util.MapUtil;
@@ -13,9 +14,11 @@ import com.tmall.txcs.gs.framework.extensions.origindata.request.ItemOriginDataR
 import com.tmall.txcs.gs.framework.model.SgFrameworkContextItem;
 import com.tmall.txcs.gs.model.spi.model.RecommendRequest;
 import com.tmall.wireless.tac.biz.processor.o2ocn.enums.O2OChannelEnum;
+import com.tmall.wireless.tac.biz.processor.o2ocn.utils.Constants;
 import com.tmall.wireless.tac.biz.processor.o2ocn.utils.O2OChannelUtil;
 import com.tmall.wireless.tac.biz.processor.common.ScenarioConstantApp;
 import com.tmall.wireless.tac.client.dataservice.TacLogger;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -27,7 +30,6 @@ import org.springframework.stereotype.Service;
     scenario = ScenarioConstantApp.O2O_CNXH)
 @Service
 public class CntemOriginDataRequestExtPt implements ItemOriginDataRequestExtPt {
-    public static final long APP_ID = 22171;
 
     @Autowired
     TacLogger tacLogger;
@@ -37,53 +39,47 @@ public class CntemOriginDataRequestExtPt implements ItemOriginDataRequestExtPt {
         return this.buildTppParams(sgFrameworkContextItem);
     }
 
+    /**
+     * https://tui.taobao.com/recommend?appid=22171&itemSetIdList=13545&smAreaId=330110&rt1HourStoreId=233930038&userId=1832025789&itemBusinessType=OneHour&isFirstPage=true
+     * @param context
+     * @return
+     */
     private RecommendRequest buildTppParams(SgFrameworkContextItem context) {
-        String pageId = "pageId";
-        String itemBusinessType = "itemBusinessType";
         RecommendRequest recommendRequest = new RecommendRequest();
         Map<String, String> params = new HashMap<>(16);
         String csa = MapUtil.getStringWithDefault(context.getRequestParams(), "csa", "");
+        AddressDTO addressDTO = null;
+        if(StringUtils.isNotBlank(csa)){
+            addressDTO = AddressUtil.parseCSA(csa);
+        }
+        recommendRequest.setAppId(Long.valueOf(Constants.APP_ID));
         String O2OChannel = MapUtil.getStringWithDefault(context.getRequestParams(), "O2OChannel", "");
         O2OChannel = O2OChannel.equals("") ? O2OChannelUtil.getO2OChannel(csa) : O2OChannel;
-        String moduleId = MapUtil.getStringWithDefault(context.getRequestParams(), "moduleId", "");
         Long index = MapUtil.getLongWithDefault(context.getRequestParams(), "index", 0L);
         Long userId = MapUtil.getLongWithDefault(context.getRequestParams(), "userId", 0L);
         Long smAreaId = context.getLocParams().getSmAreaId() == 0 ? LogicalArea.parseByCode(
             AddressUtil.parseCSA(csa).getRegionCode()).getCoreCityCode() : context.getLocParams().getSmAreaId();
         String logicAreaId = AddressUtil.parseCSA(csa).getRegionCode();
-        params.put("itemSetIdSource", "crm");
-        params.put("pmtSource", "sm_manager");
-        params.put("pmtName", "o2oGuessULike");
-        params.put("smAreaId", smAreaId + "");
+        params.put("smAreaId", String.valueOf(smAreaId));
         params.put("userId", String.valueOf(userId));
-        params.put("appid", APP_ID + "");
+        params.put("appid", Constants.APP_ID);
         params.put("logicAreaId", logicAreaId);
-
         params.put("isFirstPage", (index == 0L) + "");
-        if (O2OChannelEnum.ONE_HOUR.getCode().equals(O2OChannel)) {
-            params.put(pageId, "onehourcnxh");
-            params.put(itemBusinessType, "OneHour");
+        if (O2OChannelEnum.ONE_HOUR.getCode().equals(O2OChannel)
+            || (addressDTO!=null && addressDTO.isRt1HourStoreCover())) {
             params.put("rt1HourStoreId", String.valueOf(context.getLocParams().getRt1HourStoreId()));
             params.put("itemBusinessType", "OneHour");
-            params.put("moduleId", moduleId);
-            params.put("itemSetIdList", "13545");
-        } else if (O2OChannelEnum.HALF_DAY.getCode().equals(O2OChannel)) {
-            params.put(pageId, "halfdaycnxh");
-            params.put(itemBusinessType, "HalfDay");
+            params.put("itemSetIdList", Constants.O2O_ITEMSET_ID);
+        } else if (O2OChannelEnum.HALF_DAY.getCode().equals(O2OChannel)
+            || (addressDTO!=null && addressDTO.isRtHalfDayStoreCover())) {
+            params.put("itemBusinessType", "HalfDay");
             params.put("rtHalfDayStoreId", String.valueOf(context.getLocParams().getRtHalfDayStoreId()));
-            params.put("moduleId", moduleId);
-            params.put("itemSetIdList", "13545");
-        } else if (O2OChannelEnum.NEXT_DAY.getCode().equals(O2OChannel)) {
-            params.put(pageId, "nextdaycnxh");
-            params.put(itemBusinessType, "NextDay");
-            params.put("itemSetIdList", "13545");
-        } else if (O2OChannelEnum.ALL_FRESH.getCode().equals(O2OChannel)) {
-            params.put("pageId", "onehourcnxh");
-            params.put(itemBusinessType, "B2C");
-            params.put("itemSetIdList", "198684");
+            params.put("itemSetIdList", Constants.O2O_ITEMSET_ID);
+        }else if(O2OChannelEnum.ALL_FRESH.getCode().equals(O2OChannel) ||
+            (addressDTO!=null && !addressDTO.isRtStoreCover())){
+            params.put("itemBusinessType", "B2C");
+            params.put("itemSetIdList", Constants.ALL_FRESH_ITEMSET_ID);
         }
-        recommendRequest.setAppId(APP_ID);
-        recommendRequest.setLogResult(true);
         recommendRequest.setParams(params);
         recommendRequest.setUserId(userId);
         tacLogger.info("recommendRequest:" + JSON.toJSONString(recommendRequest));
---
+++ src/main/java/com/tmall/wireless/tac/biz/processor/o2ocn/utils/Constants.java
@@ -0,0 +1,16 @@
+package com.tmall.wireless.tac.biz.processor.o2ocn.utils;
+
+/**
+ * @author haixiao.zhang
+ * @date 2021/6/28
+ */
+public class Constants {
+
+    public static final String O2O_ITEMSET_ID = "13545";
+
+    public static final String ALL_FRESH_ITEMSET_ID = "198684";
+
+    public static final String APP_ID = "22171";
+
+
+}
