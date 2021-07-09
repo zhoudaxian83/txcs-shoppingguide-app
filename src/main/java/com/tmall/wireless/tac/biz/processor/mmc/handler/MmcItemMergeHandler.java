96a6c5df98f2cf0b714b26ee8cf1ef5d30a57a42
1 <haixiao.zhx@alibaba-inc.com>
2021-07-09 14:37:49 +0800

    done

---
+++ src/main/java/com/tmall/wireless/tac/biz/processor/mmc/handler/MmcItemMergeHandler.java
@@ -0,0 +1,8 @@
+package com.tmall.wireless.tac.biz.processor.mmc.handler;
+
+/**
+ * @author haixiao.zhang
+ * @date 2021/7/9
+ */
+public class MmcItemMergeHandler {
+}
---
+++ src/main/java/com/tmall/wireless/tac/biz/processor/mmc/handler/MmcItemQueryHandler.java
@@ -0,0 +1,35 @@
+package com.tmall.wireless.tac.biz.processor.mmc.handler;
+
+import com.taobao.freshx.homepage.client.domain.ItemRecallModeDO;
+import com.taobao.poi2.client.result.StoreResult;
+import com.tmall.txcs.gs.spi.recommend.AldSpi;
+import com.tmall.wireless.tac.client.common.TacResult;
+import com.tmall.wireless.tac.client.domain.Context;
+import com.tmall.wireless.tac.client.handler.TacReactiveHandler;
+import io.reactivex.Flowable;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * @author haixiao.zhang
+ * @date 2021/7/9
+ */
+public class MmcItemQueryHandler implements TacReactiveHandler<ItemRecallModeDO> {
+
+
+    @Autowired
+    private AldSpi aldSpi;
+    
+    @Override
+    public Flowable<TacResult<ItemRecallModeDO>> executeFlowable(Context context) throws Exception {
+
+        context.getParams().get("userId");
+        context.getParams().get("source");
+        List<StoreResult> storeResultList = (List<StoreResult>)context.getParams().get("stores");
+
+        ItemRecallModeDO itemRecallModeDO = new ItemRecallModeDO();
+
+        return Flowable.just(TacResult.newResult(itemRecallModeDO));
+    }
+}
