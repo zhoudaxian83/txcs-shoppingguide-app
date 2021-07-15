c6e51010c2b2db4a4ebb06f61d995a31a6a92e8f
yangqing.byq <yangqing.byq@alibaba-inc.com>
2021-07-15 17:51:55 +0800

    add tac result

---
+++ src/main/java/com/tmall/wireless/tac/biz/processor/newTemplate/NewTemplateTestHandler.java
@@ -0,0 +1,31 @@
+package com.tmall.wireless.tac.biz.processor.newTemplate;
+
+
+import com.tmall.tcls.gs.sdk.framework.model.EntityVO;
+import com.tmall.tcls.gs.sdk.framework.model.ItemEntityVO;
+import com.tmall.tcls.gs.sdk.framework.model.SgFrameworkResponse;
+import com.tmall.tcls.gs.sdk.framework.model.context.SgFrameworkContextItem;
+import com.tmall.tcls.gs.sdk.framework.service.SgFrameworkServiceItem;
+import com.tmall.txcs.gs.base.RpmReactiveHandler;
+import com.tmall.wireless.tac.client.common.TacResult;
+import com.tmall.wireless.tac.client.domain.Context;
+import io.reactivex.Flowable;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by yangqing.byq on 2021/7/15.
+ */
+public class NewTemplateTestHandler extends RpmReactiveHandler<SgFrameworkResponse<ItemEntityVO>> {
+
+    @Resource
+    SgFrameworkServiceItem sgFrameworkServiceItem;
+
+    @Override
+    public Flowable<TacResult<SgFrameworkResponse<ItemEntityVO>>> executeFlowable(Context context) throws Exception {
+        SgFrameworkContextItem sgFrameworkContextItem = new SgFrameworkContextItem();
+        return sgFrameworkServiceItem.recommend0(sgFrameworkContextItem).map(
+                TacResult::newResult
+        );
+    }
+}
