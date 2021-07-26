a891dc281b23b76dbecd808e4ef419d296492842
guijian <maxiang.ma@alibaba-inc.com>
2021-07-26 17:07:08 +0800

    类目会场接入智能ui

--- src/main/java/com/tmall/wireless/tac/biz/processor/gul/promotion/GulPromotionHandler.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/gul/promotion/GulPromotionHandler.java
@@ -6,13 +6,18 @@ import com.tmall.txcs.gs.framework.model.SgFrameworkResponse;
 import com.tmall.wireless.tac.client.common.TacResult;
 import com.tmall.wireless.tac.client.domain.Context;
 import io.reactivex.Flowable;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * @author guijian
  */
 public class GulPromotionHandler extends RpmReactiveHandler<SgFrameworkResponse<EntityVO>> {
+    
+    @Autowired
+    GulPromotionScene gulPromotionScene;
+
     @Override
     public Flowable<TacResult<SgFrameworkResponse<EntityVO>>> executeFlowable(Context context) throws Exception {
-        return null;
+        return gulPromotionScene.recommend(context);
     }
 }
