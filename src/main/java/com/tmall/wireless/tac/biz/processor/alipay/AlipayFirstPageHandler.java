5d6b56bb920fc23cbec8895762c6d6a24db206e8
yangqing.byq <yangqing.byq@alibaba-inc.com>
2021-08-06 10:03:15 +0800

    add

--- src/main/java/com/tmall/wireless/tac/biz/processor/alipay/AlipayFirstPageHandler.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/alipay/AlipayFirstPageHandler.java
@@ -1,11 +1,15 @@
 package com.tmall.wireless.tac.biz.processor.alipay;
 
+import com.alipay.recmixer.common.service.facade.model.MixerCollectRecRequest;
 import com.alipay.recmixer.common.service.facade.model.MixerCollectRecResult;
+import com.alipay.tradecsa.common.service.spi.request.MiddlePageSPIRequest;
 import com.tmall.txcs.gs.base.RpmReactiveHandler;
 import com.tmall.wireless.tac.client.common.TacResult;
 import com.tmall.wireless.tac.client.domain.Context;
 import io.reactivex.Flowable;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 /**
  * Created by yangqing.byq on 2021/8/1.
@@ -16,7 +20,15 @@ public class AlipayFirstPageHandler extends RpmReactiveHandler<MixerCollectRecRe
     public Flowable<TacResult<MixerCollectRecResult>> executeFlowable(Context context) throws Exception {
 
         MixerCollectRecResult mixerCollectRecResult = new MixerCollectRecResult();
-        mixerCollectRecResult.setErrorMsg("jinzhou test");
+        Object param = Optional.of(context).map(Context::getParams).map(m -> m.get(AlipayMiddlePageHandler.PARAM_KEY)).orElse(null);
+
+        if (!(param instanceof MixerCollectRecRequest)) {
+            mixerCollectRecResult.setErrorCode("PARAMS_IS_NULL");
+            return Flowable.just(TacResult.newResult(mixerCollectRecResult));
+        }
+        MixerCollectRecRequest mixerCollectRecRequest = (MixerCollectRecRequest) param;
+
+        mixerCollectRecResult.setErrorMsg(mixerCollectRecRequest.getUtdid());
         return Flowable.just(TacResult.newResult(mixerCollectRecResult));
     }
 }
--- src/main/java/com/tmall/wireless/tac/biz/processor/alipay/AlipayMiddlePageHandler.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/alipay/AlipayMiddlePageHandler.java
@@ -1,21 +1,36 @@
 package com.tmall.wireless.tac.biz.processor.alipay;
 
 import com.alipay.recmixer.common.service.facade.model.MixerCollectRecResult;
+import com.alipay.tradecsa.common.service.spi.request.MiddlePageSPIRequest;
+import com.alipay.tradecsa.common.service.spi.response.MiddlePageSPIResponse;
 import com.tmall.txcs.gs.base.RpmReactiveHandler;
 import com.tmall.wireless.tac.client.common.TacResult;
 import com.tmall.wireless.tac.client.domain.Context;
 import io.reactivex.Flowable;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 /**
  * Created by yangqing.byq on 2021/8/1.
  */
 @Component
-public class AlipayMiddlePageHandler extends RpmReactiveHandler<MixerCollectRecResult> {
+public class AlipayMiddlePageHandler extends RpmReactiveHandler<MiddlePageSPIResponse> {
+
+    public static final String PARAM_KEY = "paramsKey";
+
     @Override
-    public Flowable<TacResult<MixerCollectRecResult>> executeFlowable(Context context) throws Exception {
-        MixerCollectRecResult mixerCollectRecResult = new MixerCollectRecResult();
-        mixerCollectRecResult.setErrorMsg("jinzhou test middle");
-        return Flowable.just(TacResult.newResult(mixerCollectRecResult));
+    public Flowable<TacResult<MiddlePageSPIResponse>> executeFlowable(Context context) throws Exception {
+        MiddlePageSPIResponse middlePageSPIResponse = new MiddlePageSPIResponse();
+        Object param = Optional.of(context).map(Context::getParams).map(m -> m.get(PARAM_KEY)).orElse(null);
+
+        if (!(param instanceof MiddlePageSPIRequest)) {
+            middlePageSPIResponse.setErrorCode("PARAMS_IS_NULL");
+            return Flowable.just(TacResult.newResult(middlePageSPIResponse));
+        }
+        MiddlePageSPIRequest middlePageSPIRequest = (MiddlePageSPIRequest) param;
+
+        middlePageSPIResponse.setErrorMsg("jinzhou test middle");
+        return Flowable.just(TacResult.newResult(middlePageSPIResponse));
     }
 }
