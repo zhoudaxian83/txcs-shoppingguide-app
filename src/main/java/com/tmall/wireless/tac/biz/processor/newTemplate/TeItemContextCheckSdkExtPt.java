b314073d89dee2a9aaac316069d8872622e3b82d
yangqing.byq <yangqing.byq@alibaba-inc.com>
2021-08-07 02:00:48 +0800

    add

--- src/main/java/com/tmall/wireless/tac/biz/processor/newTemplate/TeItemContextCheckSdkExtPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/newTemplate/TeItemContextCheckSdkExtPt.java
@@ -5,14 +5,13 @@ import com.tmall.tcls.gs.sdk.ext.annotation.SdkPackage;
 import com.tmall.tcls.gs.sdk.ext.extension.Register;
 import com.tmall.tcls.gs.sdk.framework.extensions.item.contextcheck.ContextCheckResult;
 import com.tmall.tcls.gs.sdk.framework.extensions.item.contextcheck.ItemContextCheckSdkExtPt;
-
 import com.tmall.wireless.tac.client.domain.Context;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by yangqing.byq on 2021/8/6.
  */
-@Service
+@Component
 @SdkExtension(bizId = "cc")
 @SdkPackage(packageName = "package")
 public class TeItemContextCheckSdkExtPt extends Register implements ItemContextCheckSdkExtPt {
