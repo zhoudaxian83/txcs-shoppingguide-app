427f8c7d07f8d165bf4b445724c01e1befea1c15
yangqing.byq <yangqing.byq@alibaba-inc.com>
2021-08-07 01:25:07 +0800

    fix

--- src/main/java/com/tmall/wireless/tac/biz/processor/newTemplate/TeItemContextCheckSdkExtPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/newTemplate/TeItemContextCheckSdkExtPt.java
@@ -1,10 +1,11 @@
 package com.tmall.wireless.tac.biz.processor.newTemplate;
 
+import com.tmall.tcls.gs.sdk.ext.annotation.SdkExtension;
+import com.tmall.tcls.gs.sdk.ext.annotation.SdkPackage;
+import com.tmall.tcls.gs.sdk.ext.extension.Register;
 import com.tmall.tcls.gs.sdk.framework.extensions.item.contextcheck.ContextCheckResult;
 import com.tmall.tcls.gs.sdk.framework.extensions.item.contextcheck.ItemContextCheckSdkExtPt;
-import com.tmall.txcs.gs.sdk.ext.annotation.Extension;
-import com.tmall.txcs.gs.sdk.ext.annotation.ProductPackage;
-import com.tmall.txcs.gs.sdk.ext.extension.Register;
+
 import com.tmall.wireless.tac.client.domain.Context;
 import org.springframework.stereotype.Service;
 
@@ -12,8 +13,8 @@ import org.springframework.stereotype.Service;
  * Created by yangqing.byq on 2021/8/6.
  */
 @Service
-@Extension(bizId = "cc")
-@ProductPackage(packageName = "package")
+@SdkExtension(bizId = "cc")
+@SdkPackage(packageName = "package")
 public class TeItemContextCheckSdkExtPt extends Register implements ItemContextCheckSdkExtPt {
     @Override
     public ContextCheckResult process(Context context) {
