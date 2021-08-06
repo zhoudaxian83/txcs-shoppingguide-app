5136114224909dc14c539e49adb37dfda16619fd
1 <haixiao.zhx@alibaba-inc.com>
2021-08-06 10:30:26 +0800

    done

--- src/main/java/com/tmall/wireless/tac/biz/processor/o2ocn/utils/Constants.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/o2ocn/utils/Constants.java
@@ -10,7 +10,7 @@ public class Constants {
 
     public static final String ALL_FRESH_ITEMSET_ID = "198684";
 
-    public static final String APP_ID = "22171";
+    public static final String APP_ID = "27154";
 
 
 }
