31db10ebf46b6bf491ba9cec325cd5c661d6021d
wb-ljc587473 <wb-ljc587473@alibaba-inc.com>
2021-08-10 15:04:54 +0800

    1,init

--- src/main/java/com/tmall/wireless/tac/biz/processor/chaohaotou/ChaoHaoTouHandler.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/chaohaotou/ChaoHaoTouHandler.java
@@ -3,7 +3,6 @@ package com.tmall.wireless.tac.biz.processor.chaohaotou;
 import com.tmall.txcs.gs.base.RpmReactiveHandler;
 import com.tmall.txcs.gs.framework.model.EntityVO;
 import com.tmall.txcs.gs.framework.model.SgFrameworkResponse;
-import com.tmall.wireless.tac.biz.processor.wzt.WuZheTianPageBannerItemInfoScene;
 import com.tmall.wireless.tac.client.common.TacResult;
 import com.tmall.wireless.tac.client.domain.Context;
 import io.reactivex.Flowable;
