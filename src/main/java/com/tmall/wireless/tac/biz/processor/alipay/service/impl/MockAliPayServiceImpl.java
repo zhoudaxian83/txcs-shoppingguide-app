e72c4021f9b3ec3f841140afa12cdd53659577f1
yangqing.byq <yangqing.byq@alibaba-inc.com>
2021-08-10 17:54:36 +0800

    add field

--- src/main/java/com/tmall/wireless/tac/biz/processor/alipay/service/impl/MockAliPayServiceImpl.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/alipay/service/impl/MockAliPayServiceImpl.java
@@ -44,7 +44,11 @@ public class MockAliPayServiceImpl implements IAliPayService {
 
     private ServiceContentRec getServiceContent(String title) {
         ServiceContentRec serviceContentRec = new ServiceContentRec();
-        serviceContentRec.setItemId("12345");
+        serviceContentRec.setItemId("600561956069");
+        serviceContentRec.setImgUrl("//img.alicdn.com/imgextra/i2/725677994/O1CN01OcEXyM28vIqflAKlN_!!2-item_pic.png");
+        serviceContentRec.setSubTitle("正品保障");
+        serviceContentRec.setTitle("按压式无氟1-12岁换牙期香橙味儿童牙膏");
+        serviceContentRec.setActionLink("//detail.tmall.com/item.htm?&id=638125586345&locType=B2C&scm=1007.35385.224506.0.FF-hyhsfZ_appId-25385_I-_Q-7d00643e-eb7c-4728-8a0a-2ee55ae9df71_D-638125586345_T-ITEM_businessType-B2C_predCTR-0_predCVR-0_predCTCVR-0_calibCTR-0_calibCVR-0_calibCTCVR-0_finalScore-0-FF");
         serviceContentRec.setBizCode(AliPayConstant.BIZ_CODE);
         serviceContentRec.setSource(AliPayConstant.SOURCE);
         serviceContentRec.setTitle(title);
