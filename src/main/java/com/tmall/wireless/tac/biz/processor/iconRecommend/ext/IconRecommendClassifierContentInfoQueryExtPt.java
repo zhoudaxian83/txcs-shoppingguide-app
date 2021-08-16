2b235cf637c3c833a6be77a7934a5dd0991fcbdb
zhuomin.hzm <zhuomin.hzm@alibaba-inc.com>
2021-08-16 20:38:16 +0800

    icon加购后推荐

--- src/main/java/com/tmall/wireless/tac/biz/processor/iconRecommend/ext/IconRecommendClassifierContentInfoQueryExtPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/iconRecommend/ext/IconRecommendClassifierContentInfoQueryExtPt.java
@@ -3,12 +3,6 @@ package com.tmall.wireless.tac.biz.processor.iconRecommend.ext;
 import com.alibaba.cola.extension.Extension;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
-import com.taobao.tair.DataEntry;
-import com.taobao.tair.Result;
-import com.tmall.aselfcommon.model.gcs.enums.GcsMarketChannel;
-import com.tmall.aselfcommon.model.scene.domain.TairSceneDTO;
-import com.tmall.aselfcommon.model.scene.enums.SceneType;
-import com.tmall.aselfcommon.model.scene.valueobject.SceneDetailValue;
 import com.tmall.txcs.gs.framework.extensions.content.ContentInfoQueryExtPt;
 import com.tmall.txcs.gs.framework.extensions.origindata.OriginDataDTO;
 import com.tmall.txcs.gs.framework.model.SgFrameworkContextContent;
@@ -16,22 +10,10 @@ import com.tmall.txcs.gs.framework.model.constant.ScenarioConstant;
 import com.tmall.txcs.gs.model.Response;
 import com.tmall.txcs.gs.model.content.ContentInfoDTO;
 import com.tmall.txcs.gs.model.model.dto.ContentEntity;
-import com.tmall.txcs.gs.spi.recommend.TairFactorySpi;
 import com.tmall.wireless.tac.biz.processor.common.ScenarioConstantApp;
-import com.tmall.wireless.tac.biz.processor.firstScreenMind.enums.FrontBackMapEnum;
-import com.tmall.wireless.tac.biz.processor.firstScreenMind.enums.RenderContentTypeEnum;
-import com.tmall.wireless.tac.biz.processor.firstScreenMind.enums.RenderErrorEnum;
-import com.tmall.wireless.tac.biz.processor.firstScreenMind.model.content.SubContentModel;
-import com.tmall.wireless.tac.biz.processor.firstScreenMind.utils.RenderCheckUtil;
-import com.tmall.wireless.tac.biz.processor.firstScreenMind.utils.RenderLangUtil;
-import com.tmall.wireless.tac.client.dataservice.TacLogger;
 import io.reactivex.Flowable;
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -50,13 +32,34 @@ public class IconRecommendClassifierContentInfoQueryExtPt implements ContentInfo
     public Flowable<Response<Map<Long, ContentInfoDTO>>> process(SgFrameworkContextContent sgFrameworkContextContent) {
 
         Map<Long, ContentInfoDTO> resMap = Maps.newHashMap();
-        Map<String, Object> contentInfo = Maps.newHashMap();
-        contentInfo.put("url","111");
-        ContentInfoDTO contentInfoDTO = new ContentInfoDTO();
-        contentInfoDTO.setContentInfo(contentInfo);
-        resMap.put(5233L,contentInfoDTO);
-        resMap.put(322385L,contentInfoDTO);
+
+        List<ContentEntity> contentEntities  = Optional.of(sgFrameworkContextContent)
+                .map(SgFrameworkContextContent::getContentEntityOriginDataDTO)
+                .map(OriginDataDTO::getResult)
+                .orElse(Lists.newArrayList());
+
+        for (ContentEntity contentEntity : contentEntities) {
+            Map<String, Object> contentInfo = Maps.newHashMap();
+            Long contentId = contentEntity.getContentId();
+            contentInfo.put("contentId", contentId);
+            contentInfo.put("uniqueId", contentEntity.getUnique_id());
+            contentInfo.put("trackPoint", contentEntity.getTrack_point());
+            contentInfo.put("Rn", contentEntity.getRn());
+
+            ContentInfoDTO contentInfoDTO = new ContentInfoDTO();
+            contentInfoDTO.setContentInfo(contentInfo);
+            resMap.put(contentId, contentInfoDTO);
+        }
         return Flowable.just(Response.success(resMap));
+
+//        Map<Long, ContentInfoDTO> resMap = Maps.newHashMap();
+//        Map<String, Object> contentInfo = Maps.newHashMap();
+//        contentInfo.put("url","111");
+//        ContentInfoDTO contentInfoDTO = new ContentInfoDTO();
+//        contentInfoDTO.setContentInfo(contentInfo);
+//        resMap.put(5233L,contentInfoDTO);
+//        resMap.put(322385L,contentInfoDTO);
+//        return Flowable.just(Response.success(resMap));
     }
 }
 
