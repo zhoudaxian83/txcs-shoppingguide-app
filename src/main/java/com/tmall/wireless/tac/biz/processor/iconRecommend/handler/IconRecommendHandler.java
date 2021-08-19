f570c25034fd18b27fd013b436abce4bc99193f1
zhuomin.hzm <zhuomin.hzm@alibaba-inc.com>
2021-08-19 14:16:04 +0800

    icon加购后推荐

--- src/main/java/com/tmall/wireless/tac/biz/processor/iconRecommend/handler/IconRecommendHandler.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/iconRecommend/handler/IconRecommendHandler.java
@@ -35,10 +35,17 @@ public class IconRecommendHandler extends RpmReactiveHandler<SgFrameworkResponse
         Flowable<TacResult<SgFrameworkResponse<ContentVO>>> scene = iconRecommendService.recommend(context, ConstantValue.SCENE_WORD);
 
         return Flowable.zip(classifier, scene, (c, s) -> {
-            List<ContentVO> contentVOList = c.getData().getItemAndContentList();
-            contentVOList.addAll(s.getData().getItemAndContentList());
+            List<ContentVO> classifierContentVOList = c.getData().getItemAndContentList();
+            List<ContentVO> sceneContentVOList = s.getData().getItemAndContentList();
+            // 场景词item数量少于六个，分类词打底
+            if (sceneContentVOList != null
+                    && sceneContentVOList.size() > 0
+                    && sceneContentVOList.get(0).getJSONArray("items").size() > 6) {
+                    classifierContentVOList.set(-1, sceneContentVOList.get(0));
+            }
+//            classifierContentVOList.addAll(s.getData().getItemAndContentList());
             // 取第1个物品照片作为icon图片
-            for (ContentVO contentVO : contentVOList) {
+            for (ContentVO contentVO : classifierContentVOList) {
                 contentVO.put("iconPic", contentVO.getJSONArray("items").getJSONObject(0).getString("itemImg"));
             }
             return c;
