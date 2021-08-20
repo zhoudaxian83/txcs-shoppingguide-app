802f63aad7f361a4fa9f6c554e4f36e231fa250a
wl <guohui.wangguohui@alibaba-inc.com>
2021-08-20 15:25:16 +0800

    提交

--- src/main/java/com/tmall/wireless/tac/biz/processor/huichang/service/HallCommonContentRequestProxy.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/huichang/service/HallCommonContentRequestProxy.java
@@ -1,39 +1,19 @@
 package com.tmall.wireless.tac.biz.processor.huichang.service;
 
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 
 import com.alibaba.aladdin.lamp.domain.response.GeneralItem;
-import com.alibaba.fastjson.JSON;
 
 import com.google.common.collect.Lists;
-import com.tmall.hades.monitor.print.HadesLogUtil;
 import com.tmall.tcls.gs.sdk.ext.BizScenario;
 import com.tmall.tcls.gs.sdk.framework.model.ContentVO;
 import com.tmall.tcls.gs.sdk.framework.model.SgFrameworkResponse;
 import com.tmall.tcls.gs.sdk.framework.service.ShoppingguideSdkContentService;
-import com.tmall.txcs.biz.supermarket.scene.UserParamsKeyConstant;
-import com.tmall.txcs.biz.supermarket.scene.util.CsaUtil;
-import com.tmall.txcs.biz.supermarket.scene.util.MapUtil;
-import com.tmall.txcs.gs.framework.model.EntityVO;
-import com.tmall.txcs.gs.framework.model.SgFrameworkContext;
-import com.tmall.txcs.gs.framework.model.SgFrameworkContextItem;
-import com.tmall.txcs.gs.framework.model.meta.ItemGroupMetaInfo;
-import com.tmall.txcs.gs.framework.model.meta.ItemInfoSourceMetaInfo;
-import com.tmall.txcs.gs.framework.model.meta.ItemMetaInfo;
-import com.tmall.txcs.gs.framework.service.impl.SgFrameworkServiceItem;
-import com.tmall.txcs.gs.model.biz.context.PageInfoDO;
-import com.tmall.txcs.gs.model.biz.context.SceneInfo;
-import com.tmall.txcs.gs.model.biz.context.UserDO;
-import com.tmall.wireless.tac.biz.processor.common.ScenarioConstantApp;
 import com.tmall.wireless.tac.biz.processor.todaycrazy.LimitTimeBuyScene;
 import com.tmall.wireless.tac.biz.processor.todaycrazy.utils.AldInfoUtil;
 import com.tmall.wireless.tac.client.common.TacResult;
 import com.tmall.wireless.tac.client.dataservice.TacLogger;
-import com.tmall.wireless.tac.client.domain.Context;
 import com.tmall.wireless.tac.client.domain.RequestContext4Ald;
-import com.tmall.wireless.tac.client.domain.UserInfo;
 import io.reactivex.Flowable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
