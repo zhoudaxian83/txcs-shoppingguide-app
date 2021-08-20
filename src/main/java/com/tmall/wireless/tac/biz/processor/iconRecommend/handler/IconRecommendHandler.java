6faeb975e0d8fac8f9767a81d436cae46d4e60c6
zhuomin.hzm <zhuomin.hzm@alibaba-inc.com>
2021-08-20 15:49:15 +0800

    icon加购后推荐

--- src/main/java/com/tmall/wireless/tac/biz/processor/iconRecommend/handler/IconRecommendHandler.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/iconRecommend/handler/IconRecommendHandler.java
@@ -13,6 +13,7 @@ import io.reactivex.Flowable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -38,23 +39,31 @@ public class IconRecommendHandler extends RpmReactiveHandler<SgFrameworkResponse
         return Flowable.zip(classifier, scene, (c, s) -> {
             List<ContentVO> classifierContentVOList = c.getData().getItemAndContentList();
             List<ContentVO> sceneContentVOList = s.getData().getItemAndContentList();
+            List<ContentVO> res = new ArrayList<>();
             // 场景词item数量少于六个，分类词打底
-            if (sceneContentVOList != null
-                    && sceneContentVOList.size() > 0
-                    && sceneContentVOList.get(0).getJSONArray("items") != null
-                    && sceneContentVOList.get(0).getJSONArray("items").size() >= 6) {
-                    classifierContentVOList.set(classifierContentVOList.size() - 1, sceneContentVOList.get(0));
+            if (sceneContentVOList != null && sceneContentVOList.size() > 0 && classifierContentVOList != null && classifierContentVOList.size() > 0) {
+                if (sceneContentVOList.get(0).getJSONArray("items") != null && sceneContentVOList.get(0).getJSONArray("items").size() < 6) {
+                    res = classifierContentVOList;
+                } else {
+                    res.addAll(classifierContentVOList.subList(0, 2));
+                    res.addAll(sceneContentVOList);
+                }
             }
+            // 总数量少于4个，传空下发
+//            if (!res.isEmpty() && res.size() <= 4) {
+//                return TacResult.newResult(new SgFrameworkResponse<ContentVO>());
+//            }
+
             // 取第1个物品照片作为icon图片
-            for (ContentVO contentVO : classifierContentVOList) {
-                if (contentVO != null
-                        && contentVO.getJSONArray("items") != null
-                        && contentVO.getJSONArray("items").size() < 6) {
+            for (ContentVO contentVO : res) {
+                if (contentVO != null && contentVO.getJSONArray("items") != null && contentVO.getJSONArray("items").size() < 6) {
                     return TacResult.newResult(new SgFrameworkResponse<ContentVO>());
                 }
                 contentVO.put("iconPic", contentVO.getJSONArray("items").getJSONObject(0).getString("itemImg"));
             }
-            return c;
-        }).onErrorReturn((r -> TacResult.errorResult("")));
+            SgFrameworkResponse<ContentVO> sgFrameworkResponse = new SgFrameworkResponse<>();
+            sgFrameworkResponse.setItemAndContentList(res);
+            return TacResult.newResult(sgFrameworkResponse);
+        });
     }
 }
