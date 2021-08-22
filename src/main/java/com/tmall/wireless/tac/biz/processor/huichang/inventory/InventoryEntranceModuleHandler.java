20e4c51b80b3e0c130d773c58515d846239ed884
wl <guohui.wangguohui@alibaba-inc.com>
2021-08-22 15:55:19 +0800

    提交

--- src/main/java/com/tmall/wireless/tac/biz/processor/huichang/inventory/InventoryEntranceModuleHandler.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/huichang/inventory/InventoryEntranceModuleHandler.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.alibaba.aladdin.lamp.domain.response.GeneralItem;
 
 import com.tmall.tcls.gs.sdk.ext.BizScenario;
+import com.tmall.wireless.tac.biz.processor.huichang.common.constant.HallScenarioConstant;
 import com.tmall.wireless.tac.biz.processor.huichang.service.HallCommonContentRequestProxy;
 import com.tmall.wireless.tac.client.common.TacResult;
 import com.tmall.wireless.tac.client.domain.RequestContext4Ald;
@@ -24,7 +25,9 @@ public class InventoryEntranceModuleHandler extends TacReactiveHandler4Ald {
 
     @Override
     public Flowable<TacResult<List<GeneralItem>>> executeFlowable(RequestContext4Ald requestContext4Ald) throws Exception {
-        BizScenario bizScenario = BizScenario.valueOf("", "", "");
+        BizScenario bizScenario = BizScenario.valueOf(HallScenarioConstant.HALL_SCENARIO_BIZ_ID,
+            HallScenarioConstant.HALL_SCENARIO_USE_CASE_B2C,
+            HallScenarioConstant.HALL_SCENARIO_SCENARIO_INVENTORY_ENTRANCE_MODULE);
         bizScenario.addProducePackage("huichang");
         return hallCommonContentRequestProxy.recommend(requestContext4Ald, bizScenario);
     }
