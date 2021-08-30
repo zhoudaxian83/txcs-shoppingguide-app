6009d7a98bb55abb0066eb31fb4469038e0c4b8a
zhuomin.hzm <zhuomin.hzm@alibaba-inc.com>
2021-08-30 16:21:10 +0800

    mock

--- src/main/java/com/tmall/wireless/tac/biz/processor/iconRecommend/constant/ConstantValue.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/iconRecommend/constant/ConstantValue.java
@@ -15,4 +15,19 @@ public class ConstantValue {
      * 场景词
      */
     public static final String SCENE_WORD = "SCENE_WORD";
+
+    /**
+     * ald bizId
+     */
+    public static final Integer ALD_BIZ_ID = 101;
+
+    /**
+     * ald 资源位id
+     */
+    public static final String ALD_RES_ID = "19181533";
+
+    /**
+     * ald call source
+     */
+    public static final String ALD_CALL_SOURCE = "txcs-shoppingguide";
 }
--- src/main/java/com/tmall/wireless/tac/biz/processor/iconRecommend/ext/IconRecommendSceneContentOriginDataRequestExtPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/iconRecommend/ext/IconRecommendSceneContentOriginDataRequestExtPt.java
@@ -1,5 +1,10 @@
 package com.tmall.wireless.tac.biz.processor.iconRecommend.ext;
 
+import com.alibaba.aladdin.lamp.domain.request.Request;
+import com.alibaba.aladdin.lamp.domain.request.RequestItem;
+import com.alibaba.aladdin.lamp.domain.request.modules.LocationInfo;
+import com.alibaba.aladdin.lamp.domain.response.ResResponse;
+import com.alibaba.aladdin.lamp.domain.user.UserProfile;
 import com.alibaba.cola.extension.Extension;
 import com.alibaba.fastjson.JSON;
 import com.google.common.base.Joiner;
@@ -13,11 +18,15 @@ import com.tmall.txcs.gs.model.biz.context.LocParams;
 import com.tmall.txcs.gs.model.biz.context.PageInfoDO;
 import com.tmall.txcs.gs.model.biz.context.UserDO;
 import com.tmall.txcs.gs.model.spi.model.RecommendRequest;
+import com.tmall.txcs.gs.spi.recommend.AldSpi;
 import com.tmall.wireless.tac.biz.processor.common.ScenarioConstantApp;
+import com.tmall.wireless.tac.biz.processor.iconRecommend.constant.ConstantValue;
 import com.tmall.wireless.tac.client.dataservice.TacLogger;
+import org.apache.commons.collections.MapUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -35,11 +44,13 @@ public class IconRecommendSceneContentOriginDataRequestExtPt implements ContentO
     @Autowired
     TacLogger logger;
 
+    @Autowired
+    private AldSpi aldSpi;
+
     public static final Long appId = 26765L;
 
     @Override
     public RecommendRequest process(SgFrameworkContextContent sgFrameworkContextContent) {
-
         // https://tuipre.taobao.com/recommend?_mtop_rl_url_=true&rtHalfDayStoreId=236635411&commerce=B2C,O2O
         // &smAreaId=330110&_devEnv_=1&regionCode=107&appid=26765&index=0&pageSize=8&sceneSet=intelligentCombinationItems
         // &itemIds=535780624508,624851528087&exposureSwitch=false&maxItemReturn=21&manufacturerId=device_id_12345
@@ -50,11 +61,19 @@ public class IconRecommendSceneContentOriginDataRequestExtPt implements ContentO
                 .map(UserDO::getUserId)
                 .orElse(0L));
         recommendRequest.setLogResult(true);
-
         Map<String, String> params = Maps.newHashMap();
+        // 阿拉丁拉取鸿雁配置
+        Map<String, ResResponse> resResponseMap= aldSpi.queryAldInfoSync(buildAldRequest(sgFrameworkContextContent));
+        if (MapUtils.isNotEmpty(resResponseMap)) {
+            List<Map<String, Object>> dataList = (List<Map<String, Object>>) resResponseMap.get(ConstantValue.ALD_RES_ID).get("data");
+            if (dataList != null && dataList.size() > 0) {
+                String contentId = (String)dataList.get(0).get("ContentId");
+                params.put("sceneSet", contentId);
+            }
+        }
+
         // 固定前缀
         params.put("_mtop_rl_url_", "true");
-//        params.put("rtHalfDayStoreId", "236635411");
         params.put("commerce", "B2C");
         params.put("smAreaId", Optional.ofNullable(sgFrameworkContextContent).map(SgFrameworkContext::getLocParams).map(LocParams::getSmAreaId).orElse(330110L).toString());
         params.put("_devEnv_", "1");
@@ -66,7 +85,6 @@ public class IconRecommendSceneContentOriginDataRequestExtPt implements ContentO
         }
         // 其余参数
         // 曝光过滤数据
-        params.put("sceneSet", "intelligentCombinationItems");
         params.put("itemIds", Optional.ofNullable(sgFrameworkContextContent)
                 .map(SgFrameworkContext::getRequestParams)
                 .map(map -> map.get("itemIdList"))
@@ -88,5 +106,24 @@ public class IconRecommendSceneContentOriginDataRequestExtPt implements ContentO
 
         logger.info("[SceneContentOriginDataRequestExtPt] tppRequest: " + JSON.toJSONString(recommendRequest));
         return recommendRequest;
+    }
+
+    private Request buildAldRequest(SgFrameworkContextContent sgFrameworkContextContent){
+        Request request = new Request();
+        request.setBizId(ConstantValue.ALD_BIZ_ID);
+        request.setCallSource(ConstantValue.ALD_CALL_SOURCE);
+        request.setDebug(false);
+        RequestItem item = new RequestItem();
+        item.setResId(ConstantValue.ALD_RES_ID);
+        UserProfile userProfile = request.getUserProfile();
+        userProfile.setUserId(sgFrameworkContextContent.getUserDO().getUserId());
+        request.setRequestItems(Lists.newArrayList(item));
+        //地址信息
+        LocationInfo locationInfo = request.getLocationInfo();
+        //四级地址
+        locationInfo.setCityLevel4(String.valueOf(sgFrameworkContextContent.getLocParams().getSmAreaId()));
+        List<String> wdkCodes = Lists.newArrayList();
+        locationInfo.setWdkCodes(wdkCodes);
+        return request;
     }
 }
