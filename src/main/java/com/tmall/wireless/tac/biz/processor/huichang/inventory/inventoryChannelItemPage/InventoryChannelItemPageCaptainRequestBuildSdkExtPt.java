63f49c2e4bc27ea70b35f515d98259c259fd1f82
likunlin.lkl <likunlin.lkl@alibaba-inc.com>
2021-08-30 14:35:06 +0800

    增加日志

--- src/main/java/com/tmall/wireless/tac/biz/processor/huichang/inventory/inventoryChannelItemPage/InventoryChannelItemPageCaptainRequestBuildSdkExtPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/huichang/inventory/inventoryChannelItemPage/InventoryChannelItemPageCaptainRequestBuildSdkExtPt.java
@@ -7,14 +7,20 @@ import com.tmall.tcls.gs.sdk.framework.extensions.item.iteminfo.CaptainRequestBu
 import com.tmall.tcls.gs.sdk.sm.iteminfo.bysource.captain.DefaultCaptainRequestBuildSdkExtPt;
 import com.tmall.wireless.store.spi.render.model.RenderRequest;
 import com.tmall.wireless.tac.biz.processor.huichang.common.constant.HallScenarioConstant;
+import com.tmall.wireless.tac.client.dataservice.TacLogger;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @SdkExtension(bizId = HallScenarioConstant.HALL_SCENARIO_BIZ_ID,
         useCase = HallScenarioConstant.HALL_SCENARIO_USE_CASE_B2C,
         scenario = HallScenarioConstant.HALL_SCENARIO_SCENARIO_INVENTORY_CHANNEL_ITEM_PAGE)
 public class InventoryChannelItemPageCaptainRequestBuildSdkExtPt extends DefaultCaptainRequestBuildSdkExtPt implements CaptainRequestBuildSdkExtPt {
+    @Autowired
+    TacLogger tacLogger;
     private final String captainSceneCode = "supermarket.hall.inventory";
+
     @Override
     public RenderRequest process(CaptainRequestBuildRequest captainRequestBuildRequest) {
+        tacLogger.debug("扩展点InventoryChannelItemPageCaptainRequestBuildSdkExtPt");
         RenderRequest renderRequest = super.process(captainRequestBuildRequest);
         QueryOptionDO option = renderRequest.getOption();
         option.setSceneCode(captainSceneCode);
