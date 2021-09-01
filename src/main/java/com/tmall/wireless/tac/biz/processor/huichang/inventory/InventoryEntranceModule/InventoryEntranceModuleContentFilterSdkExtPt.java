2ca32b472641b560220aef8a8598b4779f9a5570
wl <guohui.wangguohui@alibaba-inc.com>
2021-09-01 22:46:16 +0800

    提交代码

---
+++ src/main/java/com/tmall/wireless/tac/biz/processor/huichang/inventory/InventoryEntranceModule/InventoryEntranceModuleContentFilterSdkExtPt.java
@@ -0,0 +1,32 @@
+package com.tmall.wireless.tac.biz.processor.huichang.inventory.InventoryEntranceModule;
+
+import com.alibaba.fastjson.JSON;
+
+import com.tmall.tcls.gs.sdk.ext.annotation.SdkExtension;
+import com.tmall.tcls.gs.sdk.ext.extension.Register;
+import com.tmall.tcls.gs.sdk.framework.extensions.content.vo.ContentFilterSdkExtPt;
+import com.tmall.tcls.gs.sdk.framework.model.ContentVO;
+import com.tmall.tcls.gs.sdk.framework.model.SgFrameworkResponse;
+import com.tmall.tcls.gs.sdk.framework.model.context.SgFrameworkContextContent;
+import com.tmall.wireless.tac.biz.processor.huichang.common.constant.HallScenarioConstant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 过滤掉 售罄和不卖的商品，进而过滤掉没挂品的场景
+ */
+@SdkExtension(bizId = HallScenarioConstant.HALL_SCENARIO_BIZ_ID,
+    useCase = HallScenarioConstant.HALL_SCENARIO_USE_CASE_B2C,
+    scenario = HallScenarioConstant.HALL_SCENARIO_SCENARIO_INVENTORY_ENTRANCE_MODULE)
+public class InventoryEntranceModuleContentFilterSdkExtPt extends Register implements ContentFilterSdkExtPt {
+
+    Logger logger = LoggerFactory.getLogger(InventoryEntranceModuleContentFilterSdkExtPt.class);
+
+    @Override
+    public SgFrameworkResponse<ContentVO> process(SgFrameworkContextContent sgFrameworkContextContent) {
+        logger.info("InventoryEntranceModuleContentFilterSdkExtPt.start.sgFrameworkContextContent:{}",
+            JSON.toJSONString(sgFrameworkContextContent));
+        return sgFrameworkContextContent.getContentVOSgFrameworkResponse();
+    }
+
+}
