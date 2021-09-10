482083c28b34ffa3a76f984fc0b821df092041e2
zhuomin.hzm <zhuomin.hzm@alibaba-inc.com>
2021-09-10 14:50:37 +0800

    icon加购后推荐过滤处理

---
+++ src/main/java/com/tmall/wireless/tac/biz/processor/iconRecommend/ext/IconRecommendSceneContentFilterExtPt.java
@@ -0,0 +1,125 @@
+package com.tmall.wireless.tac.biz.processor.iconRecommend.ext;
+
+import com.alibaba.cola.extension.Extension;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.tmall.txcs.gs.framework.extensions.filter.ContentFilterExtPt;
+import com.tmall.txcs.gs.framework.extensions.filter.ContentFilterRequest;
+import com.tmall.txcs.gs.framework.model.ContentVO;
+import com.tmall.txcs.gs.framework.model.ItemEntityVO;
+import com.tmall.txcs.gs.framework.model.SgFrameworkContextContent;
+import com.tmall.txcs.gs.framework.model.SgFrameworkResponse;
+import com.tmall.txcs.gs.framework.model.constant.ScenarioConstant;
+import com.tmall.txcs.gs.framework.support.LogUtil;
+import com.tmall.wireless.tac.biz.processor.common.ScenarioConstantApp;
+import com.tmall.wireless.tac.client.dataservice.TacLogger;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Yushan
+ * @date 2021/9/10 2:30 下午
+ */
+@Extension(bizId = ScenarioConstant.BIZ_TYPE_SUPERMARKET,
+        useCase = ScenarioConstant.LOC_TYPE_B2C,
+        scenario = ScenarioConstantApp.SCENARIO_ICON_RECOMMEND_SCENE
+)
+public class IconRecommendSceneContentFilterExtPt implements ContentFilterExtPt {
+
+    @Resource
+    TacLogger logger;
+
+    public static final int CONTENT_ITEM_SIZE = 6;
+    @Override
+    public SgFrameworkResponse<ContentVO> process(ContentFilterRequest contentFilterRequest) {
+        try {
+            SgFrameworkResponse<ContentVO> contentVOSgFrameworkResponse = contentFilterRequest.getContentVOSgFrameworkResponse();
+            if (CollectionUtils.isEmpty(contentVOSgFrameworkResponse.getItemAndContentList())) {
+                return contentVOSgFrameworkResponse;
+            }
+
+            Map<Long, List<ItemEntityVO>> contentId2CanBuyItemList = calcuContentId2CanBuyItemList(
+                    contentVOSgFrameworkResponse.getItemAndContentList(),
+                    contentFilterRequest.getSgFrameworkContextContent());
+
+            List<ContentVO> contentListAfterFilter = Lists.newArrayList();
+
+            contentVOSgFrameworkResponse.getItemAndContentList().forEach(contentVO -> {
+                Long contentId = contentVO.getLong("contentId");
+                if (contentId2CanBuyItemList.get(contentId) != null
+                        && contentId2CanBuyItemList.get(contentId).size() >= CONTENT_ITEM_SIZE) {
+                    contentListAfterFilter.add(contentVO);
+                    contentVO.put("items", contentId2CanBuyItemList.get(contentId));
+                } else {
+
+                    LogUtil.errorCode(contentFilterRequest.getSgFrameworkContextContent().getBizScenario().getUniqueIdentity(),
+                            "CONTENT_FILTER_BY_ITEM" + "," + contentId + "");
+                }
+            });
+
+            if (!CollectionUtils.isEmpty(contentListAfterFilter)) {
+                contentVOSgFrameworkResponse.setItemAndContentList(contentListAfterFilter);
+            } else {
+                LogUtil.errorCode(contentFilterRequest.getSgFrameworkContextContent().getBizScenario().getUniqueIdentity(),
+                        "CONTENT_FILTER_RESULT_EMPTY",
+                        "");
+            }
+            return contentVOSgFrameworkResponse;
+
+        } catch (Exception e) {
+            logger.error("IconRecommendSceneContentFilterExtPt_error:", e);
+        }
+        return contentFilterRequest.getContentVOSgFrameworkResponse();
+    }
+
+    private Map<Long, List<ItemEntityVO>> calcuContentId2CanBuyItemList(List<ContentVO> contentVOS, SgFrameworkContextContent sgFrameworkContextContent) {
+
+        Map<Long, List<ItemEntityVO>> result = Maps.newHashMap();
+
+        contentVOS.stream().forEach(contentVO -> {
+
+            List<ItemEntityVO> canBuyItemList = Lists.newArrayList();
+            Long contentId = contentVO.getLong("contentId");
+            result.put(contentId, canBuyItemList);
+            List<ItemEntityVO> items = (List<ItemEntityVO>) contentVO.get("items");
+            if (CollectionUtils.isEmpty(items)) {
+                return;
+            }
+            for (ItemEntityVO item : items) {
+                if (canBuy(item, sgFrameworkContextContent, contentId)) {
+                    canBuyItemList.add(item);
+                } else {
+                    LogUtil.errorCode(sgFrameworkContextContent.getBizScenario().getUniqueIdentity(),
+                            "ITEM_CANBUY_FALSE" + "," + contentId + " " + item.getString("itemId"));
+                }
+            }
+
+            result.put(contentId, canBuyItemList);
+        });
+
+        return result;
+    }
+
+    private boolean canBuy(ItemEntityVO item, SgFrameworkContextContent sgFrameworkContextContent, Long contentId) {
+
+        if (itemInfoError(item, sgFrameworkContextContent, contentId)) {
+            LogUtil.errorCode(sgFrameworkContextContent.getBizScenario().getUniqueIdentity(),
+                    "ITEM_INFO_ERROR" + "," + contentId + " " + item.getString("itemId"));
+            return false;
+        }
+
+        Boolean canBuy = item.getBoolean("canBuy");
+        Boolean sellOut = item.getBoolean("sellOut");
+
+        return (canBuy == null || canBuy) && (sellOut == null || !sellOut);
+    }
+
+    private boolean itemInfoError(ItemEntityVO item, SgFrameworkContextContent sgFrameworkContextContent, Long contentId) {
+        return StringUtils.isEmpty(item.getString("shortTitle")) || StringUtils.isEmpty(item.getString("itemImg"));
+
+    }
+}
--- src/main/java/com/tmall/wireless/tac/biz/processor/iconRecommend/handler/IconRecommendHandler.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/iconRecommend/handler/IconRecommendHandler.java
@@ -1,28 +1,22 @@
 package com.tmall.wireless.tac.biz.processor.iconRecommend.handler;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
+
+import com.taobao.util.CollectionUtil;
 import com.tmall.hades.monitor.print.HadesLogUtil;
 import com.tmall.txcs.gs.base.RpmReactiveHandler;
 import com.tmall.txcs.gs.framework.model.ContentVO;
 import com.tmall.txcs.gs.framework.model.SgFrameworkResponse;
 import com.tmall.wireless.tac.biz.processor.common.ScenarioConstantApp;
-import com.tmall.wireless.tac.biz.processor.firstScreenMind.FirstScreenMindContentInfoQueryExtPt;
 import com.tmall.wireless.tac.biz.processor.iconRecommend.constant.ConstantValue;
 import com.tmall.wireless.tac.biz.processor.iconRecommend.service.IconRecommendService;
 import com.tmall.wireless.tac.client.common.TacResult;
 import com.tmall.wireless.tac.client.dataservice.TacLogger;
 import com.tmall.wireless.tac.client.domain.Context;
 import io.reactivex.Flowable;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 /**
  * @author Yushan
@@ -34,10 +28,10 @@ public class IconRecommendHandler extends RpmReactiveHandler<SgFrameworkResponse
     @Autowired
     TacLogger logger;
 
-    Logger LOGGER = LoggerFactory.getLogger(IconRecommendHandler.class);
-
     @Autowired
     IconRecommendService iconRecommendService;
+
+    public static final int SCENE_NUMBER = 4;
 
     @Override
     public Flowable<TacResult<SgFrameworkResponse<ContentVO>>> executeFlowable(Context context) throws Exception {
@@ -47,35 +41,28 @@ public class IconRecommendHandler extends RpmReactiveHandler<SgFrameworkResponse
         Flowable<TacResult<SgFrameworkResponse<ContentVO>>> scene = iconRecommendService.recommend(context, ConstantValue.SCENE_WORD);
 
         return Flowable.zip(classifier, scene, (c, s) -> {
+
             List<ContentVO> classifierContentVOList = c.getData().getItemAndContentList();
             List<ContentVO> sceneContentVOList = s.getData().getItemAndContentList();
-            if (sceneContentVOList == null || sceneContentVOList.isEmpty()) {
+
+            if (CollectionUtil.isEmpty(classifierContentVOList)) {
+                return TacResult.newResult(new SgFrameworkResponse<>());
+            }
+            if (CollectionUtil.isEmpty(sceneContentVOList)) {
                 logger.info("[IconRecommendHandler] 场景词推荐为空！");
                 HadesLogUtil.stream(ScenarioConstantApp.SCENARIO_ICON_RECOMMEND_SCENE)
                         .kv("[IconRecommendHandler] ", "场景词推荐为空！")
                         .info();
             }
 
-            // 商品数量过滤
-            classifierContentVOList = Optional.ofNullable(classifierContentVOList).orElse(new ArrayList<>()).stream()
-                    .filter(contentVO -> contentVO.getJSONArray("items").size() >= 6)
-                    .collect(Collectors.toList());
-            sceneContentVOList = Optional.ofNullable(sceneContentVOList).orElse(new ArrayList<>()).stream()
-                    .filter(contentVO -> contentVO.getJSONArray("items").size() >= 6)
-                    .collect(Collectors.toList());
-            logger.info("[IconRecommendHandler] Classifier content: " + classifierContentVOList.size() + " and scene content: " + sceneContentVOList.size());
-            HadesLogUtil.stream(ScenarioConstantApp.SCENARIO_ICON_RECOMMEND_SCENE)
-                    .code("[IconRecommendHandler] ", "Classifier content: " + classifierContentVOList.size() + " and scene content: " + sceneContentVOList.size())
-                    .info();
-
             // 少于4个，不下发
-            if (classifierContentVOList.size() + sceneContentVOList.size() < 4) {
+            if (classifierContentVOList.size() + sceneContentVOList.size() < SCENE_NUMBER) {
                 return TacResult.newResult(new SgFrameworkResponse<>());
             }
 
             // 4分类 1场景
-            if (sceneContentVOList.size() == 1 && classifierContentVOList.size() == 4) {
-                classifierContentVOList.set(3, sceneContentVOList.get(0));
+            if (sceneContentVOList.size() == 1 && classifierContentVOList.size() == SCENE_NUMBER) {
+                classifierContentVOList.set(classifierContentVOList.size() - 1, sceneContentVOList.get(0));
             }
             // 3分类 1场景
             else if (classifierContentVOList.size() == 3) {
