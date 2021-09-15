cf60b6eb9cdccca84f41eee331c7bf780a766368
guichen <yoselin.yy@alibaba-inc.com>
2021-09-15 21:19:57 +0800

    dev

---
+++ src/main/java/com/tmall/wireless/tac/biz/processor/detail/o2o/content/O2ODetailRecContentOriginDataResponseConvertSdkExtPt.java
@@ -0,0 +1,70 @@
+package com.tmall.wireless.tac.biz.processor.detail.o2o.content;
+
+import java.util.List;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.TypeReference;
+
+import com.google.common.collect.Lists;
+import com.tmall.tcls.gs.sdk.biz.extensions.item.origindata.ConvertUtil;
+import com.tmall.tcls.gs.sdk.ext.annotation.SdkExtension;
+import com.tmall.tcls.gs.sdk.ext.extension.Register;
+import com.tmall.tcls.gs.sdk.framework.extensions.content.origindata.ContentOriginDataResponseConvertSdkExtPt;
+import com.tmall.tcls.gs.sdk.framework.extensions.content.origindata.ContentResponseConvertRequest;
+import com.tmall.tcls.gs.sdk.framework.model.context.ContentEntity;
+import com.tmall.tcls.gs.sdk.framework.model.context.OriginDataDTO;
+import com.tmall.tcls.gs.sdk.framework.suport.LogUtil;
+import com.tmall.wireless.tac.biz.processor.detail.common.constant.DetailConstant;
+import org.apache.commons.collections4.CollectionUtils;
+
+/**
+ * @author: guichen
+ * @Data: 2021/9/15
+ * @Description:
+ */
+@SdkExtension(bizId = DetailConstant.BIZ_ID,
+    useCase = DetailConstant.USE_CASE_O2O,
+    scenario = DetailConstant.CONTENT_SCENERIO)
+public class O2ODetailRecContentOriginDataResponseConvertSdkExtPt extends Register implements
+    ContentOriginDataResponseConvertSdkExtPt {
+
+    @Override
+    public OriginDataDTO<ContentEntity> process(ContentResponseConvertRequest contentResponseConvertRequest) {
+        JSONObject resObj = JSON.parseObject(contentResponseConvertRequest.getResponse());
+        OriginDataDTO<ContentEntity> responseEntity = ConvertUtil.processResponse(resObj);
+        List<ContentEntity> list = Lists.newArrayList();
+        responseEntity.setResult(list);
+        JSONArray result = resObj.getJSONArray("result");
+
+        if (CollectionUtils.isEmpty(result)) {
+            responseEntity.setErrorCode("TPP_RETURN_CONTENT_LIST_IS_EMPTY");
+            return responseEntity;
+        }
+        for (int i = 0; i < result.size(); i++) {
+            JSONObject jsonObject = result.getJSONObject(i);
+
+            ContentEntity contentEntity = new ContentEntity();
+
+            contentEntity.setRn(jsonObject.getInteger("rn"));
+            contentEntity.setTrack_point(responseEntity.getScm() + "." + jsonObject.getString("trackPoint"));
+            contentEntity.setContentId(jsonObject.getLong("contentId"));
+            contentEntity.setContentSetId(jsonObject.getString("sceneSetId"));
+            Object itemSets = jsonObject.get("itemSets");
+            if(itemSets != null){
+                List<String> strings = JSON.parseObject(JSON.toJSONString(itemSets),
+                    new TypeReference<List<String>>() {});
+                contentEntity.setItemSets(strings);
+            }
+
+            if (jsonObject.getJSONArray("items") == null || org.apache.commons.collections.CollectionUtils.isEmpty(jsonObject.getJSONArray("items"))) {
+                LogUtil.errorCode("TPP_RECOMMEND_CONTENT_HAS_NO_ITEM", contentEntity.getContentId());
+                continue;
+            }
+            contentEntity.setItems(null);
+            list.add(contentEntity);
+        }
+        return responseEntity;
+    }
+}
