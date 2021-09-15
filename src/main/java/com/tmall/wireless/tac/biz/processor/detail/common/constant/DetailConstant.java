a8ba63db104c87c470a17832f241d50451d29622
guichen <yoselin.yy@alibaba-inc.com>
2021-09-15 10:40:26 +0800

    dev

--- src/main/java/com/tmall/wireless/tac/biz/processor/detail/common/constant/DetailConstant.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/detail/common/constant/DetailConstant.java
@@ -13,8 +13,8 @@ public class DetailConstant {
     public static final String BIZ_ID="supermarket";
     public static final String USE_CASE_B2C="B2C";
     public static final String USE_CASE_O2O="O2O";
-    public static final String CONTENT_SCENERIO="contentRec";
-    public static final String ITEM_SCENERIO="itemRec";
+    public static final String CONTENT_SCENERIO="detailContentRec";
+    public static final String ITEM_SCENERIO="detailItemRec";
 
     public static final String REQUEST="detailRecommendRequest";
 
