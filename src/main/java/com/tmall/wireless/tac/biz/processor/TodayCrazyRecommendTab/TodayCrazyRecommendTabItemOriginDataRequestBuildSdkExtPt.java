076cf63a843960f811d7e50b1ccea964127e565a
wb-ljc587473 <wb-ljc587473@alibaba-inc.com>
2021-09-16 11:26:27 +0800

    1,test

--- src/main/java/com/tmall/wireless/tac/biz/processor/TodayCrazyRecommendTab/TodayCrazyRecommendTabItemOriginDataRequestBuildSdkExtPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/TodayCrazyRecommendTab/TodayCrazyRecommendTabItemOriginDataRequestBuildSdkExtPt.java
@@ -1,11 +1,18 @@
 package com.tmall.wireless.tac.biz.processor.TodayCrazyRecommendTab;
 
+import com.alibaba.fastjson.JSON;
+import com.google.common.collect.Maps;
 import com.tmall.tcls.gs.sdk.ext.annotation.SdkExtension;
 import com.tmall.tcls.gs.sdk.ext.extension.Register;
 import com.tmall.tcls.gs.sdk.framework.extensions.item.origindata.ItemOriginDataRequestBuildSdkExtPt;
-import com.tmall.tcls.gs.sdk.framework.model.context.SgFrameworkContextItem;
+import com.tmall.tcls.gs.sdk.framework.model.context.*;
 import com.tmall.wireless.store.spi.recommend.model.RecommendRequest;
-import org.springframework.stereotype.Service;
+import com.tmall.wireless.tac.client.dataservice.TacLogger;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 
 /**
  * Created from template by 罗俊冲 on 2021-09-15 17:54:58.
@@ -17,8 +24,25 @@ import org.springframework.stereotype.Service;
         scenario = "TodayCrazyRecommendTab"
 )
 public class TodayCrazyRecommendTabItemOriginDataRequestBuildSdkExtPt extends Register implements ItemOriginDataRequestBuildSdkExtPt {
+    @Autowired
+    TacLogger tacLogger;
+
     @Override
     public RecommendRequest process(SgFrameworkContextItem sgFrameworkContextItem) {
-        return null;
+        return this.buildTppParam(sgFrameworkContextItem);
+    }
+
+    private RecommendRequest buildTppParam(SgFrameworkContextItem sgFrameworkContextItem) {
+        RecommendRequest recommendRequest = new RecommendRequest();
+        Map<String, String> params = Maps.newHashMap();
+        params.put("pageSize", Optional.of(sgFrameworkContextItem).map(SgFrameworkContext::getCommonUserParams).map(CommonUserParams::getUserPageInfo).map(PageInfoDO::getPageSize).map(Objects::toString).orElse("20"));
+        params.put("index", Optional.of(sgFrameworkContextItem).map(SgFrameworkContext::getCommonUserParams).map(CommonUserParams::getUserPageInfo).map(PageInfoDO::getIndex).map(Objects::toString).orElse("0"));
+        params.put("smAreaId", Optional.of(sgFrameworkContextItem).map(SgFrameworkContext::getCommonUserParams).map(CommonUserParams::getLocParams).map(LocParams::getSmAreaId).map(Objects::toString).orElse("330100"));
+        recommendRequest.setAppId(21895L);
+        recommendRequest.setUserId(Optional.of(sgFrameworkContextItem).map(SgFrameworkContext::getCommonUserParams).map(CommonUserParams::getUserDO).map(UserDO::getUserId).orElse(0L));
+        recommendRequest.setParams(params);
+        recommendRequest.setLogResult(true);
+        tacLogger.info("recommendRequest_:" + JSON.toJSONString(recommendRequest));
+        return recommendRequest;
     }
 }
