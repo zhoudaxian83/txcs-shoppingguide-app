1a45571a35d71890f07fcedb2a27454ed1c632f9
guichen <yoselin.yy@alibaba-inc.com>
2021-09-20 15:17:21 +0800

    dev

--- src/main/java/com/tmall/wireless/tac/biz/processor/detail/common/config/DetailSwitch.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/detail/common/config/DetailSwitch.java
@@ -22,7 +22,7 @@ public class DetailSwitch {
     public static Map<String, TppRequestConfig> appIdMap = new HashMap<String, TppRequestConfig>() {
         {
             put(RecTypeEnum.RECIPE.getType(), new TppRequestConfig(27924L,"138002","7"));
-            put(RecTypeEnum.SIMILAR_ITEM_CONTENT.getType(), new TppRequestConfig(27924L,"138002","7"));
+            put(RecTypeEnum.SIMILAR_ITEM_CONTENT.getType(), new TppRequestConfig(23198L,"6006","7"));
             put(RecTypeEnum.SIMILAR_ITEM_ITEM.getType(), new TppRequestConfig(25385L));
         }
     };
