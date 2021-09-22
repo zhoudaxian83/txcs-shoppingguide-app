803eb2ff976eb3818857e04b568e3a62e49837b8
yangqing.byq <yangqing.byq@alibaba-inc.com>
2021-09-22 20:05:42 +0800

    add TodayCrazyTabSdkContentHandler

--- src/main/java/com/tmall/wireless/tac/biz/processor/todayCrazyTab/TodayCrazyTabSdkContentHandler.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/todayCrazyTab/TodayCrazyTabSdkContentHandler.java
@@ -1,5 +1,6 @@
 package com.tmall.wireless.tac.biz.processor.todayCrazyTab;
 
+import com.alibaba.aladdin.lamp.domain.response.GeneralItem;
 import com.tmall.tcls.gs.sdk.ext.BizScenario;
 import com.tmall.tcls.gs.sdk.framework.model.ContentVO;
 import com.tmall.tcls.gs.sdk.framework.model.SgFrameworkResponse;
@@ -7,11 +8,16 @@ import com.tmall.tcls.gs.sdk.framework.service.ShoppingguideSdkContentService;
 import com.tmall.txcs.gs.base.RpmReactiveHandler;
 import com.tmall.wireless.tac.biz.processor.common.PackageNameKey;
 import com.tmall.wireless.tac.biz.processor.huichang.common.constant.HallScenarioConstant;
+import com.tmall.wireless.tac.biz.processor.huichang.service.HallCommonContentRequestProxy;
 import com.tmall.wireless.tac.client.common.TacResult;
 import com.tmall.wireless.tac.client.domain.Context;
+import com.tmall.wireless.tac.client.domain.RequestContext4Ald;
+import com.tmall.wireless.tac.client.handler.TacReactiveHandler4Ald;
 import io.reactivex.Flowable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created from template by 进舟 on 2021-09-22 11:17:10.
@@ -19,22 +25,22 @@ import org.springframework.stereotype.Service;
  */
 
 @Service
-public class TodayCrazyTabSdkContentHandler extends RpmReactiveHandler<SgFrameworkResponse<ContentVO>> {
+public class TodayCrazyTabSdkContentHandler extends TacReactiveHandler4Ald {
 
     @Autowired
-    ShoppingguideSdkContentService shoppingguideSdkContentService;
+    HallCommonContentRequestProxy hallCommonContentRequestProxy;
 
     @Override
-    public Flowable<TacResult<SgFrameworkResponse<ContentVO>>> executeFlowable(Context context) throws Exception {
+    public Flowable<TacResult<List<GeneralItem>>> executeFlowable(RequestContext4Ald context) throws Exception {
         BizScenario b = BizScenario.valueOf(
-                 "supermarket",
-                 "b2c",
-                 "todayCrazyTab"
+                "supermarket",
+                "b2c",
+                "todayCrazyTab"
         );
 
         b.addProducePackage(HallScenarioConstant.HALL_CONTENT_SDK_PACKAGE);
         b.addProducePackage(PackageNameKey.OLD_RECOMMEND);
 
-        return shoppingguideSdkContentService.recommend0(context, b).map(TacResult::newResult);
+        return hallCommonContentRequestProxy.recommend(context, b);
     }
 }
