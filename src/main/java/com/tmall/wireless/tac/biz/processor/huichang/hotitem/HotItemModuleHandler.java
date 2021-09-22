448b139ac74d29dacbba1286d306ddfd5fae462a
wl <guohui.wangguohui@alibaba-inc.com>
2021-09-22 15:56:00 +0800

    修改tppid

--- src/main/java/com/tmall/wireless/tac/biz/processor/huichang/hotitem/HotItemModuleHandler.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/huichang/hotitem/HotItemModuleHandler.java
@@ -1,19 +1,14 @@
 package com.tmall.wireless.tac.biz.processor.huichang.hotitem;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import com.alibaba.aladdin.lamp.domain.response.GeneralItem;
 
 import com.google.common.collect.Lists;
 import com.tmall.tcls.gs.sdk.ext.BizScenario;
 import com.tmall.tcls.gs.sdk.framework.model.ItemEntityVO;
-import com.tmall.tcls.gs.sdk.framework.model.SgFrameworkResponse;
 import com.tmall.tcls.gs.sdk.framework.service.ShoppingguideSdkItemService;
-import com.tmall.wireless.tac.biz.processor.huichang.common.constant.HallCommonAldConstant;
 import com.tmall.wireless.tac.biz.processor.huichang.common.constant.HallScenarioConstant;
-import com.tmall.wireless.tac.biz.processor.huichang.service.HallCommonItemRequestProxy;
 import com.tmall.wireless.tac.client.common.TacResult;
 import com.tmall.wireless.tac.client.dataservice.TacLogger;
 import com.tmall.wireless.tac.client.domain.RequestContext4Ald;
@@ -24,13 +19,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-
 @Component
 public class HotItemModuleHandler extends TacReactiveHandler4Ald {
 
     Logger logger = LoggerFactory.getLogger(HotItemModuleHandler.class);
-
-    private static final String defaultLocType = "B2C";
 
     @Autowired
     TacLogger tacLogger;
@@ -39,47 +31,28 @@ public class HotItemModuleHandler extends TacReactiveHandler4Ald {
     ShoppingguideSdkItemService shoppingguideSdkItemService;
 
     @Override
-    public Flowable<TacResult<List<GeneralItem>>> executeFlowable(RequestContext4Ald requestContext4Ald) throws Exception {
+    public Flowable<TacResult<List<GeneralItem>>> executeFlowable(RequestContext4Ald requestContext4Ald)
+        throws Exception {
 
         BizScenario bizScenario = BizScenario.valueOf(HallScenarioConstant.HALL_SCENARIO_BIZ_ID,
             HallScenarioConstant.HALL_SCENARIO_USE_CASE_B2C,
             HallScenarioConstant.HALL_SCENARIO_HOT_ITEM);
         bizScenario.addProducePackage(HallScenarioConstant.HALL_ITEM_SDK_PACKAGE);
 
-        Flowable<TacResult<List<GeneralItem>>> tacResultFlowable = shoppingguideSdkItemService.recommend(
-            requestContext4Ald, bizScenario)
+        return shoppingguideSdkItemService.recommend(requestContext4Ald, bizScenario)
             .map(response -> {
-                List<GeneralItem> re = Lists.newArrayList();
-                re.add(convertAldItem(response));
-                return re;
+                List<GeneralItem> generalItemList = Lists.newArrayList();
+                List<ItemEntityVO> itemAndContentList = response.getItemAndContentList();
+                itemAndContentList.forEach(contentVO -> {
+                    GeneralItem generalItem = new GeneralItem();
+                    contentVO.keySet().forEach(key -> {
+                        generalItem.put(key, contentVO.get(key));
+                    });
+                    generalItemList.add(generalItem);
+                });
+                return generalItemList;
             })
             .map(TacResult::newResult)
             .onErrorReturn(r -> TacResult.errorResult(""));
-        return tacResultFlowable;
-
-
-        //Map<String, Object> aldContext = requestContext4Ald.getAldContext();
-        //Map<String, Object> aldParam = requestContext4Ald.getAldParam();
-        //Object aldStaticData  = aldContext.get(HallCommonAldConstant.STATIC_SCHEDULE_DATA);
-        //if(aldStaticData == null){
-        //    throw new Exception("静态数据获取为空");
-        //}
-        //List<Map<String, Object>> aldStaticDataList = (List<Map<String, Object>>)aldStaticData;
-        //String locType = defaultLocType;
-        //String tacParams = MapUtil.getStringWithDefault(aldParam, "tacParams", "");
-        //if(StringUtils.isNotBlank(tacParams)){
-        //    JSONObject tacParamsMap = JSON.parseObject(tacParams);
-        //    locType = Optional.ofNullable(tacParamsMap.getString(HallCommonAldConstant.LOC_TYPE)).orElse(defaultLocType);
-        //}
-        //
-        //
-        //return null;
     }
-
-    public GeneralItem convertAldItem(SgFrameworkResponse<ItemEntityVO> response) {
-        GeneralItem generalItem = new GeneralItem();
-        generalItem.put("data", response.getItemAndContentList());
-        return generalItem;
-    }
-
 }
