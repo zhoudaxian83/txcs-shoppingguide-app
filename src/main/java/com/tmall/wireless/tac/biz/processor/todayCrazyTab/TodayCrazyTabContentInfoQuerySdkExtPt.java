bb02b39c7f65f8ed36ca04089a17cba4d5dfc211
yangqing.byq <yangqing.byq@alibaba-inc.com>
2021-09-22 18:18:22 +0800

    add

--- src/main/java/com/tmall/wireless/tac/biz/processor/todayCrazyTab/TodayCrazyTabContentInfoQuerySdkExtPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/todayCrazyTab/TodayCrazyTabContentInfoQuerySdkExtPt.java
@@ -1,13 +1,20 @@
 package com.tmall.wireless.tac.biz.processor.todayCrazyTab;
 
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.tmall.tcls.gs.sdk.ext.annotation.SdkExtension;
 import com.tmall.tcls.gs.sdk.ext.extension.Register;
 import com.tmall.tcls.gs.sdk.framework.extensions.content.contentinfo.ContentInfoQuerySdkExtPt;
 import com.tmall.tcls.gs.sdk.framework.model.Response;
 import com.tmall.tcls.gs.sdk.framework.model.context.ContentInfoDTO;
 import com.tmall.tcls.gs.sdk.framework.model.context.SgFrameworkContextContent;
+import com.tmall.wireless.tac.biz.processor.huichang.common.constant.HallCommonAldConstant;
+import com.tmall.wireless.tac.client.domain.Context;
+import com.tmall.wireless.tac.client.domain.RequestContext4Ald;
 import io.reactivex.Flowable;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -23,6 +30,27 @@ public class TodayCrazyTabContentInfoQuerySdkExtPt extends Register implements C
 
     @Override
     public Flowable<Response<Map<Long, ContentInfoDTO>>> process(SgFrameworkContextContent sgFrameworkContextContent) {
-        return null;
+        return Flowable.just(Response.success(getContentInfo(sgFrameworkContextContent)));
+    }
+
+    private Map<Long, ContentInfoDTO> getContentInfo(SgFrameworkContextContent sgFrameworkContextContent) {
+
+        Map<Long, ContentInfoDTO> result = Maps.newHashMap();
+
+        Context tacContext = sgFrameworkContextContent.getTacContext();
+        RequestContext4Ald requestContext4Ald = (RequestContext4Ald) tacContext;
+
+        Map<String, Object> aldContext = requestContext4Ald.getAldContext();
+        Object staticScheduleData = aldContext.get(HallCommonAldConstant.STATIC_SCHEDULE_DATA);
+        List<Map<String, Object>> staticScheduleDataList = (List<Map<String, Object>>)staticScheduleData;
+
+        for (Map<String, Object> stringObjectMap : staticScheduleDataList) {
+            Long itemSetId = (Long) stringObjectMap.get("itemSetId");
+            ContentInfoDTO contentInfoDTO = new ContentInfoDTO();
+            contentInfoDTO.setContentInfo(stringObjectMap);
+            result.put(itemSetId, contentInfoDTO);
+        }
+
+        return result;
     }
 }
