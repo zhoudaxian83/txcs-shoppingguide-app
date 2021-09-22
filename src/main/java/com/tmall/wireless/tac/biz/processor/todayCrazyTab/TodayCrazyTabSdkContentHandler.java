338424222bd686c24bd7de78222de8ddb12c32d0
猫超团队鸿雁开发账号 <chaoshi-anser@alibaba-inc.com>
2021-09-22 11:17:10 +0800

    feat(todayCrazyTab): 初始化模板文件TodayCrazyTabSdkContentHandler

---
+++ src/main/java/com/tmall/wireless/tac/biz/processor/todayCrazyTab/TodayCrazyTabSdkContentHandler.java
@@ -0,0 +1,36 @@
+package com.tmall.wireless.tac.biz.processor.todayCrazyTab;
+
+import com.tmall.tcls.gs.sdk.ext.BizScenario;
+import com.tmall.tcls.gs.sdk.framework.model.ContentVO;
+import com.tmall.tcls.gs.sdk.framework.model.SgFrameworkResponse;
+import com.tmall.tcls.gs.sdk.framework.service.ShoppingguideSdkContentService;
+import com.tmall.txcs.gs.base.RpmReactiveHandler;
+import com.tmall.wireless.tac.client.common.TacResult;
+import com.tmall.wireless.tac.client.domain.Context;
+import io.reactivex.Flowable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created from template by 进舟 on 2021-09-22 11:17:10.
+ *
+ */
+
+@Service
+public class TodayCrazyTabSdkContentHandler extends RpmReactiveHandler<SgFrameworkResponse<ContentVO>> {
+
+    @Autowired
+    ShoppingguideSdkContentService shoppingguideSdkContentService;
+
+    @Override
+    public Flowable<TacResult<SgFrameworkResponse<ContentVO>>> executeFlowable(Context context) throws Exception {
+        BizScenario b = BizScenario.valueOf(
+                 "supermarket",
+                 "b2c",
+                 "todayCrazyTab"
+        );
+         b.addProducePackage("ald上下文解析");
+
+        return shoppingguideSdkContentService.recommend0(context, b).map(TacResult::newResult);
+    }
+}
