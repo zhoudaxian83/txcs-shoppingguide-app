9239a6c7c71157d90714e99b2d3d8e7ce1a89aca
猫超团队鸿雁开发账号 <chaoshi-anser@alibaba-inc.com>
2021-09-25 13:40:47 +0800

    feat(TodayCrazyRecommendTab): Nextdy自动提交，更新/删除文件

---
+++ src/main/java/com/tmall/wireless/tac/biz/processor/TodayCrazyRecommendTab/TodayCrazyRecommendTabItemFilterSdkExtPt.java
@@ -0,0 +1,25 @@
+package com.tmall.wireless.tac.biz.processor.TodayCrazyRecommendTab;
+
+import com.tmall.tcls.gs.sdk.ext.annotation.SdkExtension;
+import com.tmall.tcls.gs.sdk.ext.extension.Register;
+import com.tmall.tcls.gs.sdk.framework.extensions.item.filter.ItemFilterRequest;
+import com.tmall.tcls.gs.sdk.framework.extensions.item.filter.ItemFilterSdkExtPt;
+import com.tmall.tcls.gs.sdk.framework.model.ItemEntityVO;
+import com.tmall.tcls.gs.sdk.framework.model.SgFrameworkResponse;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created from template by 罗俊冲 on 2021-09-25 13:40:47.
+ */
+
+@SdkExtension(
+        bizId = "supermarket",
+        useCase = "b2c",
+        scenario = "TodayCrazyRecommendTab"
+)
+public class TodayCrazyRecommendTabItemFilterSdkExtPt extends Register implements ItemFilterSdkExtPt {
+    @Override
+    public SgFrameworkResponse<ItemEntityVO> process(ItemFilterRequest itemFilterRequest) {
+        return null;
+    }
+}
