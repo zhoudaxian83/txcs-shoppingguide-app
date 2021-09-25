9919194ccb99975b6c196912dccacc3fdd6d50d1
wb-ljc587473 <wb-ljc587473@alibaba-inc.com>
2021-09-25 10:51:23 +0800

    1,test

--- src/main/java/com/tmall/wireless/tac/biz/processor/TodayCrazyRecommendTab/TodayCrazyRecommendTabItemOriginDataPostProcessorSdkExtPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/TodayCrazyRecommendTab/TodayCrazyRecommendTabItemOriginDataPostProcessorSdkExtPt.java
@@ -1,5 +1,6 @@
 package com.tmall.wireless.tac.biz.processor.TodayCrazyRecommendTab;
 
+import com.alibaba.fastjson.JSON;
 import com.tmall.tcls.gs.sdk.ext.annotation.SdkExtension;
 import com.tmall.tcls.gs.sdk.ext.extension.Register;
 import com.tmall.tcls.gs.sdk.framework.extensions.item.origindata.ItemOriginDataPostProcessorSdkExtPt;
@@ -30,12 +31,14 @@ public class TodayCrazyRecommendTabItemOriginDataPostProcessorSdkExtPt extends R
 
     @Override
     public OriginDataDTO<ItemEntity> process(OriginDataProcessRequest originDataProcessRequest) {
+        tacLogger.info("开始查询limit信息");
         Map<Long, List<ItemLimitDTO>> itemLimitResult = todayCrazyLimitService.getItemLimitResult(originDataProcessRequest.getSgFrameworkContextItem());
         if (itemLimitResult != null) {
             originDataProcessRequest.getSgFrameworkContextItem().getUserParams().put(Constant.ITEM_LIMIT_RESULT, itemLimitResult);
         } else {
             tacLogger.warn("TodayCrazyRecommendTabItemOriginDataPostProcessorSdkExtPt_" + "获取限购数据为空");
         }
+        tacLogger.info("limit查询结果_" + JSON.toJSONString(itemLimitResult));
         return originDataProcessRequest.getItemEntityOriginDataDTO();
     }
 }
