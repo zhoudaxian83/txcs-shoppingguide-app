191105056e19925371306e6f60b2914056617875
recheard <wfx01179122@alibaba-inc.com>
2021-09-26 15:46:52 +0800

    feat:猫超支付成功页猜你喜欢

---
+++ src/main/java/com/tmall/wireless/tac/biz/processor/paySuccessGuessYouLike/AliPaySuccessGuessYouLikeContentVoBuildSdkExtPt.java
@@ -0,0 +1,32 @@
+package com.tmall.wireless.tac.biz.processor.paySuccessGuessYouLike;
+
+import com.alibaba.fastjson.JSON;
+import com.tmall.tcls.gs.sdk.biz.extensions.content.vo.DefaultContentVoBuildSdkExtPt;
+import com.tmall.tcls.gs.sdk.ext.annotation.SdkExtension;
+import com.tmall.tcls.gs.sdk.framework.extensions.content.vo.ContentVoBuildSdkExtPt;
+import com.tmall.tcls.gs.sdk.framework.model.ContentVO;
+import com.tmall.tcls.gs.sdk.framework.model.SgFrameworkResponse;
+import com.tmall.tcls.gs.sdk.framework.model.context.SgFrameworkContextContent;
+import com.tmall.wireless.tac.biz.processor.common.ScenarioConstantApp;
+import com.tmall.wireless.tac.dataservice.log.TacLoggerImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+@SdkExtension(bizId = ScenarioConstantApp.BIZ_TYPE_SUPERMARKET,
+        useCase = ScenarioConstantApp.LOC_TYPE_B2C
+        , scenario = ScenarioConstantApp.PAY_FOR_SUCCESS_GUESS_YOU_LIKE
+)
+public class AliPaySuccessGuessYouLikeContentVoBuildSdkExtPt extends DefaultContentVoBuildSdkExtPt
+        implements ContentVoBuildSdkExtPt {
+
+
+    @Autowired
+    TacLoggerImpl tacLogger;
+    @Override
+    public SgFrameworkResponse<ContentVO> process(SgFrameworkContextContent sgFrameworkContextContent) {
+
+
+        tacLogger.info("vo解析前信息：" + JSON.toJSONString(sgFrameworkContextContent));
+        SgFrameworkResponse<ContentVO> process = super.process(sgFrameworkContextContent);
+        tacLogger.info("vo解析后信息：" + JSON.toJSONString(sgFrameworkContextContent));
+        return process;
+    }
+}
