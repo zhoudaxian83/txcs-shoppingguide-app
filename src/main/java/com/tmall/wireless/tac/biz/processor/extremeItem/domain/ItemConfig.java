6a2299aeb25255d79dcd7f38fb7ea2cfb10fc524
yanwu <jiabin.pjb@alibaba-inc.com>
2021-09-27 16:28:11 +0800

    update

--- src/main/java/com/tmall/wireless/tac/biz/processor/extremeItem/domain/ItemConfig.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/extremeItem/domain/ItemConfig.java
@@ -58,16 +58,16 @@ public class ItemConfig {
 
     public static ItemConfig valueOf(Map<String, Object> stringObjectMap) {
         ItemConfig itemConfig = new ItemConfig();
-        itemConfig.setItemId(Long.valueOf((String)stringObjectMap.get("contentId")));
-        itemConfig.setItemName((String)stringObjectMap.get("shortTitle"));
-        itemConfig.setGroupNo(Integer.valueOf((String)stringObjectMap.get("groupNo")));
-        itemConfig.setForceSort(Integer.parseInt((String)stringObjectMap.get("forceSort")) == 1);
-        itemConfig.setSequenceNo(Integer.parseInt((String) stringObjectMap.get("sequenceNo")));
-        itemConfig.setExposurePercent(Integer.parseInt((String)stringObjectMap.get("exposurePercent")));
-        itemConfig.setActivityId((String)stringObjectMap.get("activityId"));
-        itemConfig.setCouponValue((String)stringObjectMap.get("couponValue"));
-        itemConfig.setItemDescCustom((String)stringObjectMap.get("itemDescCustom"));
-        itemConfig.setItemImg((String)stringObjectMap.get("itemImg"));
+        itemConfig.setItemId(Long.valueOf(String.valueOf(stringObjectMap.get("contentId"))));
+        itemConfig.setItemName(String.valueOf(stringObjectMap.get("shortTitle")));
+        itemConfig.setGroupNo(Integer.valueOf(String.valueOf(stringObjectMap.get("groupNo"))));
+        itemConfig.setForceSort(Integer.parseInt(String.valueOf(stringObjectMap.get("forceSort"))) == 1);
+        itemConfig.setSequenceNo(Integer.parseInt(String.valueOf(stringObjectMap.get("sequenceNo"))));
+        itemConfig.setExposurePercent(Integer.parseInt(String.valueOf(stringObjectMap.get("exposurePercent"))));
+        itemConfig.setActivityId(String.valueOf(stringObjectMap.get("activityId")));
+        itemConfig.setCouponValue(String.valueOf(stringObjectMap.get("couponValue")));
+        itemConfig.setItemDescCustom(String.valueOf(stringObjectMap.get("itemDescCustom")));
+        itemConfig.setItemImg(String.valueOf(stringObjectMap.get("itemImg")));
         return itemConfig;
     }
 }
