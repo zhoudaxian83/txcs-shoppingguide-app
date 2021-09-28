9e79309c6783c88f962ca1fce42991dca10eadb5
chengfeifei <wb-cff926080@alibaba-inc.com>
2021-09-28 16:01:24 +0800

    微调

--- src/main/java/com/tmall/wireless/tac/biz/processor/guessYourLikeShopCart4/GuessYourLikeShopCart4ItemOriginDataFailProcessorSdkExtPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/guessYourLikeShopCart4/GuessYourLikeShopCart4ItemOriginDataFailProcessorSdkExtPt.java
@@ -1,10 +1,15 @@
 package com.tmall.wireless.tac.biz.processor.guessYourLikeShopCart4;
 
+import com.alibaba.fastjson.JSON;
+import com.tmall.tcls.gs.sdk.biz.extensions.item.origindata.DefaultItemOriginDataFailProcessorSdkExtPt;
 import com.tmall.tcls.gs.sdk.ext.annotation.SdkExtension;
 import com.tmall.tcls.gs.sdk.framework.extensions.item.origindata.ItemOriginDataFailProcessorSdkExtPt;
 import com.tmall.tcls.gs.sdk.framework.extensions.item.origindata.OriginDataProcessRequest;
 import com.tmall.tcls.gs.sdk.framework.model.context.ItemEntity;
 import com.tmall.tcls.gs.sdk.framework.model.context.OriginDataDTO;
+import com.tmall.tcls.gs.sdk.ext.extension.Register;
+import com.tmall.wireless.tac.dataservice.log.TacLoggerImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,9 +21,15 @@ import org.springframework.stereotype.Service;
         useCase = "b2c",
         scenario = "guessYourLikeShopCart4"
 )
-public class GuessYourLikeShopCart4ItemOriginDataFailProcessorSdkExtPt extends Register implements ItemOriginDataFailProcessorSdkExtPt {
+public class GuessYourLikeShopCart4ItemOriginDataFailProcessorSdkExtPt extends DefaultItemOriginDataFailProcessorSdkExtPt implements ItemOriginDataFailProcessorSdkExtPt {
+
+    @Autowired
+    TacLoggerImpl tacLogger;
     @Override
     public OriginDataDTO<ItemEntity> process(OriginDataProcessRequest originDataProcessRequest) {
-        return null;
+        tacLogger.info("originDataProcessRequest = "+ JSON.toJSONString(originDataProcessRequest));
+        OriginDataDTO<ItemEntity> process = super.process(originDataProcessRequest);
+        tacLogger.info("process = "+ JSON.toJSONString(process));
+        return process;
     }
 }
