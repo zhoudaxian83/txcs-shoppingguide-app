75c0cde062e1b15aabafa4bcafb1872c3d10e569
chengfeifei <wb-cff926080@alibaba-inc.com>
2021-09-29 17:29:30 +0800

    微调测试

--- src/main/java/com/tmall/wireless/tac/biz/processor/guessYourLikeShopCart4/GuessYourLikeShopCart4ItemFilterSdkExtPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/guessYourLikeShopCart4/GuessYourLikeShopCart4ItemFilterSdkExtPt.java
@@ -1,11 +1,15 @@
 package com.tmall.wireless.tac.biz.processor.guessYourLikeShopCart4;
 
+import com.alibaba.fastjson.JSON;
+import com.tmall.tcls.gs.sdk.biz.extensions.item.filter.DefaultItemFilterSdkExtPt;
 import com.tmall.tcls.gs.sdk.ext.annotation.SdkExtension;
 import com.tmall.tcls.gs.sdk.ext.extension.Register;
 import com.tmall.tcls.gs.sdk.framework.extensions.item.filter.ItemFilterRequest;
 import com.tmall.tcls.gs.sdk.framework.extensions.item.filter.ItemFilterSdkExtPt;
 import com.tmall.tcls.gs.sdk.framework.model.ItemEntityVO;
 import com.tmall.tcls.gs.sdk.framework.model.SgFrameworkResponse;
+import com.tmall.wireless.tac.dataservice.log.TacLoggerImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,9 +21,15 @@ import org.springframework.stereotype.Service;
         useCase = "b2c",
         scenario = "guessYourLikeShopCart4"
 )
-public class GuessYourLikeShopCart4ItemFilterSdkExtPt extends Register implements ItemFilterSdkExtPt {
+public class GuessYourLikeShopCart4ItemFilterSdkExtPt extends DefaultItemFilterSdkExtPt implements ItemFilterSdkExtPt {
+
+    @Autowired
+    TacLoggerImpl tacLogger;
     @Override
     public SgFrameworkResponse<ItemEntityVO> process(ItemFilterRequest itemFilterRequest) {
-        return null;
+
+        SgFrameworkResponse<ItemEntityVO> process = super.process(itemFilterRequest);
+        tacLogger.info("商品过滤处理：ItemEntityVOResponse="+ JSON.toJSONString(process));
+        return process;
     }
 }
--- src/main/java/com/tmall/wireless/tac/biz/processor/guessYourLikeShopCart4/GuessYourLikeShopCart4ItemProcessBeforeReturnSdkExtPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/guessYourLikeShopCart4/GuessYourLikeShopCart4ItemProcessBeforeReturnSdkExtPt.java
@@ -1,9 +1,13 @@
 package com.tmall.wireless.tac.biz.processor.guessYourLikeShopCart4;
 
+import com.alibaba.fastjson.JSON;
+import com.tmall.tcls.gs.sdk.biz.extensions.item.filter.DefaultItemProcessBeforeReturnSdkExtPt;
 import com.tmall.tcls.gs.sdk.ext.annotation.SdkExtension;
 import com.tmall.tcls.gs.sdk.ext.extension.Register;
 import com.tmall.tcls.gs.sdk.framework.extensions.item.filter.ItemProcessBeforeReturnSdkExtPt;
 import com.tmall.tcls.gs.sdk.framework.model.context.SgFrameworkContextItem;
+import com.tmall.wireless.tac.dataservice.log.TacLoggerImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,9 +19,15 @@ import org.springframework.stereotype.Service;
         useCase = "b2c",
         scenario = "guessYourLikeShopCart4"
 )
-public class GuessYourLikeShopCart4ItemProcessBeforeReturnSdkExtPt extends Register implements ItemProcessBeforeReturnSdkExtPt {
+public class GuessYourLikeShopCart4ItemProcessBeforeReturnSdkExtPt extends DefaultItemProcessBeforeReturnSdkExtPt implements ItemProcessBeforeReturnSdkExtPt {
+
+    @Autowired
+    TacLoggerImpl tacLogger;
     @Override
     public SgFrameworkContextItem process(SgFrameworkContextItem sgFrameworkContextItem) {
-        return null;
+
+        SgFrameworkContextItem process = super.process(sgFrameworkContextItem);
+        tacLogger.info("返回前处理：sgFrameworkContextItem="+ JSON.toJSONString(sgFrameworkContextItem));
+        return process;
     }
 }
