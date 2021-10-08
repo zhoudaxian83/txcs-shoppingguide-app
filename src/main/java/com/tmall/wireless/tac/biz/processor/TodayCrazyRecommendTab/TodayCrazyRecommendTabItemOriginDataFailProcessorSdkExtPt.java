b3804f598c2283b78b06986c6465cca84c41acb7
wb-ljc587473 <wb-ljc587473@alibaba-inc.com>
2021-10-08 09:53:13 +0800

    tpp打底

--- src/main/java/com/tmall/wireless/tac/biz/processor/TodayCrazyRecommendTab/TodayCrazyRecommendTabItemOriginDataFailProcessorSdkExtPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/TodayCrazyRecommendTab/TodayCrazyRecommendTabItemOriginDataFailProcessorSdkExtPt.java
@@ -1,15 +1,34 @@
 package com.tmall.wireless.tac.biz.processor.TodayCrazyRecommendTab;
 
 import com.alibaba.fastjson.JSON;
+import com.google.common.collect.Lists;
+import com.taobao.tair.DataEntry;
+import com.taobao.tair.Result;
+import com.taobao.tair.ResultCode;
+import com.tmall.hades.monitor.print.HadesLogUtil;
 import com.tmall.tcls.gs.sdk.ext.annotation.SdkExtension;
 import com.tmall.tcls.gs.sdk.ext.extension.Register;
 import com.tmall.tcls.gs.sdk.framework.extensions.item.origindata.ItemOriginDataFailProcessorSdkExtPt;
 import com.tmall.tcls.gs.sdk.framework.extensions.item.origindata.OriginDataProcessRequest;
 import com.tmall.tcls.gs.sdk.framework.model.context.ItemEntity;
 import com.tmall.tcls.gs.sdk.framework.model.context.OriginDataDTO;
+import com.tmall.txcs.gs.framework.extensions.excutor.SgExtensionExecutor;
+import com.tmall.txcs.gs.framework.extensions.failprocessor.ItemFailProcessorRequest;
+import com.tmall.txcs.gs.framework.extensions.failprocessor.ItemOriginDataFailKeyBuilderExtPt;
+import com.tmall.txcs.gs.framework.model.ErrorCode;
+import com.tmall.txcs.gs.framework.model.SgFrameworkContextItem;
+import com.tmall.txcs.gs.framework.model.meta.ItemMetaInfo;
+import com.tmall.txcs.gs.spi.recommend.TairFactorySpi;
+import com.tmall.txcs.gs.spi.recommend.TairManager;
 import com.tmall.wireless.tac.biz.processor.common.ScenarioConstantApp;
 import com.tmall.wireless.tac.dataservice.log.TacLoggerImpl;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created from template by 罗俊冲 on 2021-09-30 16:51:23.
@@ -23,10 +42,131 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class TodayCrazyRecommendTabItemOriginDataFailProcessorSdkExtPt extends Register implements ItemOriginDataFailProcessorSdkExtPt {
     @Autowired
     TacLoggerImpl tacLogger;
+    public static final int SAMPLING_INTERVAL = 10;
+
+    private static final AtomicLong counter = new AtomicLong(0L);
+
+    @Autowired
+    TairFactorySpi tairFactorySpi;
+
+    @Autowired
+    private SgExtensionExecutor sgExtensionExecutor;
+
+    String logKey = "originDataFailProcessor";
+    String originDataSuccessKey = "originDataSuccess";
 
     @Override
     public OriginDataDTO<ItemEntity> process(OriginDataProcessRequest originDataProcessRequest) {
-        tacLogger.info("TPP返回失败了" + JSON.toJSONString(originDataProcessRequest));
-        return originDataProcessRequest.getItemEntityOriginDataDTO();
+        tacLogger.info("tpp失败打底逻辑");
+        ItemFailProcessorRequest itemFailProcessorRequest = new ItemFailProcessorRequest();
+        itemFailProcessorRequest.setItemEntityOriginDataDTO(itemFailProcessorRequest.getItemEntityOriginDataDTO());
+        itemFailProcessorRequest.setSgFrameworkContextItem(itemFailProcessorRequest.getSgFrameworkContextItem());
+        SgFrameworkContextItem sgFrameworkContextItem = itemFailProcessorRequest.getSgFrameworkContextItem();
+        int interval = Optional.of(itemFailProcessorRequest)
+                .map(ItemFailProcessorRequest::getSgFrameworkContextItem)
+                .map(SgFrameworkContextItem::getItemMetaInfo)
+                .map(ItemMetaInfo::getSamplingInterval)
+                .orElse(SAMPLING_INTERVAL);
+        if (interval <= 0) {
+            interval = SAMPLING_INTERVAL;
+        }
+        String tairKey = buildTairKey(itemFailProcessorRequest);
+        long currentCount = counter.addAndGet(1);
+        boolean success = itemFailProcessorRequest.getItemEntityOriginDataDTO() != null
+                && CollectionUtils.isNotEmpty(itemFailProcessorRequest.getItemEntityOriginDataDTO().getResult());
+        TairManager merchantsTair = tairFactorySpi.getOriginDataFailProcessTair();
+        if (merchantsTair == null || merchantsTair.getMultiClusterTairManager() == null || merchantsTair.getNameSpace() <= 0) {
+
+            HadesLogUtil.stream(sgFrameworkContextItem.getBizScenario().getUniqueIdentity())
+                    .kv("step", logKey)
+                    .kv("errorCode", ErrorCode.ITEM_FAIL_PROCESSOR_TAIR_MANAGER_NULL)
+                    .error();
+            return originDataProcessRequest.getItemEntityOriginDataDTO();
+        }
+
+        if (success) {
+            HadesLogUtil.stream(sgFrameworkContextItem.getBizScenario().getUniqueIdentity())
+                    .kv("step", logKey)
+                    .kv("info", originDataSuccessKey)
+                    .info();
+
+            if (currentCount % interval == 1) {
+                ResultCode put = merchantsTair.getMultiClusterTairManager().put(merchantsTair.getNameSpace(),
+                        tairKey,
+                        JSON.toJSONString(itemFailProcessorRequest.getItemEntityOriginDataDTO().getResult()),
+                        0);
+
+                if (put == null || put.getCode() != ResultCode.SUCCESS.getCode()) {
+                    HadesLogUtil.stream(sgFrameworkContextItem.getBizScenario().getUniqueIdentity())
+                            .kv("step", logKey)
+                            .kv("errorCode", ErrorCode.ITEM_FAIL_PROCESSOR_TAIR_PUT_ERROR)
+                            .error();
+                }
+
+            }
+            return originDataProcessRequest.getItemEntityOriginDataDTO();
+
+        } else {
+
+            HadesLogUtil.stream(sgFrameworkContextItem.getBizScenario().getUniqueIdentity())
+                    .kv("step", logKey)
+                    .kv("errorCode", ErrorCode.ITEM_FAIL_PROCESSOR_ORIGIN_DATA_FAIL)
+                    .error();
+
+            List<ItemEntity> itemEntityList = readFromTair(tairKey, merchantsTair);
+            if (CollectionUtils.isEmpty(itemEntityList)) {
+                HadesLogUtil.stream(sgFrameworkContextItem.getBizScenario().getUniqueIdentity())
+                        .kv("step", logKey)
+                        .kv("errorCode", ErrorCode.ITEM_FAIL_PROCESSOR_READ_FROM_TARI_FAIL)
+                        .error();
+
+                return originDataProcessRequest.getItemEntityOriginDataDTO();
+            }
+
+            HadesLogUtil.stream(sgFrameworkContextItem.getBizScenario().getUniqueIdentity())
+                    .kv("step", logKey)
+                    .kv("errorCode", ErrorCode.ITEM_FAIL_PROCESSOR_READ_FROM_TARI_SUCCESS)
+                    .error();
+
+            OriginDataDTO<ItemEntity> originDataDTO = new OriginDataDTO<>();
+            originDataDTO.setResult(itemEntityList);
+            originDataDTO.setIndex(0);
+            originDataDTO.setHasMore(false);
+            originDataDTO.setPvid("");
+            originDataDTO.setScm("1007.0.0.0");
+            tacLogger.info("tpp失败打底数据：" + JSON.toJSONString(originDataDTO));
+            return originDataDTO;
+        }
+
     }
+
+
+    private String buildTairKey(ItemFailProcessorRequest itemFailProcessorRequest) {
+        return sgExtensionExecutor.execute(ItemOriginDataFailKeyBuilderExtPt.class,
+                itemFailProcessorRequest.getSgFrameworkContextItem().getBizScenario(),
+                pt -> pt.process0(itemFailProcessorRequest));
+
+    }
+
+    private List<ItemEntity> readFromTair(String tairKey, TairManager tairManager) {
+
+        List<ItemEntity> list = Lists.newArrayList();
+
+        Result<DataEntry> dataEntryResult = tairManager.getMultiClusterTairManager().get(tairManager.getNameSpace(), tairKey);
+
+        String value = Optional.ofNullable(dataEntryResult).map(Result::getValue).map(DataEntry::getValue).map(Object::toString).orElse("");
+
+        if (StringUtils.isEmpty(value)) {
+            return list;
+        }
+
+        return JSON.parseArray(value, ItemEntity.class);
+    }
+
+
+//    protected boolean checkSuccess(OriginDataDTO<ItemEntity> originDataDTO) {
+//        return originDataDTO != null && CollectionUtils.isNotEmpty(originDataDTO.getResult());
+//    }
+
+
 }
