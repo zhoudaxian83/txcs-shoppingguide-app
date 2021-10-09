933310e0a4a7372acbaaf261178e56815fd8267f
wb-ljc587473 <wb-ljc587473@alibaba-inc.com>
2021-10-09 15:41:35 +0800

    tac 打底规范化

--- src/main/java/com/tmall/wireless/tac/biz/processor/common/util/TacResultBackupUtil.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/common/util/TacResultBackupUtil.java
@@ -2,7 +2,6 @@ package com.tmall.wireless.tac.biz.processor.common.util;
 
 import com.tmall.hades.monitor.print.HadesLogUtil;
 import com.tmall.tcls.gs.sdk.ext.BizScenario;
-import com.tmall.tcls.gs.sdk.framework.model.ItemEntityVO;
 import com.tmall.tcls.gs.sdk.framework.model.SgFrameworkResponse;
 import com.tmall.wireless.tac.client.common.TacResult;
 import org.apache.commons.collections.CollectionUtils;
@@ -19,7 +18,7 @@ public class TacResultBackupUtil {
      * @param bizScenario  业务身份必须传，否则无法根据业务身份监控
      * @return
      */
-    public static TacResult tacResultBackup(TacResult<SgFrameworkResponse<ItemEntityVO>> tacResult, BizScenario bizScenario){
+    public static TacResult tacResultBackup(TacResult<SgFrameworkResponse> tacResult, BizScenario bizScenario){
         if(bizScenario == null || StringUtils.isEmpty(bizScenario.getUniqueIdentity())){
             tacResult.getBackupMetaData().setUseBackup(true);
             return tacResult;
@@ -28,14 +27,14 @@ public class TacResultBackupUtil {
             tacResult = TacResult.errorResult("TacResultBackup");
 
             HadesLogUtil.stream(bizScenario.getUniqueIdentity())
-                .kv("key","tacBackup")
-                .kv("tacResultBackup","true")
-                .info();
+                    .kv("key","tacBackup")
+                    .kv("tacResultBackup","true")
+                    .info();
         }else{
             HadesLogUtil.stream(bizScenario.getUniqueIdentity())
-                .kv("key","tacBackup")
-                .kv("tacResultBackup","false")
-                .info();
+                    .kv("key","tacBackup")
+                    .kv("tacResultBackup","false")
+                    .info();
         }
         tacResult.getBackupMetaData().setUseBackup(true);
         return tacResult;
