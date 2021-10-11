c2a56f86f8fb273a02ae823c38c6a062f533dc07
wb-ljc587473 <wb-ljc587473@alibaba-inc.com>
2021-10-11 22:03:55 +0800

    1,vo字段扩充

--- src/main/java/com/tmall/wireless/tac/biz/processor/TodayCrazyRecommendTab/TodayCrazyRecommendTabBuildItemVoSdkExtPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/TodayCrazyRecommendTab/TodayCrazyRecommendTabBuildItemVoSdkExtPt.java
@@ -1,7 +1,10 @@
 package com.tmall.wireless.tac.biz.processor.TodayCrazyRecommendTab;
 
+import com.alibaba.fastjson.JSONObject;
 import com.google.common.base.Joiner;
 import com.google.common.collect.Maps;
+import com.tcls.mkt.atmosphere.model.response.ItemPromotionResp;
+import com.tcls.mkt.atmosphere.model.response.UnifyPriceDTO;
 import com.tmall.aselfcaptain.item.model.ItemDTO;
 import com.tmall.tcls.gs.sdk.ext.annotation.SdkExtension;
 import com.tmall.tcls.gs.sdk.ext.extension.Register;
@@ -16,16 +19,20 @@ import com.tmall.tcls.gs.sdk.framework.model.context.ItemInfoDTO;
 import com.tmall.tcls.gs.sdk.framework.model.context.O2oType;
 import com.tmall.tcls.gs.sdk.sm.iteminfo.bysource.captain.ItemInfoBySourceCaptainDTO;
 import com.tmall.tcls.gs.sdk.sm.iteminfo.bysource.tpp.ItemInfoBySourceTppDTO;
+import com.tmall.txcs.biz.supermarket.scene.util.MapUtil;
 import com.tmall.wireless.tac.biz.processor.TodayCrazyRecommendTab.constant.CommonConstant;
 import com.tmall.wireless.tac.biz.processor.TodayCrazyRecommendTab.model.ItemLimitDTO;
 import com.tmall.wireless.tac.biz.processor.common.ScenarioConstantApp;
+import com.tmall.wireless.tac.biz.processor.common.VoKeyConstantApp;
 import com.tmall.wireless.tac.biz.processor.wzt.constant.Constant;
 import com.tmall.wireless.tac.dataservice.log.TacLoggerImpl;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.collections.MapUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.mortbay.util.ajax.JSON;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -43,7 +50,9 @@ public class TodayCrazyRecommendTabBuildItemVoSdkExtPt extends Register implemen
     public Response<ItemEntityVO> process(BuildItemVoRequest buildItemVoRequest) {
         Map<String, Object> userParams = buildItemVoRequest.getContext().getUserParams();
         ItemEntityVO itemEntityVO = new ItemEntityVO();
-
+        String umpChannel = MapUtil.getStringWithDefault(userParams, VoKeyConstantApp.UMP_CHANNEL,
+                VoKeyConstantApp.CHANNEL_KEY);
+        itemEntityVO.put("contentType", 0);
         if (buildItemVoRequest == null || buildItemVoRequest.getItemInfoDTO() == null) {
             return Response.fail(ErrorCode.PARAMS_ERROR);
         }
@@ -53,20 +62,32 @@ public class TodayCrazyRecommendTabBuildItemVoSdkExtPt extends Register implemen
         itemEntityVO.setItemId(Optional.of(itemInfoDTO).map(ItemInfoDTO::getItemEntity).map(ItemEntity::getItemId).orElse(0L));
         itemEntityVO.setO2oType(Optional.of(itemInfoDTO).map(ItemInfoDTO::getItemEntity).map(ItemEntity::getO2oType).orElse(O2oType.B2C.name()));
 
-
+        String itemDesc = null;
         String originScm = "";
         String itemUrl = "";
-
+        String specifications = "";
+        String reservePrice = "";
+        Map<String, Object> attachments = null;
         Map<String, String> trackPoint = Maps.newHashMap();
 
         for (String s : itemInfoDTO.getItemInfos().keySet()) {
             ItemInfoBySourceDTO itemInfoBySourceDTO = itemInfoDTO.getItemInfos().get(s);
-            if (itemInfoBySourceDTO instanceof ItemInfoBySourceCaptainDTO) {
 
+            if (itemInfoBySourceDTO instanceof ItemInfoBySourceCaptainDTO) {
                 ItemInfoBySourceCaptainDTO itemInfoBySourceCaptainDTO = (ItemInfoBySourceCaptainDTO) itemInfoBySourceDTO;
+                specifications = itemInfoBySourceCaptainDTO.getItemDTO().getSpecDetail();
+                itemEntityVO.put("specifications", specifications);
                 itemUrl = Optional.of(itemInfoBySourceCaptainDTO)
                         .map(ItemInfoBySourceCaptainDTO::getItemDTO)
                         .map(ItemDTO::getDetailUrl).orElse("");
+
+                ItemDTO itemDTO = itemInfoBySourceCaptainDTO.getItemDTO();
+                attachments = itemDTO.getAttachments();
+                ItemPromotionResp itemPromotionResp = itemDTO.getItemPromotionResp();
+                itemDesc = buildItemDesc(itemPromotionResp);
+                UnifyPriceDTO unifyPrice = itemPromotionResp.getUnifyPrice();
+                reservePrice = unifyPrice.getChaoShiPrice().getPrice();
+
             }
             if (itemInfoBySourceDTO instanceof ItemInfoBySourceTppDTO) {
                 ItemInfoBySourceTppDTO itemInfoBySourceDTOOrigin = (ItemInfoBySourceTppDTO) itemInfoBySourceDTO;
@@ -78,15 +99,21 @@ public class TodayCrazyRecommendTabBuildItemVoSdkExtPt extends Register implemen
                 trackPoint.putAll(scmKeyValue);
             }
 
-
             itemEntityVO.putAll(getItemVoMap(itemInfoBySourceDTO));
 
         }
         String scm = processScm(originScm, trackPoint);
         itemUrl = itemUrl + "&scm=" + scm;
-
+        JSONObject jsonObject = JSONObject.parseObject(JSON.toString(itemEntityVO.get("locType")));
+        String locType = jsonObject.getString("name");
         itemEntityVO.put("scm", scm);
         itemEntityVO.put("itemUrl", itemUrl);
+        itemEntityVO.put("locType", locType);
+        itemEntityVO.put("reservePrice", reservePrice);
+        itemEntityVO.put("attachment", JSON.toString(attachments));
+        itemEntityVO.put("itemDesc", itemDesc);
+
+        itemEntityVO.put(VoKeyConstantApp.UMP_CHANNEL, umpChannel);
         //todo
         //itemEntityVO.put("source", true);
         this.buildLimit(itemEntityVO, userParams);
@@ -158,6 +185,19 @@ public class TodayCrazyRecommendTabBuildItemVoSdkExtPt extends Register implemen
          * 限购信息
          */
         itemEntityVO.put("itemLimit", itemLimitDTOS.get(0));
+    }
+
+    private String buildItemDesc(ItemPromotionResp itemPromotionResp) {
+        try {
+            BigDecimal chaoShiPrice = new BigDecimal(itemPromotionResp.getUnifyPrice().getChaoShiPrice().getPrice());
+            BigDecimal showPrice = new BigDecimal(itemPromotionResp.getUnifyPrice().getShowPrice().getPrice());
+        } catch (Exception e) {
+            tacLogger.info("获取失败：" + e);
+        }
+        BigDecimal chaoShiPrice = new BigDecimal(itemPromotionResp.getUnifyPrice().getChaoShiPrice().getPrice());
+        BigDecimal showPrice = new BigDecimal(itemPromotionResp.getUnifyPrice().getShowPrice().getPrice());
+        String text = "专享补贴";
+        return text + chaoShiPrice.subtract(showPrice) + "元";
     }
 
     private Map<Long, List<ItemLimitDTO>> getLimitResult(Map<String, Object> userParams) {
