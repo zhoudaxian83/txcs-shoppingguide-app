c9d01c0060fe4d99e2bba1806b97ec98c22a1ebd
wb-ljc587473 <wb-ljc587473@alibaba-inc.com>
2021-10-11 22:46:03 +0800

    1,vo字段扩充

--- src/main/java/com/tmall/wireless/tac/biz/processor/TodayCrazyRecommendTab/TodayCrazyRecommendTabBuildItemVoSdkExtPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/TodayCrazyRecommendTab/TodayCrazyRecommendTabBuildItemVoSdkExtPt.java
@@ -111,6 +111,7 @@ public class TodayCrazyRecommendTabBuildItemVoSdkExtPt extends Register implemen
         tacLogger.info("vo异常-4");
         try {
             tacLogger.info("vo异常-locType" + JSON.toJSONString(itemEntityVO.get("locType")));
+            tacLogger.info("vo异常-locType-2"+itemEntityVO.get("locType"));
             JSONObject jsonObject = JSONObject.parseObject(JSON.toJSONString(itemEntityVO.get("locType")));
             tacLogger.info("vo异常-5");
             String locType = jsonObject.getString("name");
