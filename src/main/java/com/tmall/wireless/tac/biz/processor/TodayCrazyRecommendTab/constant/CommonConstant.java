86a8afa7e0911593e9bec7d6c73301afce416990
wb-ljc587473 <wb-ljc587473@alibaba-inc.com>
2021-10-18 10:38:57 +0800

    1，debug

--- src/main/java/com/tmall/wireless/tac/biz/processor/TodayCrazyRecommendTab/constant/CommonConstant.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/TodayCrazyRecommendTab/constant/CommonConstant.java
@@ -5,7 +5,7 @@ public class CommonConstant {
      * true打开限购开关,如果不需要限购逻辑关闭即可
      */
     public static final boolean LIMIT_BUY_SWITCH = true;
-    public static final String CHANNEL_KEY = "superMarket_todayCrazy";
+    public static final String CHANNEL_KEY = "panicBuyingToday";
     public static final String SUPER_MARKET_TODAY_CRAZY = "superMarket_todayCrazy";
     //public static final String CHANNEL_KEY = "panicBuyingToday";
 
