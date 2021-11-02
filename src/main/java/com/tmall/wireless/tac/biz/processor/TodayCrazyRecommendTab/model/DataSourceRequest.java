272d47cedd1a942fe6555bdae75556fcb0ca0db3
wb-ljc587473 <wb-ljc587473@alibaba-inc.com>
2021-11-02 15:20:43 +0800

    1,优化

--- src/main/java/com/tmall/wireless/tac/biz/processor/TodayCrazyRecommendTab/model/DataSourceRequest.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/TodayCrazyRecommendTab/model/DataSourceRequest.java
@@ -4,21 +4,10 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.io.Serializable;
-import java.util.List;
 
 public class DataSourceRequest implements Serializable {
 
     private static final long serialVersionUID = -1L;
-
-    @Getter
-    @Setter
-    private Long userId;
-    @Getter
-    @Setter
-    private Long smAreaId;
-    @Getter
-    @Setter
-    private String bizCode;
 
     @Getter
     @Setter
@@ -31,10 +20,4 @@ public class DataSourceRequest implements Serializable {
     @Setter
     private String tab;
 
-    @Setter
-    @Getter
-    private List<Long> itemIdList;
-
-    public DataSourceRequest() {
-    }
 }
