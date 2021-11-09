8472192fd33719363f15b8195003138e0e5e9017
wl <guohui.wangguohui@alibaba-inc.com>
2021-11-09 21:08:57 +0800

    提交

--- src/main/java/com/tmall/wireless/tac/biz/processor/firstScreenMind/FirstScreenMindItemItemMetaInfoBuildSdkExtPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/firstScreenMind/FirstScreenMindItemItemMetaInfoBuildSdkExtPt.java
@@ -3,9 +3,11 @@ package com.tmall.wireless.tac.biz.processor.firstScreenMind;
 import java.util.List;
 
 import com.alibaba.cola.extension.Extension;
+import com.alibaba.fastjson.JSON;
 
 import com.google.common.collect.Lists;
-import com.tmall.tcls.gs.sdk.ext.annotation.SdkExtension;
+import com.tmall.aselfcaptain.item.model.ChannelDataDO;
+import com.tmall.tcls.gs.sdk.biz.iteminfo.bysource.ItemInfoSourceKey;
 import com.tmall.tcls.gs.sdk.ext.extension.Register;
 import com.tmall.tcls.gs.sdk.framework.extensions.item.contextbuild.ItemMetaInfoBuildSdkExtPt;
 import com.tmall.tcls.gs.sdk.framework.model.context.BizType;
@@ -14,9 +16,15 @@ import com.tmall.tcls.gs.sdk.framework.model.meta.ItemGroupMetaInfo;
 import com.tmall.tcls.gs.sdk.framework.model.meta.ItemInfoSourceMetaInfo;
 import com.tmall.tcls.gs.sdk.framework.model.meta.ItemMetaInfo;
 import com.tmall.tcls.gs.sdk.framework.model.meta.node.ItemInfoNode;
+import com.tmall.txcs.biz.supermarket.scene.util.MapUtil;
+import com.tmall.txcs.gs.framework.model.SgFrameworkContext;
+import com.tmall.wireless.tac.biz.processor.common.RequestKeyConstantApp;
 import com.tmall.wireless.tac.biz.processor.common.ScenarioConstantApp;
 import com.tmall.wireless.tac.biz.processor.config.TxcsShoppingguideAppSwitch;
+import com.tmall.wireless.tac.biz.processor.firstScreenMind.enums.RenderContentTypeEnum;
 import com.tmall.wireless.tac.client.domain.Context;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Extension(bizId = ScenarioConstantApp.BIZ_TYPE_SUPERMARKET,
     useCase = ScenarioConstantApp.LOC_TYPE_B2C,
@@ -24,9 +32,15 @@ import com.tmall.wireless.tac.client.domain.Context;
 public class FirstScreenMindItemItemMetaInfoBuildSdkExtPt extends Register implements ItemMetaInfoBuildSdkExtPt {
 
 
-    public static final String SCENE_CODE = "shoppingguide.category";
+    Logger LOGGER = LoggerFactory.getLogger(FirstScreenMindItemItemMetaInfoBuildSdkExtPt.class);
+
+    public static final String DEFAULT_SCENE_NAME = "default_scene";
+
+    private static final String CHANNEL_NAME = "sceneLdb";
+
     @Override
     public ItemMetaInfo process(Context context) {
+        LOGGER.error("FirstScreenMindItemItemMetaInfoBuildSdkExtPt.context:{}", JSON.toJSONString(context));
         ItemMetaInfo itemMetaInfo = new ItemMetaInfo();
         ItemGroupMetaInfo itemGroupMetaInfoSmB2c = ItemGroupMetaInfo.build(BizType.SM.getCode() + "_" + O2oType.B2C.name());
         itemGroupMetaInfoSmB2c.setItemInfoNodes(this.buildDefaultItemInfoNodes());
@@ -42,34 +56,39 @@ public class FirstScreenMindItemItemMetaInfoBuildSdkExtPt extends Register imple
     }
 
     private List<ItemInfoNode> buildDefaultItemInfoNodes() {
+
         ItemInfoNode itemInfoNode = new ItemInfoNode();
-        ItemInfoSourceMetaInfo item = ItemInfoSourceMetaInfo.build("captain");
-        item.setMktSceneCode(SCENE_CODE);
-        ItemInfoSourceMetaInfo tppItemInfoSource = ItemInfoSourceMetaInfo.build("tpp");
-        ItemInfoSourceMetaInfo smartUiItemInfoSource = ItemInfoSourceMetaInfo.build("smartui");
-        smartUiItemInfoSource.setStrategyPackageId("707_11169");
-        List<String> e1 = Lists.newArrayList(new String[]{"supermarketPrice", "timesBot", "salesLast30d"});
-        List<String> e2 = Lists.newArrayList(new String[]{"priceLabel", "timesBot", "salesLast30d"});
-        List<List<String>> exclusiveMaterials = Lists.newArrayList();
-        exclusiveMaterials.add(e1);
-        exclusiveMaterials.add(e2);
-        smartUiItemInfoSource.setExclusiveMaterials(exclusiveMaterials);
-        List<String> requireList = Lists.newArrayList(new String[]{"extVideo5S", "richPict", "whitePict"});
-        List<String> requireListPrice = Lists.newArrayList(new String[]{"pagePrice"});
-        List<List<String>> requireListList = Lists.newArrayList();
-        requireListList.add(requireList);
-        requireListList.add(requireListPrice);
-        smartUiItemInfoSource.setRequiredMaterials(requireListList);
-        smartUiItemInfoSource.setAppId(27642L);
+        ItemInfoSourceMetaInfo item= ItemInfoSourceMetaInfo.build(ItemInfoSourceKey.CAPTAIN);
+        item.setMktSceneCode(DEFAULT_SCENE_NAME);
 
-        smartUiItemInfoSource.setMktSceneCode(SCENE_CODE);
+        // tpp返回的数据获取埋点、品牌、类目等数据
+        ItemInfoSourceMetaInfo tppItemInfoSource = ItemInfoSourceMetaInfo.build(ItemInfoSourceKey.TPP);
 
-        if (TxcsShoppingguideAppSwitch.openSmartUiInIconCategory) {
-            itemInfoNode.setItemInfoSourceMetaInfos(Lists.newArrayList(item, tppItemInfoSource, smartUiItemInfoSource));
-        } else {
-            itemInfoNode.setItemInfoSourceMetaInfos(Lists.newArrayList(item, tppItemInfoSource));
-        }
+
+        //isBangdan(context);
+        //新增榜单信息查询
+        ItemInfoSourceMetaInfo channelDataItemInfoSource = ItemInfoSourceMetaInfo.build("channel");
+        ChannelDataDO mostBuyChannelData = new ChannelDataDO();
+        mostBuyChannelData.setChannelField("data");
+        mostBuyChannelData.setChannelName(CHANNEL_NAME);
+        mostBuyChannelData.setDataKey("data");
+        List<ChannelDataDO> datas = Lists.newArrayList(mostBuyChannelData);
+        //channelDataItemInfoSource.setChannelFields(datas);
+        //Map<String, String> extraMap = Maps.newHashMap();
+        //extraMap.put("salePointType", "MOST_WORTH_BUYING");
+        //channelDataItemInfoSource.setExtraMap(extraMap);
+
+
+
+        itemInfoNode.setItemInfoSourceMetaInfos(Lists.newArrayList(item, tppItemInfoSource));
 
         return Lists.newArrayList(new ItemInfoNode[]{itemInfoNode});
     }
+
+    private boolean isBangdan(SgFrameworkContext sgFrameworkContext) {
+        String contentType = MapUtil.getStringWithDefault(sgFrameworkContext.getRequestParams(), RequestKeyConstantApp.CONTENT_TYPE, RenderContentTypeEnum.b2cNormalContent.getType());
+        return RenderContentTypeEnum.bangdanContent.getType().equals(contentType)
+            || RenderContentTypeEnum.bangdanO2OContent.getType().equals(contentType);
+    }
+
 }
