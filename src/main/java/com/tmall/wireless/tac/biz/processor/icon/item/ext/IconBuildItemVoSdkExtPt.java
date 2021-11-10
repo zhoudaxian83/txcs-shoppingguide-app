8d97570e482403b4f39f8747604f1bd9ed2ce507
lw231756 <lw231756@alibaba-inc.com>
2021-11-10 10:49:27 +0800

    增加值得买标识

--- src/main/java/com/tmall/wireless/tac/biz/processor/icon/item/ext/IconBuildItemVoSdkExtPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/icon/item/ext/IconBuildItemVoSdkExtPt.java
@@ -43,7 +43,7 @@ public class IconBuildItemVoSdkExtPt  extends DefaultBuildItemVoSdkExtPt impleme
 
     private static final String MOST_WORTH_BUY_KEY = "mostWorthBuy";
 
-    private static final String MOST_WORTH_BUY_PIC = "https://gw.alicdn.com/imgextra/i1/O1CN01W6XWQV1woBJKJRyhV_!!6000000006354-2-tps-87-45.png";
+    private static final String MOST_WORTH_BUY_PIC = "https://img.alicdn.com/imgextra/i1/O1CN01kAxxAD1aO9GC7TSzp_!!6000000003319-2-tps-216-90.png";
 
 
     @Override
