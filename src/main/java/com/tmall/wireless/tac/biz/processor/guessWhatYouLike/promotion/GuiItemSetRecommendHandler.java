d2a09295e62c231a3f5e42b5c7af2fd43e7e2bcf
lw231756 <lw231756@alibaba-inc.com>
2021-12-01 15:53:26 +0800

    分会场猜你喜欢

---
+++ src/main/java/com/tmall/wireless/tac/biz/processor/guessWhatYouLike/promotion/GuiItemSetRecommendHandler.java
@@ -0,0 +1,54 @@
+package com.tmall.wireless.tac.biz.processor.guessWhatYouLike.promotion;
+
+import java.util.List;
+
+import com.alibaba.aladdin.lamp.domain.response.GeneralItem;
+
+import com.google.common.collect.Lists;
+import com.tmall.tcls.gs.sdk.ext.BizScenario;
+import com.tmall.tcls.gs.sdk.framework.model.ItemEntityVO;
+import com.tmall.tcls.gs.sdk.framework.service.ShoppingguideSdkItemService;
+import com.tmall.wireless.tac.biz.processor.common.ScenarioConstantApp;
+import com.tmall.wireless.tac.biz.processor.huichang.common.constant.HallScenarioConstant;
+import com.tmall.wireless.tac.client.common.TacResult;
+import com.tmall.wireless.tac.client.domain.RequestContext4Ald;
+import com.tmall.wireless.tac.client.handler.TacReactiveHandler4Ald;
+import io.reactivex.Flowable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author zhongwei
+ * @date 2021/12/1
+ */
+@Component
+public class GuiItemSetRecommendHandler extends TacReactiveHandler4Ald {
+
+    @Autowired
+    private ShoppingguideSdkItemService shoppingguideSdkItemService;
+
+
+    @Override
+    public Flowable<TacResult<List<GeneralItem>>> executeFlowable(RequestContext4Ald requestContext4Ald) throws Exception {
+        BizScenario bizScenario = BizScenario.valueOf(ScenarioConstantApp.BIZ_TYPE_SUPERMARKET,
+            ScenarioConstantApp.LOC_TYPE_B2C,
+            ScenarioConstantApp.SCENARIO_SUB_PROMOTION_PAGE);
+        bizScenario.addProducePackage(HallScenarioConstant.HALL_ITEM_SDK_PACKAGE);
+
+        return shoppingguideSdkItemService.recommend(requestContext4Ald, bizScenario)
+            .map(response -> {
+                List<GeneralItem> generalItemList = Lists.newArrayList();
+                List<ItemEntityVO> itemAndContentList = response.getItemAndContentList();
+                itemAndContentList.forEach(contentVO -> {
+                    GeneralItem generalItem = new GeneralItem();
+                    contentVO.keySet().forEach(key -> {
+                        generalItem.put(key, contentVO.get(key));
+                    });
+                    generalItemList.add(generalItem);
+                });
+                return generalItemList;
+            })
+            .map(TacResult::newResult)
+            .onErrorReturn(r -> TacResult.errorResult(""));
+    }
+}
---
+++ src/main/java/com/tmall/wireless/tac/biz/processor/guessWhatYouLike/promotion/GulItemOriginDataRequestBuildSdkExtPt.java
@@ -0,0 +1,125 @@
+package com.tmall.wireless.tac.biz.processor.guessWhatYouLike.promotion;
+
+import java.util.Map;
+import java.util.Optional;
+
+import com.alibaba.cola.extension.Extension;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+import com.ali.com.google.common.base.Joiner;
+import com.ali.com.google.common.collect.Maps;
+import com.google.common.collect.Lists;
+import com.tmall.aselfcommon.util.StackTraceUtil;
+import com.tmall.tcls.gs.sdk.biz.uti.MapUtil;
+import com.tmall.tcls.gs.sdk.ext.extension.Register;
+import com.tmall.tcls.gs.sdk.framework.extensions.item.origindata.ItemOriginDataRequestBuildSdkExtPt;
+import com.tmall.tcls.gs.sdk.framework.model.constant.RequestKeyConstant;
+import com.tmall.tcls.gs.sdk.framework.model.context.CommonUserParams;
+import com.tmall.tcls.gs.sdk.framework.model.context.LocParams;
+import com.tmall.tcls.gs.sdk.framework.model.context.SgFrameworkContext;
+import com.tmall.tcls.gs.sdk.framework.model.context.SgFrameworkContextItem;
+import com.tmall.wireless.store.spi.recommend.model.RecommendRequest;
+import com.tmall.wireless.tac.biz.processor.common.ScenarioConstantApp;
+import com.tmall.wireless.tac.biz.processor.gsh.itemsetrecommend.GshItemSetOriginDataRequestBuildSdkExtPt;
+import com.tmall.wireless.tac.biz.processor.huichang.common.constant.HallCommonAldConstant;
+import com.tmall.wireless.tac.biz.processor.huichang.common.utils.ParseCsa;
+import com.tmall.wireless.tac.client.dataservice.TacLogger;
+import com.tmall.wireless.tac.client.domain.Context;
+import com.tmall.wireless.tac.client.domain.RequestContext4Ald;
+import org.apache.commons.collections.MapUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @author zhongwei
+ * @date 2021/12/1
+ */
+@Extension(
+    bizId = ScenarioConstantApp.BIZ_TYPE_SUPERMARKET,
+    useCase = ScenarioConstantApp.LOC_TYPE_B2C,
+    scenario = ScenarioConstantApp.SCENARIO_SUB_PROMOTION_PAGE
+)
+public class GulItemOriginDataRequestBuildSdkExtPt extends Register implements ItemOriginDataRequestBuildSdkExtPt {
+
+    Logger logger = LoggerFactory.getLogger(GulItemOriginDataRequestBuildSdkExtPt.class);
+
+    private static final String ITEM_SET_PREFIX = "rb_";
+    public static final Long DEFAULT_LOGAREAID = 107L;
+    public static final Long ITEM_SET_RECOMMEND_APPID = 29202L;
+    public static final Long DEFAULT_SMAREAID = 330100L;
+    public static final int PAGE_SIZE = 6;
+    private static final Long DefaultUserId = 0L;
+
+    @Autowired
+    TacLogger tacLogger;
+
+    @Override
+    public RecommendRequest process(SgFrameworkContextItem sgFrameworkContextItem) {
+        tacLogger.debug("GulItemOriginDataRequestBuildSdkExtPt");
+        RecommendRequest recommendRequest = new RecommendRequest();
+        try {
+
+            //String resourceId = MapUtil.getStringWithDefault(aldContext, HallCommonAldConstant.ALD_CURRENT_RES_ID, "0");
+            //params.put("resourceId", resourceId);
+
+            recommendRequest.setLogResult(true);
+            Context context = sgFrameworkContextItem.getTacContext();
+            RequestContext4Ald requestContext4Ald = (RequestContext4Ald)context;
+            Map<String, Object> aldParams = requestContext4Ald.getAldParam();
+            Map<String, Object> aldContext = requestContext4Ald.getAldContext();
+            Map<String, String> params = Maps.newHashMap();
+            Long smAreaId = MapUtil.getLongWithDefault(aldParams, RequestKeyConstant.SMAREAID, DEFAULT_SMAREAID);
+            params.put("smAreaId", String.valueOf(smAreaId));
+            params.put("usingCrowd2I", "false");
+
+            params.put("logicAreaId", Joiner.on(",").join(Optional.of(sgFrameworkContextItem).map(SgFrameworkContext::getCommonUserParams)
+                .map(CommonUserParams::getLocParams).map(LocParams::getLogicIdByPriority).orElse(
+                    Lists.newArrayList())));
+
+            Integer index = 0;
+            if(MapUtils.isNotEmpty(sgFrameworkContextItem.getRequestParams())){
+                index = MapUtil.getIntWithDefault(sgFrameworkContextItem.getRequestParams(),"index",0);
+            }
+            params.put("isFirstPage", index > 0 ? "false" : "true");
+
+            Long rt1HourStoreId = Optional.of(sgFrameworkContextItem).map(SgFrameworkContext::getCommonUserParams)
+                .map(CommonUserParams::getLocParams).map(LocParams::getRt1HourStoreId).orElse(0L);
+            params.put("rt1HourStoreId", String.valueOf(rt1HourStoreId));
+
+            Long rtHalfDayStoreId = Optional.of(sgFrameworkContextItem).map(SgFrameworkContext::getCommonUserParams)
+                .map(CommonUserParams::getLocParams).map(LocParams::getRtHalfDayStoreId).orElse(0L);
+            params.put("rtHalfDayStoreId", String.valueOf(rtHalfDayStoreId));
+
+            params.put("closeSls", "0");
+
+            String itemSetId = "";
+            String tacParams = MapUtil.getStringWithDefault(aldParams, "tacParams", "");
+            if (StringUtils.isNotBlank(tacParams)) {
+                JSONObject tacParamsMap = JSON.parseObject(tacParams);
+                itemSetId = Optional.ofNullable(tacParamsMap.getString(HallCommonAldConstant.ITEM_SET_ID)).orElse("");
+            }
+            if (StringUtils.isEmpty(itemSetId)) {
+                logger.error("-----GulItemOriginDataRequestBuildSdkExtPt.itemSetId is empty-------");
+                throw new Exception("itemSetId is empty");
+            }
+            params.put("itemSetIdList", itemSetId);
+            // todo  先写死
+            params.put("itemBusinessType","B2C");
+
+            recommendRequest.setAppId(ITEM_SET_RECOMMEND_APPID);
+            recommendRequest.setUserId(
+                MapUtil.getLongWithDefault(aldContext, HallCommonAldConstant.USER_ID, DefaultUserId));
+            recommendRequest.setParams(params);
+
+            tacLogger.debug("Tpp参数：" + JSONObject.toJSONString(recommendRequest));
+            return recommendRequest;
+        } catch (Exception e) {
+            tacLogger.debug(
+                "GulItemOriginDataRequestBuildSdkExtPt 失败" + StackTraceUtil.stackTrace(e));
+            return recommendRequest;
+        }
+    }
+}
