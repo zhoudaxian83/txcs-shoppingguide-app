dec9777a099210f75a2f4aefaf87064964f59b16
yanwu <jiabin.pjb@alibaba-inc.com>
2021-12-13 22:30:52 +0800

    sec kill

--- src/main/java/com/tmall/wireless/tac/biz/processor/processtemplate/common/service/ProcessTemplateRecommendService.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/processtemplate/common/service/ProcessTemplateRecommendService.java
@@ -1,11 +1,8 @@
 package com.tmall.wireless.tac.biz.processor.processtemplate.common.service;
 
 import com.tmall.wireless.store.spi.recommend.model.RecommendContentEntityDTO;
-import com.tmall.wireless.store.spi.recommend.model.RecommendItemEntityDTO;
-import com.tmall.wireless.store.spi.recommend.model.RecommendRequest;
 import com.tmall.wireless.store.spi.recommend.model.RecommendResponseEntity;
 import com.tmall.wireless.tac.biz.processor.processtemplate.common.ProcessTemplateContext;
-import com.tmall.wireless.tac.biz.processor.processtemplate.common.service.model.recommend.RecommendModel;
 
 import java.util.Map;
 
