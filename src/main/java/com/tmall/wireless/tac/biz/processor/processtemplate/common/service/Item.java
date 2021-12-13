c85526bf80d06968edec06a7d247967d99c68ba6
yanwu <jiabin.pjb@alibaba-inc.com>
2021-12-14 01:17:52 +0800

    sec kill

--- src/main/java/com/tmall/wireless/tac/biz/processor/processtemplate/common/service/Item.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/processtemplate/common/service/Item.java
@@ -1,5 +1,10 @@
 package com.tmall.wireless.tac.biz.processor.processtemplate.common.service;
 
+import lombok.Data;
+import org.springframework.stereotype.Service;
+
+@Service
+@Data
 public class Item {
     private Long itemId;
 }
