64ab5f124de3702ad62964eb633c4a78569a6e57
yanwu <jiabin.pjb@alibaba-inc.com>
2021-12-14 00:53:52 +0800

    sec kill

--- src/main/java/com/tmall/wireless/tac/biz/processor/processtemplate/common/service/model/recommend/ItemSetRecommendModel.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/processtemplate/common/service/model/recommend/ItemSetRecommendModel.java
@@ -11,10 +11,9 @@ import java.util.stream.Collectors;
  * 圈品集推荐模型，根据圈品集推商品的数据模型
  */
 @Data
-public class ItemSetRecommendModel implements RecommendModel {
+public class ItemSetRecommendModel{
     private List<ItemSetItems> itemSetItemsList;
 
-    @Override
     public List<Long> getAllItemIds() {
         if(CollectionUtils.isEmpty(itemSetItemsList)) {
             return new ArrayList<>();
