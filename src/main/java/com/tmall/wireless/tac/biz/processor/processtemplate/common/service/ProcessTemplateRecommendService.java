3528dbf8414580809dff4530e62db36f4b908211
yanwu <jiabin.pjb@alibaba-inc.com>
2021-12-14 00:47:59 +0800

    sec kill

--- src/main/java/com/tmall/wireless/tac/biz/processor/processtemplate/common/service/ProcessTemplateRecommendService.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/processtemplate/common/service/ProcessTemplateRecommendService.java
@@ -1,10 +1,5 @@
 package com.tmall.wireless.tac.biz.processor.processtemplate.common.service;
 
-import com.tmall.wireless.tac.biz.processor.processtemplate.common.ProcessTemplateContext;
-import com.tmall.wireless.tac.biz.processor.processtemplate.common.service.model.recommend.ItemSetRecommendModel;
-
-import java.util.Map;
-
 public interface ProcessTemplateRecommendService {
     void recommend();
     //ItemSetRecommendModel recommendContent(Long appId, ProcessTemplateContext context, Map<String, String>params);
