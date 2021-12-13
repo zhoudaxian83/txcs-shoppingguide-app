6cb4f17a177a514a77443dc8fe05a336e1fb7fa5
yanwu <jiabin.pjb@alibaba-inc.com>
2021-12-13 23:41:42 +0800

    sec kill

--- src/main/java/com/tmall/wireless/tac/biz/processor/processtemplate/timelimitedseckill/TimeLimitedSecKillHandler.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/processtemplate/timelimitedseckill/TimeLimitedSecKillHandler.java
@@ -1,7 +1,12 @@
 package com.tmall.wireless.tac.biz.processor.processtemplate.timelimitedseckill;
 
 import com.alibaba.aladdin.lamp.domain.response.GeneralItem;
+import com.tmall.wireless.store.spi.recommend.model.RecommendContentEntityDTO;
 import com.tmall.wireless.tac.biz.processor.processtemplate.common.ProcessTemplateContext;
+import com.tmall.wireless.tac.biz.processor.processtemplate.common.service.ProcessTemplateRecommendService;
+import com.tmall.wireless.tac.biz.processor.processtemplate.common.service.model.recommend.ItemSetRecommendModel;
+import com.tmall.wireless.tac.biz.processor.processtemplate.common.service.model.recommend.ItemSetRecommendModelHandler;
+import com.tmall.wireless.tac.biz.processor.processtemplate.common.service.model.recommend.RecommendResponseHandler;
 import com.tmall.wireless.tac.client.common.TacResult;
 import com.tmall.wireless.tac.client.dataservice.TacLogger;
 import com.tmall.wireless.tac.client.domain.RequestContext4Ald;
@@ -17,8 +22,8 @@ import java.util.Map;
 @Component
 public class TimeLimitedSecKillHandler extends TacReactiveHandler4Ald {
 
-    //@Autowired
-    //private ProcessTemplateRecommendService recommendService;
+    @Autowired
+    private ProcessTemplateRecommendService recommendService;
     @Autowired
     private TacLogger tacLogger;
 
@@ -29,7 +34,7 @@ public class TimeLimitedSecKillHandler extends TacReactiveHandler4Ald {
         Map<String, String> params = new HashMap<>();
         params.put("contentType", "3");
         params.put("itemSetIdList", "415609,415620");
-        //RecommendResponseHandler<RecommendContentEntityDTO, ItemSetRecommendModel> handler = new ItemSetRecommendModelHandler<>();
+        RecommendResponseHandler<RecommendContentEntityDTO, ItemSetRecommendModel> handler = new ItemSetRecommendModelHandler<>();
         tacLogger.warn("aaaa");
         //RecommendModel recommendModel = recommendService.recommendContent(21557L, context, params, handler);
         //tacLogger.warn("recommendResponse" + JSON.toJSONString(recommendModel.getAllItemIds()));
