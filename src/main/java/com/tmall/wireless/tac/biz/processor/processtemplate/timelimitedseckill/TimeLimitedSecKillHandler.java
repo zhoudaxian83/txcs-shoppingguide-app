dd6fe74c33705849bc572f425e6345f0ab411792
yanwu <jiabin.pjb@alibaba-inc.com>
2021-12-14 01:34:20 +0800

    sec kill

--- src/main/java/com/tmall/wireless/tac/biz/processor/processtemplate/timelimitedseckill/TimeLimitedSecKillHandler.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/processtemplate/timelimitedseckill/TimeLimitedSecKillHandler.java
@@ -4,6 +4,8 @@ import com.alibaba.aladdin.lamp.domain.response.GeneralItem;
 import com.tmall.wireless.tac.biz.processor.processtemplate.common.ProcessTemplateContext;
 import com.tmall.wireless.tac.biz.processor.processtemplate.common.service.ProcessTemplateRecommendService;
 import com.tmall.wireless.tac.biz.processor.processtemplate.common.service.ProcessTemplateRenderService;
+import com.tmall.wireless.tac.biz.processor.processtemplate.common.service.model.recommend.ItemSetRecommendModel;
+import com.tmall.wireless.tac.biz.processor.processtemplate.common.service.model.recommend.RecommendResponseHandler;
 import com.tmall.wireless.tac.client.common.TacResult;
 import com.tmall.wireless.tac.client.dataservice.TacLogger;
 import com.tmall.wireless.tac.client.domain.RequestContext4Ald;
@@ -23,7 +25,7 @@ public class TimeLimitedSecKillHandler extends TacReactiveHandler4Ald {
     private ProcessTemplateRenderService processTemplateRenderService;
 
     @Autowired
-    private ProcessTemplateRecommendService processTemplateRecommendService;
+    private ProcessTemplateRecommendService recommendService;
 
     @Autowired
     private TacLogger tacLogger;
@@ -35,9 +37,9 @@ public class TimeLimitedSecKillHandler extends TacReactiveHandler4Ald {
         Map<String, String> params = new HashMap<>();
         params.put("contentType", "3");
         params.put("itemSetIdList", "415609,415620");
-        //RecommendResponseHandler handler = a -> new ItemSetRecommendModel();
+        RecommendResponseHandler handler = a -> new ItemSetRecommendModel();
         tacLogger.warn("aaaa");
-        //RecommendModel recommendModel = recommendService.recommendContent(21557L, context, params, handler);
+        Object recommendModel = recommendService.recommendContent(21557L, context, params);
         //tacLogger.warn("recommendResponse" + JSON.toJSONString(recommendModel.getAllItemIds()));
         return Flowable.just(TacResult.newResult(null));
     }
