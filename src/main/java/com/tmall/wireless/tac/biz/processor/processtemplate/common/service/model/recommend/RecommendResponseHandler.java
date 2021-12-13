3cbd6807d9aadc031a0939a7e252b48505a99572
yanwu <jiabin.pjb@alibaba-inc.com>
2021-12-14 01:43:45 +0800

    sec kill

--- src/main/java/com/tmall/wireless/tac/biz/processor/processtemplate/common/service/model/recommend/RecommendResponseHandler.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/processtemplate/common/service/model/recommend/RecommendResponseHandler.java
@@ -3,6 +3,7 @@ package com.tmall.wireless.tac.biz.processor.processtemplate.common.service.mode
 import com.tmall.wireless.store.spi.recommend.model.RecommendContentEntityDTO;
 import com.tmall.wireless.store.spi.recommend.model.RecommendResponseEntity;
 
+@FunctionalInterface
 public interface RecommendResponseHandler {
     ItemSetRecommendModel handle(RecommendResponseEntity<RecommendContentEntityDTO> responseEntity);
 }
