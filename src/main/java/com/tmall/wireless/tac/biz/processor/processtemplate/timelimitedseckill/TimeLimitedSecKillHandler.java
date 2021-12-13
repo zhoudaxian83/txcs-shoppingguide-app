615aaacde424f95a265b63af03d9326b60b10cb1
yanwu <jiabin.pjb@alibaba-inc.com>
2021-12-14 00:33:39 +0800

    sec kill

--- src/main/java/com/tmall/wireless/tac/biz/processor/processtemplate/timelimitedseckill/TimeLimitedSecKillHandler.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/processtemplate/timelimitedseckill/TimeLimitedSecKillHandler.java
@@ -3,6 +3,7 @@ package com.tmall.wireless.tac.biz.processor.processtemplate.timelimitedseckill;
 import com.alibaba.aladdin.lamp.domain.response.GeneralItem;
 import com.tmall.wireless.tac.biz.processor.processtemplate.common.ProcessTemplateContext;
 import com.tmall.wireless.tac.biz.processor.processtemplate.common.service.ProcessTemplateRecommendService;
+import com.tmall.wireless.tac.biz.processor.processtemplate.common.service.ProcessTemplateRenderService;
 import com.tmall.wireless.tac.client.common.TacResult;
 import com.tmall.wireless.tac.client.dataservice.TacLogger;
 import com.tmall.wireless.tac.client.domain.RequestContext4Ald;
@@ -19,7 +20,7 @@ import java.util.Map;
 public class TimeLimitedSecKillHandler extends TacReactiveHandler4Ald {
 
     @Autowired
-    private ProcessTemplateRecommendService processTemplateRecommendService;
+    private ProcessTemplateRenderService processTemplateRenderService;
     @Autowired
     private TacLogger tacLogger;
 
