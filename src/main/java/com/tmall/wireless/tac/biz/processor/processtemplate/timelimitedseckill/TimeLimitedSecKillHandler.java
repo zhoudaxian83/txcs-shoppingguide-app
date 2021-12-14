d90f5caf56659d263a074cac262bdbcf97835c01
yanwu <jiabin.pjb@alibaba-inc.com>
2021-12-14 16:07:20 +0800

    sec kill

--- src/main/java/com/tmall/wireless/tac/biz/processor/processtemplate/timelimitedseckill/TimeLimitedSecKillHandler.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/processtemplate/timelimitedseckill/TimeLimitedSecKillHandler.java
@@ -37,11 +37,6 @@ public class TimeLimitedSecKillHandler extends TacReactiveHandler4Ald {
         Map<String, String> params = new HashMap<>();
         params.put("contentType", "3");
         params.put("itemSetIdList", "415609,415620");
-        try {
-            RecommendResponseHandler handler = a -> new ItemSetRecommendModel();
-        } catch (Exception e) {
-            tacLogger.error("handler", e);
-        }
         tacLogger.warn("aaaa");
         Object recommendModel = recommendService.recommendContent(21557L, context, params);
         tacLogger.warn("recommendResponse" + JSON.toJSONString(recommendModel));
