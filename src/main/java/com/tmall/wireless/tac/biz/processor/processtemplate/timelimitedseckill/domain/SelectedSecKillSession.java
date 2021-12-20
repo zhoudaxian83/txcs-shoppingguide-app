bc902633eaf7ce17150abd66974684b79681381f
yanwu <jiabin.pjb@alibaba-inc.com>
2021-12-20 11:56:27 +0800

    sec kill

--- src/main/java/com/tmall/wireless/tac/biz/processor/processtemplate/timelimitedseckill/domain/SelectedSecKillSession.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/processtemplate/timelimitedseckill/domain/SelectedSecKillSession.java
@@ -25,17 +25,16 @@ public class SelectedSecKillSession {
     }
 
     /**
+     * 未开始的才使用未来时间，秒杀中和已结束直接返回null，用当前时间
      * 查询未来价格的时间，如果返回不为空，则查询未来时间的价格
      * 如果返回为空，则查询当前价格
-     * 注意：开始时间特意往后偏移了59秒，避免优惠价在开始时间那一刻还没生效
-     * 具体偏移的秒数还是需要具体情况具体分析，只要没有一分钟内的优惠，59秒是个合适的选择。
      *
      * @return 查询未来价格的时间
      */
     public Long timeOfFuturePrice() {
-        if(status == 0 || status == 1) {
+        if(status == 1 || status == 2) {
             return null;
         }
-        return startTime.plusSeconds(59).toInstant(ZoneOffset.ofHours(8)).toEpochMilli();
+        return startTime.toInstant(ZoneOffset.ofHours(8)).toEpochMilli();
     }
 }
