1fcedb3431f8d2fc515829fe1c75c85ebd200dad
wl <guohui.wangguohui@alibaba-inc.com>
2022-02-14 14:23:53 +0800

    新增打印日志

--- src/main/java/com/tmall/wireless/tac/biz/processor/firstScreenMind/FirstScreenMindContentOriginDataRequestExPt.java
+++ src/main/java/com/tmall/wireless/tac/biz/processor/firstScreenMind/FirstScreenMindContentOriginDataRequestExPt.java
@@ -1,7 +1,11 @@
 package com.tmall.wireless.tac.biz.processor.firstScreenMind;
 
 import java.util.Map;
+
+import com.alibaba.cola.extension.BizScenario;
 import com.alibaba.cola.extension.Extension;
+import com.alibaba.fastjson.JSON;
+
 import com.tmall.txcs.biz.supermarket.scene.util.MapUtil;
 import com.tmall.txcs.gs.framework.extensions.origindata.request.ContentOriginDataRequestExtPt;
 import com.tmall.txcs.gs.framework.model.SgFrameworkContextContent;
@@ -10,6 +14,7 @@ import com.tmall.wireless.tac.biz.processor.common.ScenarioConstantApp;
 import com.tmall.wireless.tac.biz.processor.firstScreenMind.common.FirstScreenConstant;
 import com.tmall.wireless.tac.biz.processor.firstScreenMind.origindatarequest.OriginDataRequestFactory;
 import com.tmall.wireless.tac.client.dataservice.TacLogger;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +24,7 @@ import org.springframework.stereotype.Service;
         useCase = ScenarioConstantApp.LOC_TYPE_B2C,
         scenario = ScenarioConstantApp.SCENE_FIRST_SCREEN_MIND_CONTENT)
 @Service
+@Slf4j
 public class FirstScreenMindContentOriginDataRequestExPt implements ContentOriginDataRequestExtPt {
     Logger LOGGER = LoggerFactory.getLogger(FirstScreenMindContentOriginDataRequestExPt.class);
 
@@ -29,6 +35,7 @@ public class FirstScreenMindContentOriginDataRequestExPt implements ContentOrigi
 
     @Override
     public RecommendRequest process(SgFrameworkContextContent sgFrameworkContextContent) {
+        BizScenario bizScenario = sgFrameworkContextContent.getBizScenario();
         Map<String,Object> requestParams = sgFrameworkContextContent.getRequestParams();
         RecommendRequest tppRequest = null;
         /**前端没有传递，默认是首页内容**/
@@ -44,6 +51,7 @@ public class FirstScreenMindContentOriginDataRequestExPt implements ContentOrigi
         }else if(FirstScreenConstant.GCS_ITEM_FEEDS.equals(requestFrom)){
             tppRequest = originDataRequestFactory.getRecommendRequest(FirstScreenConstant.GCS_SUB_CONTENT_FEEDS,sgFrameworkContextContent);
         }
+        log.error("FirstScreenMindContentOriginDataRequestExPt.biz:{},tppRequest:{}",bizScenario.getUniqueIdentity(), JSON.toJSONString(tppRequest));
         return tppRequest;
     }
 
